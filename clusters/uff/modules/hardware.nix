4b8a85d19df3d66bba99f1174c98f813ec2fc2c8
Michael Buckley <michaelcbuckley@proton.me>
2025-03-29 19:06:42 -0400

    Tweaked error in UFF configs

--- clusters/uff/modules/hardware.nix
+++ clusters/uff/modules/hardware.nix
@@ -1,6 +1,6 @@
 # The common hardware of the cluster
 
-{config, lib, modulesPath, ...}: {
+{config, modulesPath, ...}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
@@ -14,5 +14,6 @@
     extraModulePackages = [];
   };
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 }
