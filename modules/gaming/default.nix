1311fec4a0878350754997a8dabcfe202d500664
Michael Buckley <michaelcbuckley@proton.me>
2025-03-31 15:37:13 -0400

    Updated structure of user modules

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -3,13 +3,11 @@
   lib,
   ...
 }: {
-  options.features.gaming = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Enable gaming on the host.";
+  options.features.gaming = {
+    enable = lib.mkEnableOption  "Enable gaming on the host.";
   };
 
-  config = lib.mkIf config.features.gaming {
+  config = lib.mkIf config.features.gaming.enable {
     # Any Custom Filesystem mounts will be under the host itself
     hardware.steam-hardware.enable = true;
 
