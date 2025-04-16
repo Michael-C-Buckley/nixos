679fbe1e6fa16b3a3f40310f52671c8b6b8b5bd9
Michael Buckley <michaelcbuckley@proton.me>
2025-04-15 20:31:34 -0400

    Fixed issues on the address function

--- lib/address.nix
+++ lib/address.nix
@@ -2,5 +2,5 @@
   parts = builtins.split "/" cidr;
 in {
   address = builtins.elemAt parts 0;
-  prefixLength = lib.strings.toInt (builtins.elemAt parts 1);
+  prefixLength = lib.strings.toInt (builtins.elemAt parts 2);
 }
