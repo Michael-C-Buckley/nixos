b5f586777789aa3e03b4e6790a3ec5c3f1205cc6
Michael Buckley <michaelcbuckley@proton.me>
2025-04-21 08:53:19 -0400

    Remove temporary personal cache

--- system/nix.nix
+++ system/nix.nix
@@ -13,7 +13,6 @@
       ];
       substituters = [
         "https://cache.nixos.org"
-        "http://o1.groovyreserve.com:5000"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://microvm.cachix.org"
@@ -22,7 +21,6 @@
         "https://cache.thalheim.io" # sops-nix
       ];
       trusted-public-keys = [
-        "o1.groovyreserve.com:DNejb/fRpyMLcSelv3MV+BoOIwOo0XHafwXNp67IGAw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
