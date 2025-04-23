c3d7859f1733595429c1bb0f0fde38b8cfe82368
Michael Buckley <michaelcbuckley@proton.me>
2025-04-23 18:13:31 -0400

    Added O3 and to use disko for provisioning and disk management

--- clusters/oracle/hosts/o3/hardware.nix
+++ clusters/oracle/hosts/o3/hardware.nix
@@ -1,34 +1,19 @@
 {modulesPath, ...}: {
-  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+  imports = [
+    ../../modules/disko.nix
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot = {
     tmp.cleanOnBoot = true;
+    kernelModules = ["kvm-amd"];
     initrd = {
       availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
       kernelModules = ["nvme"];
     };
     zfs.extraPools = ["zroot"];
-  };
-
-  fileSystems = {
-    "/nix" = {
-      device = "zroot/root/nix";
-      fsType = "zfs";
-    };
-    "/home" = {
-      device = "zroot/root/home";
-      fsType = "zfs";
-    };
-    "/" = {
-      device = "zroot/root/nixos";
-      fsType = "zfs";
-    };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/585A-8DF4";
-      fsType = "vfat";
-    };
   };
 
   zramSwap.enable = true;
