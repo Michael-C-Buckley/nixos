cbd1dbe037b609bf6e5b88a70abbfd25e2d3ade0
Michael Buckley <michaelcbuckley@proton.me>
2025-04-24 06:17:30 -0400

    Tweaked O3's options more to be compatible, not there yet but getting closer

--- clusters/oracle/hosts/o3/hardware.nix
+++ clusters/oracle/hosts/o3/hardware.nix
@@ -1,4 +1,4 @@
-{modulesPath, ...}: {
+{modulesPath, lib, ...}: {
   imports = [
     ../../modules/disko.nix
     (modulesPath + "/profiles/qemu-guest.nix")
@@ -7,13 +7,26 @@
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot = {
+    # Swap to Grub
+    loader = lib.mkForce {
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = false;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;  # ⬅ Ensures BOOTX64.EFI is installed!
+        device = "nodev";              # Required for UEFI boot
+      };
+    };
+
     tmp.cleanOnBoot = true;
     kernelModules = ["kvm-amd"];
     initrd = {
-      availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+      availableKernelModules = [ "virtio_pci" "virtio_blk" "xhci_pci" "ahci" "sd_mod" "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
       kernelModules = ["nvme"];
     };
     zfs.extraPools = ["zroot"];
+    zfs.forceImportRoot = lib.mkForce true;
   };
 
   zramSwap.enable = true;
