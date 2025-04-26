f64b164f8bc9863ff4a3a68d24b6c2994c5ad265
Michael Buckley <michaelcbuckley@proton.me>
2025-04-26 07:44:39 -0400

    Removed wfetch for now

--- system/nix.nix
+++ system/nix.nix
@@ -34,14 +34,12 @@ in {
         "https://nix-community.cachix.org"
         "https://microvm.cachix.org"
         "https://cosmic.cachix.org/"
-        "https://wfetch.cachix.org"
         "https://cache.thalheim.io" # sops-nix
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "wfetch.cachix.org-1:lFMD3l0uT/M4+WwqUXpmPAm2kvEH5xFGeIld1av0kus"
         "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
       ];
       trusted-users = [
