640878b00cc7adfa2c16306a65a4be98ceb9897a
Michael Buckley <michaelcbuckley@proton.me>
2025-05-02 22:04:05 -0400

    Tweaked format of WG service

--- lib/wireguard-interface.nix
+++ lib/wireguard-interface.nix
@@ -17,17 +17,17 @@
 
     ExecStartPre =
       [
-        "${pkgs.iproute2}/bin/ip link add dev ${name} type wireguard"
-        "${pkgs.iproute2}/bin/ip link set ${name} mtu ${toString mtu}"
-        "${pkgs.wireguard-tools}/bin/wg setconf ${name} ${cfgPath}"
+        "${pkgs.iproute2}/bin/ip link add dev wg-${name} type wireguard"
+        "${pkgs.iproute2}/bin/ip link set wg-${name} mtu ${toString mtu}"
+        "${pkgs.wireguard-tools}/bin/wg setconf wg-${name} ${cfgPath}"
       ]
       ++ lib.concatMap (addr: [
-        "${pkgs.iproute2}/bin/ip address add ${addr} dev ${name}"
+        "${pkgs.iproute2}/bin/ip address add ${addr} dev wg-${name}"
       ])
       ipAddresses;
 
-    ExecStart = "${pkgs.iproute2}/bin/ip link set ${name} up";
-    ExecStopPost = "${pkgs.iproute2}/bin/ip link delete ${name}";
+    ExecStart = "${pkgs.iproute2}/bin/ip link set wg-${name} up";
+    ExecStopPost = "${pkgs.iproute2}/bin/ip link delete wg-${name}";
     Restart = "on-failure";
     RestartSec = "10s";
     StartLimitIntervalSec = 60;
