a780c5632dd617249c6685ca498fe989966a1aab
Michael Buckley <michaelcbuckley@proton.me>
2025-05-03 19:27:00 -0400

    Added a check for merge conflicts into the dev shell

--- outputs/checks.nix
+++ outputs/checks.nix
@@ -2,6 +2,7 @@
   x86_64-linux.pre-commit-check = inputs.pre-commit-hooks.lib.x86_64-linux.run {
     src = ../.;
     hooks = {
+      check-merge-conflicts.enable = true;
       deadnix.enable = true;
       detect-private-keys.enable = true;
       typos.enable = true;
