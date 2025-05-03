549e4ba5bcdcbf0a30a3cbec89bb31131d152333
Michael Buckley <michaelcbuckley@proton.me>
2025-05-02 22:22:40 -0400

    Moved O1 WG to use sops and not local files

--- clusters/oracle/hosts/o1/networking/wireguard.nix
+++ clusters/oracle/hosts/o1/networking/wireguard.nix
@@ -7,13 +7,11 @@
     "wireguard-clients" = customLib.wireguardInterface {
       inherit config;
       name = "clients";
-      cfgPath = "/etc/wireguard/wg-clients.conf";
       ipAddresses = ["192.168.32.129/27" "fe80::32:129/64"];
     };
     "wireguard-mt1" = customLib.wireguardInterface {
       inherit config;
       name = "mt1";
-      cfgPath = "/etc/wireguard/mt1.conf";
       ipAddresses = ["192.168.254.81/31" "fe80::254:81/64"];
     };
   };
