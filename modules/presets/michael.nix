bbc708f76fe45eaa9067132b496a14f970b87f56
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 18:45:08 -0400

    Modified hosts files

---
+++ modules/presets/michael.nix
@@ -0,0 +1,14 @@
+{
+    nix.settings = {
+        substituters = [
+            "http://x570.michael.lan:5000"
+            # "http://p520.michael.lan:5000"
+            # "http://uff1.michael.lan:5000"
+            # "http://uff2.michael.lan:5000"
+            # "http://uff3.micheal.lan:5000"
+        ];
+        trusted-public-keys = [
+            "x570.michael.lan:b3fLRrQyBeUbmpS+AGi68O1L2F1kSLEVX2ePAyDPNWk="
+        ];
+    };
+}
