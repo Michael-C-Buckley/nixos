7f8956fd8eb5ebaf78a8ec3efa01f4efb7d3154a
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 11:46:02 -0400

    Added routing for a nix cache page

--- clusters/oracle/hosts/o1/nginx.nix
+++ clusters/oracle/hosts/o1/nginx.nix
@@ -3,6 +3,23 @@
   services.nginx = {
     enable = true;
 
+    virtualHosts."nix-cache.groovyreserve.com" = {
+      enableACME = true;
+      forceSSL = true;
+
+      # Serve static homepage
+      root = "/var/www/nix-cache-web";
+      locations."/" = {
+        index = "index.html";
+      };
+
+      # Proxy all other requests
+      locations."~ ^/(nix-cache|nix|store|nar|.narinfo|log|cache)/" = {
+        proxyPass = "http://127.0.0.1:5000"; # replace with actual backend
+        proxyWebsockets = true;
+      };
+    };
+
     virtualHosts."groovyreserve.com" = {
       enableACME = true;
       forceSSL = true;
@@ -19,5 +36,7 @@
   systemd.tmpfiles.rules = [
     "d /var/www/splash 0755 root root"
     "f /var/www/splash/index.html 0644 root root - <html><body><h1>Welcome to groovyreserve.com.  This is only a test.</h1></body></html>"
+    "d /var/www/nix-cache-web 0755 root root"
+    "f /var/www/nix-cache-web/index.html 0644 root root - <html><body><h1>Nix Cache coming soon.</h1></body></html>"
   ];
 }
