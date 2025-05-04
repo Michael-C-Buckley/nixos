4591a888f0d94e9947846e8532cbf52b0f40e748
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 07:44:25 -0400

    Added firewall exception for web traffic

--- clusters/oracle/hosts/o1/nginx.nix
+++ clusters/oracle/hosts/o1/nginx.nix
@@ -14,6 +14,8 @@
     };
   };
 
+  networking.firewall.allowedTCPPorts = [80 443];
+
   systemd.tmpfiles.rules = [
     "d /var/www/splash 0755 root root"
     "f /var/www/splash/index.html 0644 root root - <html><body><h1>Welcome to dns.groovyreserve.com</h1></body></html>"
