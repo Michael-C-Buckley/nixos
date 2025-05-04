6b318626058b65bd506b5a8a6e0bfe9900461564
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 07:34:49 -0400

    Added simple nginx to O1

---
+++ clusters/oracle/hosts/o1/nginx.nix
@@ -0,0 +1,21 @@
+{
+  # Test deployment
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."dns.groovyreserve.com" = {
+      enableACME = true;
+      forceSSL = true;
+
+      root = "/var/www/splash";
+      locations."/" = {
+        index = "index.html";
+      };
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/www/splash 0755 root root"
+    "f /var/www/splash/index.html 0644 root root - <html><body><h1>Welcome to dns.groovyreserve.com</h1></body></html>"
+  ];
+}
