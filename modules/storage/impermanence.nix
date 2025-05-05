2672cfc21e5044758814a1e5eb96c01a7f2b9b36
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 22:17:11 -0400

    Made a basic option to enable impermanence (not yet functional, just structural)

--- modules/storage/impermanence.nix
+++ modules/storage/impermanence.nix
@@ -4,91 +4,103 @@
 # - Persist: persisted and ZFS snapshotted
 # - Cache: persisted but no snapshots
 {
-  # To make sure keys are available for sops decryption
-  fileSystems."/etc/ssh".neededForBoot = true;
-
-  environment.persistence."/cache" = {
-    hideMounts = true;
-    users.michael.directories = [
-      "Downloads"
-      ".cache/yarn"
-      ".local/share/.cargo"
-      ".local/share/.rustup"
-      ".local/share/fish"
-      ".cache/pip"
-      ".cache/thumbnails"
-    ];
-    directories = [
-      "/var/lib/libvirt"
-      "/var/lib/docker"
-      "/var/lib/incus"
-      "/var/lib/gns3"
-      "/var/lib/nixos-container"
-    ];
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    system.impermanence = {
+      enable = lib.mkEnableOption "Enable Impermanence on the system's storage";
+    };
   };
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      "/etc/ssh"
-      "/etc/nix/secrets"
-      "/var/log" # systemd journal is stored in /var/log/journal
-      "/var/lib/bluetooth"
-      "/var/lib/nixos" # for persisting user uids and gids
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
-    ];
-    files = [
-      "/etc/machine-id"
-    ];
-    users.michael = {
+
+  config = {
+    # To make sure keys are available for sops decryption
+    fileSystems."/etc/ssh".neededForBoot = true;
+
+    environment.persistence."/cache" = {
+      hideMounts = true;
+      users.michael.directories = [
+        "Downloads"
+        ".cache/yarn"
+        ".local/share/.cargo"
+        ".local/share/.rustup"
+        ".local/share/fish"
+        ".cache/pip"
+        ".cache/thumbnails"
+      ];
       directories = [
-        "projects"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-        "nixos" # My system flake is in ~/
-        ".config/zen"
-        ".cache/zen"
-        ".config/vivaldi"
-        ".config/Bitwarden"
-        ".config/fish"
-        ".config/sops"
-        ".config/signal"
-        ".config/telegram"
-        ".config/discord"
-        ".config/BraveSoftware"
-        ".config/dconf"
-        ".config/legcord"
-        ".cache/nix-index"
-        ".cache/nix-search-tv"
-        ".config/librewolf"
-        ".cache/librewolf"
-        ".pki"
-        ".local/state/wireplumber"
+        "/var/lib/libvirt"
+        "/var/lib/docker"
+        "/var/lib/incus"
+        "/var/lib/gns3"
+        "/var/lib/nixos-container"
+      ];
+    };
+    environment.persistence."/persist" = {
+      hideMounts = true;
+      directories = [
+        "/etc/ssh"
+        "/etc/nix/secrets"
+        "/var/log" # systemd journal is stored in /var/log/journal
+        "/var/lib/bluetooth"
+        "/var/lib/nixos" # for persisting user uids and gids
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
         {
-          directory = ".gnupg";
-          mode = "0700";
+          directory = "/var/lib/colord";
+          user = "colord";
+          group = "colord";
+          mode = "u=rwx,g=rx,o=";
         }
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        {
-          directory = ".local/share/keyrings";
-          mode = "0700";
-        }
-        ".local/share/direnv"
       ];
       files = [
-        ".screenrc"
+        "/etc/machine-id"
       ];
+      users.michael = {
+        directories = [
+          "projects"
+          "Music"
+          "Pictures"
+          "Documents"
+          "Videos"
+          "nixos" # My system flake is in ~/
+          ".config/zen"
+          ".cache/zen"
+          ".config/vivaldi"
+          ".config/Bitwarden"
+          ".config/fish"
+          ".config/sops"
+          ".config/signal"
+          ".config/telegram"
+          ".config/discord"
+          ".config/BraveSoftware"
+          ".config/dconf"
+          ".config/legcord"
+          ".cache/nix-index"
+          ".cache/nix-search-tv"
+          ".config/librewolf"
+          ".cache/librewolf"
+          ".pki"
+          ".local/state/wireplumber"
+          {
+            directory = ".gnupg";
+            mode = "0700";
+          }
+          {
+            directory = ".ssh";
+            mode = "0700";
+          }
+          {
+            directory = ".local/share/keyrings";
+            mode = "0700";
+          }
+          ".local/share/direnv"
+        ];
+        files = [
+          ".screenrc"
+        ];
+      };
     };
   };
 }
