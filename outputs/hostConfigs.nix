c6df424799e15a828ca620ebeb32a2260c3d92ad
Michael Buckley <michaelcbuckley@proton.me>
2025-05-04 20:04:54 -0400

    Tweaked nixosConfig generation

--- outputs/hostConfigs.nix
+++ outputs/hostConfigs.nix
@@ -1,36 +1,40 @@
 {self}: let
   inherit (self) inputs;
-  # All current individual hosts are currently X86
-  system = "x86_64-linux";
   # Build the configs for the hosts based on this function
-  hostConfig = {host}: let
+  hostConfig = {
+    host,
+    system ? "x86_64-linux",
+    extraModules ? [],
+  }: let
     pkgs = import inputs.nixpkgs {
       inherit system;
-      config = {allowUnfree = true;};
+      config.allowUnfree = true;
     };
     lib = inputs.nixpkgs.lib;
     customLib = import ../lib {inherit pkgs lib;};
   in
     inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {inherit self pkgs customLib lib host system inputs;};
-      modules = [
-        ../modules
-        ../configurations/hosts/${host}
-        ../configurations/home/hjem.nix
-      ];
+      modules =
+        [
+          ../modules
+          ../configurations/hosts/${host}
+          ../configurations/home/hjem.nix
+        ]
+        ++ extraModules;
     };
 
-  hosts = [
-    "x570"
-    "t14"
-    "p520"
-    "wsl"
-    "vm"
-  ];
+  mkMichaelConfig = {host}:
+    hostConfig {
+      inherit host;
+      extraModules = [
+        ../modules/presets/michael.nix
+      ];
+    };
+  michaelhosts = ["x570" "t14" "p520" "wsl" "vm"];
 in
-  # Construct and return the attribute set
   builtins.listToAttrs (map (host: {
       name = host;
-      value = hostConfig {inherit host;};
+      value = mkMichaelConfig {inherit host;};
     })
-    hosts)
+    michaelhosts)
