244a55ca10012d488ea25a110b3a36d0f02c978b
Michael Buckley <michaelcbuckley@proton.me>
2025-05-06 17:08:48 -0400

    Disabled microcode updates on the desktop

--- configurations/hosts/x570/hardware/default.nix
+++ configurations/hosts/x570/hardware/default.nix
@@ -36,7 +36,7 @@
   security.rtkit.enable = true;
 
   services.ucodenix = {
-    enable = true;
+    enable = false; # Desktop is suffering CPU crashes, dump the extra microcode
     cpuModelId = "00A20F12";
   };
 
