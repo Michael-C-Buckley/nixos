2c04d02e43a659d87b34b2a425067c7683c3a4fb
Michael Buckley <michaelcbuckley@proton.me>
2025-05-06 20:23:25 -0400

    Fixed some of the default that Incus should have

--- configurations/modules/virtualization/incus.nix
+++ configurations/modules/virtualization/incus.nix
@@ -12,15 +12,19 @@ in {
   };
 
   config = {
+    # Incus is bested used with these modules available
+    boot.kernelModules = ["apparmor" "virtiofs" "9p" "9pnet_virtio"];
     services.lvm.boot.thin.enable = mkDefault incus.useLvmThin;
     # Incus will prefer Red Hat's Virtiofs over 9P
     environment.systemPackages = [pkgs.virtiofsd];
     # Incus requires nftables over iptables
     networking.nftables.enable = mkDefault incus.enable;
 
+    security.apparmor.enable = mkDefault true;
+
     virtualisation = {
       incus = {
-        package = mkDefault pkgs.incus;
+        package = mkDefault pkgs.incus; # Stable version is old 
         ui.enable = mkDefault true;
         agent.enable = mkDefault true;
       };
