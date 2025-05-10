bdf82b5ea05249f730bb5434b60f23366ff74e16
Michael Buckley <michaelcbuckley@proton.me>
2025-05-10 19:32:42 -0400

    Remove local reference for the secret version

--- configurations/hosts/x570/networking/wireguard.nix
+++ configurations/hosts/x570/networking/wireguard.nix
@@ -7,8 +7,6 @@
     "wireguard-mt1" = customLib.wireguardInterface {
       inherit config;
       name = "mt1";
-      # Path because Agenix is currently not working on the desktop, this is the local file workaround
-      cfgPath = "/etc/wireguard/wg-mt1.conf";
       ipAddresses = ["192.168.240.242/30"];
     };
   };
