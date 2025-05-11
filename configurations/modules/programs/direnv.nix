25de4015a01524074c8c904a333b3f3b53ac49e4
Michael Buckley <michaelcbuckley@proton.me>
2025-05-11 10:35:17 -0400

    Added some config for direnv

--- configurations/modules/programs/direnv.nix
+++ configurations/modules/programs/direnv.nix
@@ -3,5 +3,11 @@ _: {
     enable = true;
     silent = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        warn_timeout = "0";
+        hide_env_diff = true;
+      };
+    };
   };
 }
