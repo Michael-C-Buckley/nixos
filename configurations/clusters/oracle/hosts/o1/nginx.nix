fe60c728ba33bb789bbe0928e5cb6e294f0058a5
Michael Buckley <michaelcbuckley@proton.me>
2025-05-12 23:10:32 -0400

    Added initial test for mTLS

--- configurations/clusters/oracle/hosts/o1/nginx.nix
+++ configurations/clusters/oracle/hosts/o1/nginx.nix
@@ -5,6 +5,11 @@ in {
   services.nginx = {
     enable = true;
 
+    ssl_client_certificate  /etc/nginx/ca/root-ca.crt;   # <- the file you just copied
+    ssl_trusted_certificate /etc/nginx/ca/root-ca.crt;   # good practice for full chain
+    ssl_verify_client       on;        # optional | optional_no_ca | off
+    ssl_verify_depth        2;         # how many intermediates you allow
+
     virtualHosts."nix-cache.groovyreserve.com" = {
       locations."/" = {
         proxyPass = "http://${nixServe.bindAddress}:${builtins.toString nixServe.port}";
@@ -22,9 +27,22 @@ in {
       enableACME = true;
       forceSSL = true;
 
+      extraConfig = ''
+        ssl_client_certificate  /etc/certs/michael.crt.pem;
+        ssl_trusted_certificate /etc/certs/michael.crt.pem;
+        ssl_verify_client       optional;
+        ssl_verify_depth        2;
+      '';
+
       root = "/var/www/home";
       locations."/" = {
         index = "index.html";
+      };
+      locations."/vaultwarden" = {
+        index = "vaultwarden.html";
+        extraConfig = ''
+          if ($ssl_client_verify != SUCCESS) { return 403; }
+        '';
       };
     };
   };
@@ -34,5 +52,6 @@ in {
   systemd.tmpfiles.rules = [
     "d /var/www/home 0755 root root"
     "f /var/www/home/index.html 0644 root root - <html><body><h1>Welcome to groovyreserve.com.  This is only a test.</h1></body></html>"
+    "f /var/www/home/vaultwarden.html 0644 root root - <html><body><h1>Welcome to groovyreserve.com/vaultwarden.  This is only a test.</h1></body></html>"
   ];
 }
--- configurations/hosts/x570/networking/default.nix
+++ configurations/hosts/x570/networking/default.nix
@@ -1,8 +1,20 @@
-{lib, ...}: {
+{lib, ...}: let
+  certSecret = file: {
+    owner = "michael";
+    path = "/home/michael/.certs/${file}";
+  };
+in {
   imports = [
     ./routing.nix
     ./wireguard.nix
   ];
+
+  # Certs for my own services
+  sops.secrets = {
+    "x570-key" = certSecret "x570.key.pem";
+    "x570-crt" = certSecret "x570.crt.pem";
+    "x570-p12" = certSecret "x570.p12";
+  };
 
   networking = {
     ospf.enable = true;
