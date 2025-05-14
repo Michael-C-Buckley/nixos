447970d7c41fece0f5baf72915bbcf6513bdae9d
Michael Buckley <michaelcbuckley@proton.me>
2025-05-14 12:45:58 -0400

    Added extra python language features to NVF for development

--- packages/nvf/config/languages/default.nix
+++ packages/nvf/config/languages/default.nix
@@ -6,7 +6,14 @@
   html.enable = true;
   sql.enable = true;
   rust.enable = true;
-  python.enable = true;
   yaml.enable = true;
   nix = import ./nix.nix;
+
+  python = {
+    enable = true;
+    dap.enable = true;
+    format.enable = true;
+    lsp.enable = true;
+    treesitter.enable = true;
+  };
 }
