1c3e6dc1f0bc7753d4a7142dc3cb7ff7680ac289
Michael Buckley <michaelcbuckley@proton.me>
2025-05-19 20:04:15 -0400

    Added k3s to my desktop for testing

---
+++ configurations/hosts/x570/k3s.nix
@@ -0,0 +1,12 @@
+_: {
+  # Following along at: https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/k3s/docs/USAGE.md
+  networking.firewall = {
+    allowedTCPPorts = [6443 2379 2380];
+    allowedUDPPorts = [8472];
+  };
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+  };
+}
