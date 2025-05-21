ee4b281a82b2d1af609a680e66f19d51e0b4b2ae
Michael Buckley <michaelcbuckley@proton.me>
2025-05-20 20:59:00 -0400

    Refined the outputs for devshells and checks to be cleaner

--- outputs/devshells.nix
+++ outputs/devshells.nix
@@ -1,36 +1,36 @@
 {self}: let
+  inherit (self.inputs) nixpkgs; 
   systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  forAllSystems = nixpkgs.lib.genAttrs systems;
+  pkgsForSystem = system: 
+    import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
 in
-  builtins.listToAttrs (map (system: let
-      pkgs = import self.inputs.nixpkgs {
-        inherit system;
-        config = {allowUnfree = true;};
-      };
-    in {
-      name = system;
-      value = {
-        default = self.devShells.${system}.nixos;
+  forAllSystems (system: let
+    pkgs = pkgsForSystem system;
+  in {
+    default = self.devShells.${system}.nixos;
 
-        nixos = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-          buildInputs = with pkgs; [
-            self.checks.${system}.pre-commit-check.enabledPackages
-            # Editing
-            alejandra
-            git
-            tig
+    nixos = pkgs.mkShell {
+      inherit (self.checks.${system}.pre-commit-check) shellHook;
+      buildInputs = with pkgs; [
+        self.checks.${system}.pre-commit-check.enabledPackages
+        # Editing
+        alejandra
+        git
+        tig
 
-            # Security
-            trufflehog
-            rage
-            sops
-            ssh-to-pgp
-            ssh-to-age
-          ];
-          env = {
-            TRUFFLEHOG_NO_UPDATE = "1";
-          };
-        };
+        # Security
+        trufflehog
+        rage
+        sops
+        ssh-to-pgp
+        ssh-to-age
+      ];
+      env = {
+        TRUFFLEHOG_NO_UPDATE = "1";
       };
-    })
-    systems)
+    };
+  })
