31607933a144763704ce5a09fdee6e70a53ba0da
Michael Buckley <michaelcbuckley@proton.me>
2025-05-21 04:10:15 -0400

    Removed binds and swapped themes to telescope

--- packages/nvf/config/keymaps/basic.nix
+++ packages/nvf/config/keymaps/basic.nix
@@ -1,55 +1,41 @@
-let
-  schemes = [
-    "slate"
-    "habamax"
-    "gruvbox"
-    "ayu"
-    "rose-pine"
-    "catppuccin-mocha"
-    "onedark"
-  ];
-  colorBinds =
-    builtins.map (s: {
-      mode = "n";
-      key = "<leader>cs${builtins.substring 0 1 s}";
-      action = ":colorscheme ${s}";
-      silent = true;
-    })
-    schemes;
-in
-  [
-    {
-      # Close buffer
-      mode = "n";
-      key = "<leader>bd";
-      action = ":bd<CR>";
-      silent = true;
-    }
-    {
-      mode = "n";
-      key = "<leader>fa";
-      action = ":!alejandra %<CR>";
-      silent = true;
-    }
-    {
-      mode = "n";
-      key = "<leader>ff";
-      action = ":Telescope find_files<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>fg";
-      action = ":Telescope live_grep<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>fh";
-      action = ":Telescope help_tags<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>fb";
-      action = ":Telescope buffers<CR>";
-    }
-  ]
-  ++ colorBinds
+[
+  {
+    mode = "n";
+    key = "<leader>cs";
+    action = ":Telescope colorscheme<CR>";
+    silent = true;
+  }
+  {
+    # Close buffer
+    mode = "n";
+    key = "<leader>bd";
+    action = ":bd<CR>";
+    silent = true;
+  }
+  {
+    mode = "n";
+    key = "<leader>fa";
+    action = ":!alejandra %<CR>";
+    silent = true;
+  }
+  {
+    mode = "n";
+    key = "<leader>ff";
+    action = ":Telescope find_files<CR>";
+  }
+  {
+    mode = "n";
+    key = "<leader>fg";
+    action = ":Telescope live_grep<CR>";
+  }
+  {
+    mode = "n";
+    key = "<leader>fh";
+    action = ":Telescope help_tags<CR>";
+  }
+  {
+    mode = "n";
+    key = "<leader>fb";
+    action = ":Telescope buffers<CR>";
+  }
+]
