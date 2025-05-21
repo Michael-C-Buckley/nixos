2236cd19aaea37bf7b49cdfaed079284d7070341
Michael Buckley <michaelcbuckley@proton.me>
2025-05-20 20:59:00 -0400

    Refined the outputs for devshells and checks to be cleaner

--- outputs/checks.nix
+++ outputs/checks.nix
@@ -1,12 +1,19 @@
-{inputs}: {
-  x86_64-linux.pre-commit-check = inputs.pre-commit-hooks.lib.x86_64-linux.run {
-    src = ../.;
-    hooks = {
-      check-merge-conflicts.enable = true;
-      deadnix.enable = true;
-      detect-private-keys.enable = true;
-      typos.enable = true;
-      flake-checker.enable = true;
-    };
-  };
-}
+{inputs}: let
+  inherit (inputs) nixpkgs pre-commit-hooks;
+  systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  forAllSystems = nixpkgs.lib.genAttrs systems;
+in
+  forAllSystems (
+    system: {
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ../.;
+        hooks = {
+          check-merge-conflicts.enable = true;
+          deadnix.enable = true;
+          detect-private-keys.enable = true;
+          typos.enable = true;
+          flake-checker.enable = true;
+        };
+      };
+    }
+  )
