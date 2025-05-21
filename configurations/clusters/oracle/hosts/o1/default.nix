7d50b29287b3b81d2a7dcd0f00c219fd50ee4bdb
Michael Buckley <michaelcbuckley@proton.me>
2025-05-20 20:15:05 -0400

    Restored logrotate to O1

--- configurations/clusters/oracle/hosts/o1/default.nix
+++ configurations/clusters/oracle/hosts/o1/default.nix
@@ -13,7 +13,5 @@
 
   services = {
     nix-serve.enable = true; # public key: nix-cache.groovyreserve.com:2bl9yB/F+bbd9H2zND2ZHyU21kFNErnteY47q5K6Jww=
-    # This service is causing 25.05 to fail because it can't find group 30000 for whatever reason
-    logrotate.enable = false;
   };
 }
