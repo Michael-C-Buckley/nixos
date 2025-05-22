65633ced346fff8918110cf120e7662e961b4e88
Michael Buckley <michaelcbuckley@proton.me>
2025-05-21 20:59:42 -0400

    Fixed T14's filesytems to have an actually impermanent root

--- configurations/hosts/t14/hardware/filesystems.nix
+++ configurations/hosts/t14/hardware/filesystems.nix
@@ -1,6 +1,6 @@
 _: let
   zfsFs = name: {
-    device = "zroot/${name}";
+    device = "zroot/t14/nixos/${name}";
     fsType = "zfs";
     neededForBoot = true;
   };
@@ -14,9 +14,20 @@ in {
       fsType = "vfat";
     };
 
+    # Tmpfs
+    "/" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [
+        "defaults"
+        "size=2G"
+        "mode=755"
+      ];
+    };
+
     # ZFS Volumes
     #  Root and tmp are a fallbacks for tmpfs
-    "/" = zfsFs "nixroot";
+    # "/" = zfsFs "nixroot";
     "/tmp" = zfsFs "tmp";
     "/nix" = zfsFs "nix";
     "/cache" = zfsFs "cache";
