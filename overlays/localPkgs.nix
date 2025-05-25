f4a76b7bbeeccedfaaf08d119a2ed3a9ea102fab
Michael Buckley <michaelcbuckley@proton.me>
2025-05-25 10:03:26 -0400

    Changed to more robust pathing scheme

--- overlays/localPkgs.nix
+++ overlays/localPkgs.nix
@@ -1,6 +1,8 @@
 # Add local packages
-{self, inputs}: _: super: {
-  nvf = super.callPackage ../packages/nvf/packages/default.nix {inherit self inputs;};
-  nvf-default = super.callPackage ../packages/nvf/packages/default.nix {inherit self inputs;};
-  nvf-minimal = super.callPackage ../packages/nvf/packages/minimal.nix {inherit self inputs;};
+{self, inputs}: _: super: let
+  nvfPath = "${self}/packages/nvf/packages";
+in {
+  nvf = super.callPackage "${nvfPath}/default.nix" {inherit self inputs;};
+  nvf-default = super.callPackage "${nvfPath}/default.nix" {inherit self inputs;};
+  nvf-minimal = super.callPackage "${nvfPath}/minimal.nix" {inherit self inputs;};
 }
