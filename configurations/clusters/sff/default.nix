585c0db6e6b3b9806f8e0bd26c038665ecb92844
Michael Buckley <michaelcbuckley@proton.me>
2025-05-25 21:52:09 -0400

    Disabled logrotate since the whole cluster is having that regression

--- configurations/clusters/sff/default.nix
+++ configurations/clusters/sff/default.nix
@@ -4,6 +4,10 @@
     ./modules/filesystems.nix
   ];
 
+  # Logrotate randomly breaking?
+  # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501
+  services.logrotate.checkConfig = false;
+
   system = {
     stateVersion = "25.11";
     preset = "server";
--- configurations/clusters/sff/hosts/sff2/default.nix
+++ configurations/clusters/sff/hosts/sff2/default.nix
@@ -11,8 +11,4 @@ in {
     hostId = "fb020cc2";
     interfaces.eno1.ipv4.addresses = [(addr ethIP 24)];
   };
-
-  # Logrotate randomly breaking?
-  # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501
-  services.logrotate.checkConfig = false;
 }
