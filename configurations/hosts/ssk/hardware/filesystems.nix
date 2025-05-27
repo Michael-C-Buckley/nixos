5bcc3f62f1a8a1aa569bdf19926a04a8df947ee8
Michael Buckley <michaelcbuckley@proton.me>
2025-05-26 20:51:54 -0400

    Changed SSK to use tmpfs root

--- configurations/hosts/ssk/hardware/filesystems.nix
+++ configurations/hosts/ssk/hardware/filesystems.nix
@@ -5,12 +5,21 @@ _: let
     neededForBoot = true;
   };
 in {
-  boot.zfs.forceImportAll = true;
+  boot.zfs.forceRoot = true;
 
   system.boot.uuid = "3A0E-2554";
 
   fileSystems = {
-    "/" = zfsFs "root";
+    # Tmpfs
+    "/" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [
+        "defaults"
+        "size=2G"
+        "mode=755"
+      ];
+    };
     "/tmp" = zfsFs "tmp";
     "/nix" = zfsFs "nix";
     "/cache" = zfsFs "cache";
