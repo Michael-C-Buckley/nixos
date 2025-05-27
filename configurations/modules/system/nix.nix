72cb0961b420c6ae7f3019a68da958f990c0532a
Michael Buckley <michaelcbuckley@proton.me>
2025-05-27 06:27:31 -0400

    Removed cosmic cachix

--- configurations/modules/system/nix.nix
+++ configurations/modules/system/nix.nix
@@ -48,14 +48,12 @@ in {
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://microvm.cachix.org"
-        "https://cosmic.cachix.org/"
         "https://cache.thalheim.io" # sops-nix
         "https://nix-cache.groovyreserve.com"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
         "nix-cache.groovyreserve.com:2bl9yB/F+bbd9H2zND2ZHyU21kFNErnteY47q5K6Jww="
       ];
