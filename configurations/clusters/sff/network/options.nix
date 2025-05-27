b2a827df6c6469033f3e4c9584bf63a9356f2ffc
Michael Buckley <michaelcbuckley@proton.me>
2025-05-27 17:24:54 -0400

    SFF networking enhancements pulled from UFF

--- configurations/clusters/sff/network/options.nix
+++ configurations/clusters/sff/network/options.nix
@@ -1,17 +1,34 @@
-{...}: let
+{lib, ...}: let
   inherit (lib) mkOption;
-  inherit (lib.types) str;
+  inherit (lib.types) listOf str;
+
+  mkAddrOption = desc:
+    mkOption {
+      type = listOf str;
+      default = null;
+      description = desc;
+    };
+
+  mkAddrs = intDesc: {
+    ipv4 = mkAddrOption "${intDesc} IPv4 address list (with CIDRs).";
+    ipv6 = mkAddrOption "${intDesc} IPv6 address list (with CIDRs).";
+  };
 in {
-  options = {
-    networking.hardware = {
-      enx520p1.mac = mkOption {
+  options.networkd = {
+    eno1.addresses = mkAddrs "Onboard Ethernet";
+    enx520p1 = {
+      mac = mkOption {
         type = str;
-        description = "The MAC address of the first Intel X520DA2 port.";
+        description = "Intel X520 Port 1 Ethernet MAC address.";
       };
-      enx520p2.mac = mkOption {
+      addresses = mkAddrs "ntel X520 Port 1 Ethernet";
+    };
+    enx520p2 = {
+      mac = mkOption {
         type = str;
-        description = "The MAC address of the second Intel X520DA2 port.";
+        description = "ntel X520 Port 2 Ethernet MAC address.";
       };
+      addresses = mkAddrs "ntel X520 Port 2 Ethernet";
     };
   };
 }
--- configurations/clusters/sff/network/systemd.nix
+++ configurations/clusters/sff/network/systemd.nix
@@ -1,24 +1,40 @@
-{config, ...}: {
+{config, ...}: let
+  inherit (config.networkd) eno1 enx520p1 enx520p2;
+in {
   systemd.network = {
-    links."10-enx520p1" = {
-      matchConfig.MACAddress = config.networking.hardware.enx520p1.mac;
-      linkConfig = {
-        Name = "enx520p1";
-        MTUBytes = 1500; # 1500 until I tweak the switch to support
-      };
-    };
-    links."11-enx520p2" = {
-      matchConfig.MACAddress = config.networking.hardware.enx520p2.mac;
-      linkConfig = {
-        Name = "enx520p2";
-        MTUBytes = 1500; # 1500 until I tweak the switch to support
-      };
-    };
-
     networks = {
-      br0.bridgeConfig = {
-        type = "bridge";
-        name = "br0";
+      "10-eno1" = {
+        matchConfig.Name = "eno1";
+        address = eno1.addresses.ipv4;
+      };
+      "20-enx520p1" = {
+        matchConfig.Name = "enx520p1";
+        address = enx520p1.addresses.ipv4;
+      };
+      "21-enx520p2" = {
+        matchConfig.Name = "enx520p2";
+        address = enx520p1.addresses.ipv4;
+      };
+      # 40 is the default system generated one, this overwrites it
+      "40-lo" = {
+        matchConfig.Name = "lo";
+        address = ["192.168.61.4/32" "${config.networking.loopback.ipv4}/32"];
+      };
+    };
+    links = {
+      "11-enx520p1" = {
+        matchConfig.MACAddress = enx520p1.mac;
+        linkConfig = {
+          Name = "enx520p1";
+          MTUBytes = 1500; # 1500 until I tweak the switch to support
+        };
+      };
+      "12-enx520p2" = {
+        matchConfig.MACAddress = enx520p2.mac;
+        linkConfig = {
+          Name = "enx520p2";
+          MTUBytes = 1500; # 1500 until I tweak the switch to support
+        };
       };
     };
   };
