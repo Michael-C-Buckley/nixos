e5c4553256a77a29393f0960e458cd0b65dbff84
Michael Buckley <michaelcbuckley@proton.me>
2025-05-29 17:28:33 -0400

    Fixed the nftable rule for VRRP (and OSPF)

--- configurations/modules/network/vrrp.nix
+++ configurations/modules/network/vrrp.nix
@@ -4,13 +4,15 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
-  vrrp = config.networking.eigrp;
+  vrrp = config.networking.vrrp;
 in {
   options.networking = {
     vrrp.enable = mkEnableOption "Enable VRRP and allow protocol 112";
   };
   config = mkIf vrrp.enable {
     services.frr.vrrpd.enable = true;
-    networking.firewall.extraInputRules = ''ip protocol 112 accept comment "Allow VRRP"'';
+    networking.firewall.extraInputRules = ''
+      ip protocol 112 accept comment "Allow VRRP"
+    '';
   };
 }
