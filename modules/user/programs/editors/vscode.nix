f1b569faf898a610de3d37bb0fe5dea7c6b6b4f0
Michael Buckley <michaelcbuckley@proton.me>
2025-05-28 20:45:45 -0400

    Added options to allow vscode to be swapped out (and proper vscodium remote SSH support)

--- modules/user/programs/editors/vscode.nix
+++ modules/user/programs/editors/vscode.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption mkIf;
+  inherit (lib) mkEnableOption mkOption mkIf optionals;
   inherit (lib.types) listOf str package;
   cfg = config.programs.vscode;
 
@@ -13,9 +13,35 @@
     if (cfg.nonNixExtensions != [])
     then pkgs.nix4vscode.forVscode cfg.nonNixExtensions
     else [];
+
+  msExtensionList =
+    if (cfg.package == pkgs.vscode)
+    then cfg.msExtensions
+    else [];
+
+  remoteExtension =
+    if (cfg.package == pkgs.vscode)
+    # Use the relevant ones
+    then
+      with pkgs.vscode-extensions.ms-vscode-remote; [
+        remote-ssh
+        remote-ssh-edit
+        remote-containers
+      ]
+    else
+      #
+      with pkgs.open-vsx; [
+        jeanp413.open-remote-ssh
+      ];
 in {
   options.programs.vscode = {
     enable = mkEnableOption "Enable VScode";
+    enableRemote = mkEnableOption "Enable remote development.";
+    package = mkOption {
+      type = package;
+      default = pkgs.vscode;
+      description = "The package to use (and potentially swap for alt client).";
+    };
     extensions = mkOption {
       type = listOf package;
       description = "Extensions from Nixpkgs.";
@@ -26,11 +52,17 @@ in {
       description = "Extensions by name found in the vscode store via nix4vscode overlay.";
       default = [];
     };
+    msExtensions = mkOption {
+      type = listOf package;
+      default = [];
+      description = "Microsoft Official extensions to not include with alt clients.";
+    };
   };
 
   config.packageList = mkIf cfg.enable [
     (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = cfg.extensions ++ outOfStoreExt;
+      vscode = cfg.package;
+      vscodeExtensions = cfg.extensions ++ outOfStoreExt ++ msExtensionList ++ optionals cfg.enableRemote remoteExtension;
     })
   ];
 }
