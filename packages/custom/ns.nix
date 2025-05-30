9da4ad45eb9f9bc36b42f3a9a12a1b4ae06e1781
Michael Buckley <michaelcbuckley@proton.me>
2025-05-30 18:10:22 -0400

    Added ns custom package to my flake outputs

---
+++ packages/custom/ns.nix
@@ -0,0 +1,30 @@
+{pkgs}:
+pkgs.stdenv.mkDerivation {
+  pname = "ns";
+  version = "1.0";
+
+  src = pkgs.nix-search-tv.src;
+
+  nativeBuildInputs = [pkgs.makeWrapper];
+  buildInputs = with pkgs; [
+    fzf
+    nix-search-tv
+  ];
+  unpackPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${pkgs.nix-search-tv.src}/nixpkgs.sh $out/bin/ns
+    chmod +x $out/bin/ns
+
+    # Wrap the script so runtimeInputs are in PATH
+    wrapProgram $out/bin/ns \
+      --prefix PATH : ${pkgs.lib.makeBinPath [
+      pkgs.fzf
+      pkgs.nix-search-tv
+    ]}
+  '';
+
+  # Skip tests as shell checks has false positives
+  checkPhase = "true";
+}
