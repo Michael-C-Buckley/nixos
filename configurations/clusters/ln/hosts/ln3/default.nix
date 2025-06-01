2a32759d2ddace701d4dfe7027d0da0a4793973d
Michael Buckley <michaelcbuckley@proton.me>
2025-06-01 15:43:31 -0400

    Changes to LN3 in order to bring to current standards

--- configurations/clusters/ln/hosts/ln3/default.nix
+++ configurations/clusters/ln/hosts/ln3/default.nix
@@ -1,33 +1,32 @@
-_: let
-  lo = "192.168.78.133";
+{config, ...}: let
+  inherit (config.networking) loopback;
   mainIP = "192.168.65.133";
+
 in {
-  system.stateVersion = "24.05";
+  system.boot.uuid = "B34E-A49B";
 
-  imports = [
-    ./filesystems.nix
-  ];
-
+  # Entire thing due for a refactor
   cluster.ln = {
     kubernetes.masterIP = mainIP;
     networking = {
-      lo.addr = "${lo}/32";
+      lo.addr = "${loopback.ipv4}/32"; 
       eno1.addr = "${mainIP}/24";
       enmlx1 = {
         addr = "192.168.254.3/29";
         mac = "f4:52:14:5e:22:20";
       };
       enmlx2 = {
-        addr = "192.168.254119/29";
+        addr = "192.168.254.119/29";
         mac = "f4:52:14:5e:22:21";
       };
     };
   };
 
-  custom.routing.routerId = lo;
+  custom.routing.routerId = loopback.ipv4;
 
   networking = {
     hostId = "9db44f50";
     hostName = "ln3";
+    loopback.ipv4 = "192.168.78.133";
   };
 }
