e55e4289f5d339d5d379c4f025f1fbbbc1192cc1
Michael Buckley <michaelcbuckley@proton.me>
2025-06-02 16:54:41 -0400

    Removed the unused nix-index flake and comma integration

--- configurations/hosts/wsl/default.nix
+++ configurations/hosts/wsl/default.nix
@@ -5,7 +5,6 @@
 }: {
   imports = [
     inputs.nixos-wsl.nixosModules.default
-    inputs.nix-index-database.nixosModules.nix-index
     ./hjem.nix
     ./wsl.nix
   ];
@@ -17,8 +16,6 @@
     hostId = "e07f0101";
     nameservers = ["1.1.1.1" "8.8.8.8" "9.9.9.9"];
   };
-
-  programs.nix-index-database.comma.enable = true;
 
   services.k3s.enable = true;
 
