cfae36699f5c747a09db33477654a7965d429faf
Michael Buckley <michaelcbuckley@proton.me>
2025-06-03 18:12:00 -0400

    Minor reference correction on a file location

--- flake/packages/default.nix
+++ flake/packages/default.nix
@@ -6,7 +6,7 @@
   }: {
     packages = {
       # keep-sorted start
-      ns = pkgs.callPackage ./ns {};
+      ns = pkgs.callPackage ./ns.nix {};
       nvf = pkgs.callPackage ./nvf {inherit inputs;};
       nvf-minimal = config.packages.nvf.override {is-extended-version = false;};
       # keep-sorted end
