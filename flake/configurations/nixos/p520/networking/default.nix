1833bb5c6defde2612f0a04ec0b03dd6aa84e25f
Michael Buckley <michaelcbuckley@proton.me>
2025-06-06 08:54:20 -0400

    Updated the routing for P520 with BGP module enhancements

--- flake/configurations/nixos/p520/networking/default.nix
+++ flake/configurations/nixos/p520/networking/default.nix
@@ -7,17 +7,6 @@ _: {
     ospf.enable = true;
     hostId = "181a3ead";
 
-    # WIP: Temporary default static route to avoid issues until VRRP goes online
-    defaultGateway = {
-      address = "192.168.48.102";
-      interface = "br0";
-    };
-
-    # WIP: transition to resolved
-    nameservers = [
-      "1.1.1.1"
-    ];
-
     bridges.br0.interfaces = ["eno1"];
     interfaces.br0.ipv4 = {
       addresses = [
