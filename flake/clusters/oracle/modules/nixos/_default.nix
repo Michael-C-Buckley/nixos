302ba5f0a7b652a6523e66258cc2a08e3913f897
Michael Buckley <michaelcbuckley@proton.me>
2025-06-10 16:01:52 -0400

    Condensed module

--- flake/clusters/oracle/modules/nixos/_default.nix
+++ flake/clusters/oracle/modules/nixos/_default.nix
@@ -1,7 +1,16 @@
 {inputs, ...}: {
-  imports = [
-    inputs.disko.nixosModules.disko
+  imports = with inputs; [
+    disko.nixosModules.disko
+    nix-secrets.nixosModules.oracle
   ];
+
+  environment.enableAllTerminfo = true;
+  time.timeZone = "America/New_York";
+
+  system = {
+    preset = "cloud";
+    zfs.enable = true;
+  };
 
   features = {
     graphics = false;
