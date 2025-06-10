485bf31725e3f1a8db54e672f438af8be06eaf16
Michael Buckley <michaelcbuckley@proton.me>
2025-06-10 16:07:11 -0400

    Tweaked routing and swapped away from Nvidia

--- flake/configurations/nixos/p520/networking/default.nix
+++ flake/configurations/nixos/p520/networking/default.nix
@@ -7,6 +7,8 @@ _: {
     ospf.enable = true;
     hostId = "181a3ead";
 
+    loopback.ipv4 = "192.168.63.5";
+
     bridges.br0.interfaces = ["eno1"];
     interfaces.br0.ipv4 = {
       addresses = [
