1882ee041ca7a14430ed7add29b149c43434d821
Michael Buckley <michaelcbuckley@proton.me>
2025-06-12 17:07:52 -0400

    Added a relay script for passing GPG and SSH into WSL

--- flake/configurations/nixos/wsl/hjem.nix
+++ flake/configurations/nixos/wsl/hjem.nix
@@ -1,10 +1,19 @@
 {
   lib,
   self',
+  pkgs,
   ...
-}: {
+}: let
+  inherit (lib) hiPrio;
+  inherit (pkgs) devenv socat;
+in {
   hjem.users.michael = {
-    packageList = [(lib.hiPrio self'.packages.nvf)];
+    packageList = [
+      devenv
+      socat
+      (hiPrio self'.packages.nvf)
+      (import ./packages/relay.nix {inherit pkgs;}) # USBIP will not work so I'm instead forwarding my agents
+    ];
     programs = {
       custom.ns.enable = true;
       librewolf.enable = true;
