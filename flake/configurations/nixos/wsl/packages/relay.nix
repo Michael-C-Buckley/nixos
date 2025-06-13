debf06aba5e5672319e52abe5798fcd519ade9a4
Michael Buckley <michaelcbuckley@proton.me>
2025-06-13 12:59:26 -0400

    Narrowed the scope for the relay to handle just SSH as GPG didn't work

--- flake/configurations/nixos/wsl/packages/relay.nix
+++ flake/configurations/nixos/wsl/packages/relay.nix
@@ -4,7 +4,7 @@
 in
   writeShellApplication {
     name = "relay";
-    runtimeInputs = [pkgs.socat];
+    runtimeInputs = [socat];
 
     checkPhase = ""; # Ignore the shell checks
     text = ''
@@ -12,21 +12,11 @@ in
       set -euo pipefail
 
       SSH_SOCK="/tmp/npiperelay-$PPID-ssh.sock"
-      GPG_SOCK="/tmp/npiperelay-$PPID-gpg.sock"
 
       socat UNIX-LISTEN:"$SSH_SOCK",fork,mode=0600 \
           EXEC:"/mnt/c/wsl/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
 
-      socat UNIX-LISTEN:"$GPG_SOCK",fork,mode=0600 \
-          EXEC:"/mnt/c/wsl/npiperelay.exe -ei -s //./pipe/gpg-agent",nofork &
-
       sleep 0.5
-      echo "Sockets live on PID: $PPID"
-
-      export SSH_AUTH_SOCK="$SSH_SOCK"
-      export GPG_AGENT_SOCK="$GPG_SOCK"
-      export GPG_AGENT_INFO="$GPG_SOCK"
-
-      ssh-add -l
+      echo "SSH:  export SSH_AUTH_SOCK=$SSH_SOCK"
     '';
   }
