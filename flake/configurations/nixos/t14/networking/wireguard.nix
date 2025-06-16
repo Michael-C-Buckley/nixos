17d8ecc2bad27d5ebbe603fdbc117c823709ed34
Michael Buckley <michaelcbuckley@proton.me>
2025-06-15 22:41:21 -0400

    Added new wireguard

--- flake/configurations/nixos/t14/networking/wireguard.nix
+++ flake/configurations/nixos/t14/networking/wireguard.nix
@@ -3,20 +3,30 @@
   pkgs,
   self,
   ...
-}: {
+}: let
+  inherit (self.lib.wireguard) genInterface;
+  mkInterface = {
+    name,
+    ipAddresses,
+  }:
+    genInterface {
+      inherit config pkgs name ipAddresses;
+    };
+in {
   systemd.services = {
-    "wireguard-mt1" = self.lib.wireguard.genInterface {
-      inherit config pkgs;
+    "wireguard-mt1" = mkInterface {
       name = "mt1";
       ipAddresses = ["192.168.78.2/27"];
     };
-    "wireguard-mt3" = self.lib.wireguard.genInterface {
-      inherit config pkgs;
+    "wireguard-mt3" = mkInterface {
       name = "mt3";
       ipAddresses = ["192.168.62.2/27"];
     };
-    "wireguard-o1" = self.lib.wireguard.genInterface {
-      inherit config pkgs;
+    "wireguard-mt4" = mkInterface {
+      name = "mt4";
+      ipAddresses = ["192.168.202.12/27"];
+    };
+    "wireguard-o1" = mkInterface {
       name = "o1";
       ipAddresses = ["192.168.32.131/27"];
     };
