3e23e4e15f30d697ca60fff4094c85bd7597e3e6
Michael Buckley <michaelcbuckley@proton.me>
2025-06-15 22:25:11 -0400

    Added NAT for Incus guests

--- flake/configurations/nixos/t14/networking/default.nix
+++ flake/configurations/nixos/t14/networking/default.nix
@@ -10,5 +10,11 @@
     ospf.enable = true;
     hostId = "8425e349";
     networkmanager.enable = true;
+
+    nat = {
+      enable = true;
+      externalInterface = "wlp3s0";
+      internalInterfaces = ["br0" "br1"];
+    };
   };
 }
