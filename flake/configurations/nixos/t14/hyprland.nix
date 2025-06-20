9403c21c0df1673d83e5e8552a7ae4381e5fd1f2
Michael Buckley <michaelcbuckley@proton.me>
2025-06-20 19:45:12 -0400

    Added my portable monitor for my laptop

--- flake/configurations/nixos/t14/hyprland.nix
+++ flake/configurations/nixos/t14/hyprland.nix
@@ -5,6 +5,9 @@ in {
     programs.hyprland.extraConfig = ''
       # T14 Host-specific
       monitor=eDP-1,1920x1080@60.01Hz,0x0,1
+
+      # Portable Monitor, sometimes used
+      monitor=DP-1,1920x1080@60.00Hz,-1920x0,1
     '';
 
     files = {
