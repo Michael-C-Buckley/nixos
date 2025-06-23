e4baa7988e73e16b16bbb6ea91d6433c65785892
Michael Buckley <michaelcbuckley@proton.me>
2025-06-23 07:06:28 -0400

    Swapped X570 to 6.15 Zen

--- flake/configurations/nixos/x570/hardware/default.nix
+++ flake/configurations/nixos/x570/hardware/default.nix
@@ -11,7 +11,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     kernelModules = ["kvm" "kvm-amd" "virtiofs" "9p" "9pnet_virtio"];
     kernelParams = [
       "amd_pstate=active" # AMD Power efficiency on Linux 6.3+
