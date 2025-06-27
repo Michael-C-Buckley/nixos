59d7f0a5847471a593e2886e48eaefbfe301bb7b
Michael Buckley <michaelcbuckley@proton.me>
2025-06-27 17:22:29 -0400

    First attempt at shallow pulling all inputs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,20 @@
   description = "Michael's System Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable?shallow=1";
-    nix-secrets.url = "git+ssh://git@github.com/michael-c-buckley/nix-secrets";
+    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixos-unstable";
+    nix-secrets.url = "git+ssh://git@github.com/michael-c-buckley/nix-secrets?shallow=1";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.url = "git+https://github.com/hercules-ci/flake-parts?shallow=1";
     systems.url = "github:nix-systems/default";
 
     nixos-wsl = {
-      url = "github:nix-community/nixos-wsl/main";
+      url = "git+https://github.com/nix-community/nixos-wsl?shallow=1&ref=main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
       url = "github:nix-community/home-manager";
+      # url = "git+https://github.com/nix-community/home-manager?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -31,25 +32,25 @@
       };
     };
 
-    microvm.url = "github:astro/microvm.nix";
+    microvm.url = "git+https://github.com/astro/microvm.nix?shallow=1";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nvf = {
-      url = "github:notashelf/nvf";
+      url = "git+https://github.com/notashelf/nvf?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nil.follows = "nvf/nil";
 
     nix4vscode = {
-      url = "github:nix-community/nix4vscode";
+      url = "git+https://github.com/nix-community/nix4vscode?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
+      url = "git+https://github.com/nix-community/nix-vscode-extensions?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
