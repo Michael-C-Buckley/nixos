bf04e1219ca083a9d8e496d9c909f5afa2a03d01
Michael Buckley <michaelcbuckley@proton.me>
2025-06-30 13:33:31 -0400

    Readded the NVF mistakenly removed from WSL

--- flake/configurations/nixos/wsl/hjem.nix
+++ flake/configurations/nixos/wsl/hjem.nix
@@ -1,5 +1,10 @@
-_: {
+{
+  self',
+  lib,
+  ...
+}: {
   hjem.users.michael = {
+    packageList = [(lib.hiPrio self'.packages.nvf)];
     programs = {
       custom.ns.enable = true;
       librewolf.enable = true;
