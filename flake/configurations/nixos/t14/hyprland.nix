36afaf44b87eea465a5697e0e84bce114c7ede3b
Michael Buckley <michaelcbuckley@proton.me>
2025-07-04 09:26:24 -0400

    Converted T14 back to using non-nix hyprland configs

--- flake/configurations/nixos/t14/hyprland.nix
+++ flake/configurations/nixos/t14/hyprland.nix
@@ -2,8 +2,8 @@ _: let
   wpDir = "/home/michael/Pictures/wallpapers";
   paper1 = "${wpDir}/deviant/sky_light_by_bisbiswas_de3r7zn.jpg";
 in {
-  hjem.users.michael = {
-    programs.hyprland.extraConfig = ''
+  hjem.users.michael.files = {
+    ".config/hypr/host.conf".text = ''
       # T14 Host-specific
       monitor=eDP-1,1920x1080@60.01Hz,0x0,1
 
@@ -11,11 +11,11 @@ in {
       monitor=DP-1,1920x1080@60.00Hz,-1920x0,1
     '';
 
-    files = {
-      ".config/hypr/hyprpaper.conf".text = ''
-        preload = ${paper1}
-        wallpaper = , ${paper1}
-      '';
-    };
+    ".config/hypr/hyprpaper.conf".text = ''
+      preload = ${paper1}
+      wallpaper = , ${paper1}
+    '';
+
+    ".current_wallpaper".source = paper1;
   };
 }
