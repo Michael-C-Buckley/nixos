0e1aa510edf6b6f2309f1b7c23bb53429651bef1
Michael Buckley <michaelcbuckley@proton.me>
2025-07-05 07:23:15 -0400

    Commented out the service as things will need pipe operators

--- flake/modules/nixos/services/flatpak.nix
+++ flake/modules/nixos/services/flatpak.nix
@@ -6,12 +6,12 @@
   lib,
   ...
 }: let
-  inherit (lib) concatStringsSep mkIf mkOption optionals mapAttrsToList;
+  inherit (lib) mkIf mkOption optionals; # mapAttrsToList concatStringsSep;
   inherit (lib.types) attrsOf listOf str submodule;
   inherit (config.services) flatpak;
   inherit (config.system) impermanence;
 
-  local = config.services.flatpak;
+  # local = config.services.flatpak;
 
   mkStrOption = mkOption {type = str;};
 
@@ -50,31 +50,31 @@ in {
         ];
       };
     };
-    systemd.services.flatpak-declare = {
-      enable = false; # Still testing
-      wantedBy = ["multi-user.target"];
-      after = ["network-online.target" "nss-lookup.target"];
-      wants = ["network-online.target" "nss-lookup.target"];
-      path = [config.services.flatpak.package];
-      serviceConfig.IOSchedulingClass = "2";
-      serviceConfig.IOSchedulingPriority = "6";
-      script = let
-        remotesScript =
-          local.remotes
-          |> mapAttrsToList (
-            n: v: "flatpak remote-add --if-not-exists ${n} ${v}"
-          )
-          |> concatStringsSep "\n";
-
-        installScript =
-          local.apps
-          |> map (v: "flatpak install ${v.remote} ${v.name}")
-          |> concatStringsSep "\n";
-      in ''
-        ${remotesScript}
-        ${installScript}
-        flatpak remove --unused
-      '';
-    };
+    #   systemd.services.flatpak-declare = {
+    #     enable = false; # Still testing
+    #     wantedBy = ["multi-user.target"];
+    #     after = ["network-online.target" "nss-lookup.target"];
+    #     wants = ["network-online.target" "nss-lookup.target"];
+    #     path = [config.services.flatpak.package];
+    #     serviceConfig.IOSchedulingClass = "2";
+    #     serviceConfig.IOSchedulingPriority = "6";
+    #     script = let
+    #       remotesScript =
+    #         local.remotes
+    #         |> mapAttrsToList (
+    #           n: v: "flatpak remote-add --if-not-exists ${n} ${v}"
+    #         )
+    #         |> concatStringsSep "\n";
+    #
+    #       installScript =
+    #         local.apps
+    #         |> map (v: "flatpak install ${v.remote} ${v.name}")
+    #         |> concatStringsSep "\n";
+    #     in ''
+    #       ${remotesScript}
+    #       ${installScript}
+    #       flatpak remove --unused
+    #     '';
+    #   };
   };
 }
