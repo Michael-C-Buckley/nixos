33e5ded58b3e4df52f97bf301f5c2df97405bbaa
Michael Buckley <michaelcbuckley@proton.me>
2025-07-07 11:23:57 -0400

    Fixed sanoid to match the new naming convention

--- flake/modules/nixos/storage/impermanence/default.nix
+++ flake/modules/nixos/storage/impermanence/default.nix
@@ -7,59 +7,62 @@
   config,
   lib,
   ...
-}:
-lib.mkIf config.system.impermanence.enable {
-  # To make sure keys are available for sops decryption
-  fileSystems = {
-    "/etc/ssh".neededForBoot = true;
-    "/etc/sops".neededForBoot = true;
-  };
+}: let
+  inherit (config.networking) hostName;
+  inherit (config.system) impermanence;
+in
+  lib.mkIf impermanence.enable {
+    # To make sure keys are available for sops decryption
+    fileSystems = {
+      "/etc/ssh".neededForBoot = true;
+      "/etc/sops".neededForBoot = true;
+    };
 
-  environment.persistence."/cache" = {
-    hideMounts = true;
-    directories = [
-      "/var/lib/libvirt"
-      "/var/lib/docker"
-      "/var/lib/incus"
-      "/var/lib/gns3"
-      "/var/lib/nixos-container"
-    ];
-  };
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      "/etc/ssh"
-      "/etc/nixos"
-      "/etc/sops"
-      "/etc/nix/secrets"
-      "/etc/wireguard"
-      "/var/log" # systemd journal is stored in /var/log/journal
-      "/var/lib/bluetooth"
-      "/var/lib/nixos" # for persisting user uids and gids
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
-    ];
-    files = [
-      "/etc/machine-id"
-    ];
-  };
+    environment.persistence."/cache" = {
+      hideMounts = true;
+      directories = [
+        "/var/lib/libvirt"
+        "/var/lib/docker"
+        "/var/lib/incus"
+        "/var/lib/gns3"
+        "/var/lib/nixos-container"
+      ];
+    };
+    environment.persistence."/persist" = {
+      hideMounts = true;
+      directories = [
+        "/etc/ssh"
+        "/etc/nixos"
+        "/etc/sops"
+        "/etc/nix/secrets"
+        "/etc/wireguard"
+        "/var/log" # systemd journal is stored in /var/log/journal
+        "/var/lib/bluetooth"
+        "/var/lib/nixos" # for persisting user uids and gids
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+        {
+          directory = "/var/lib/colord";
+          user = "colord";
+          group = "colord";
+          mode = "u=rwx,g=rx,o=";
+        }
+      ];
+      files = [
+        "/etc/machine-id"
+      ];
+    };
 
-  services.sanoid = {
-    inherit (config.system.zfs) enable;
+    services.sanoid = {
+      inherit (config.system.zfs) enable;
 
-    datasets = {
-      "zroot/persist" = {
-        hourly = 50;
-        daily = 15;
-        weekly = 3;
-        monthly = 1;
+      datasets = {
+        "zroot/${hostName}/persist" = {
+          hourly = 50;
+          daily = 15;
+          weekly = 3;
+          monthly = 1;
+        };
       };
     };
-  };
-}
+  }
