29644d16944f6d19a924d5a7c0e1df168735eca9
Michael Buckley <michaelcbuckley@proton.me>
2025-07-10 00:27:41 -0400

    Fixed fish functions and added a FZF CD one

--- flake/configurations/user/michael/files/fish/functions.fish
+++ flake/configurations/user/michael/files/fish/functions.fish
@@ -1,4 +1,12 @@
 # FRR show command
 function show
-    sudo vtysh -c "show (string join ' ' $argv)"
+    sudo vtysh -c "show $argv"
+end
+
+# CD to the FZF destination
+function fcd
+    set -l dir (find . -type d | fzf)
+    if test -n "$dir"
+        cd "$dir"
+    end
 end
