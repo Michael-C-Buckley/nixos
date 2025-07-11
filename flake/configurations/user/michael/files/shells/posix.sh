5f6d6441708fa1486e70b465d2bff95b33fad5d8
Michael Buckley <michaelcbuckley@proton.me>
2025-07-10 22:14:10 -0400

    Added a few more shell binds and tweaks

--- flake/configurations/user/michael/files/shells/posix.sh
+++ flake/configurations/user/michael/files/shells/posix.sh
@@ -4,3 +4,10 @@
 if command -v bat >/dev/null 2>&1; then
     alias cat='bat -p'
 fi
+
+if command -v eza >/dev/null 2>&1; then
+    alias ls='eza'
+    alias ll='eza -la -g --icons' 
+    alias lt='eza --tree --level=2 --icons' 
+    alias tree='eza --tree'
+fi
