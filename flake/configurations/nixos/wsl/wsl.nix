067a53fa2ffce00ac692e814bb92ecc2bccdea82
Michael Buckley <michaelcbuckley@proton.me>
2025-07-14 10:13:36 -0400

    Added kmod (for modprobe) to WSL

--- flake/configurations/nixos/wsl/wsl.nix
+++ flake/configurations/nixos/wsl/wsl.nix
@@ -1,8 +1,16 @@
-_: {
+{pkgs, ...}: {
   # Disable the default boot options, as WSL has its own
   features.boot = "none";
 
+  # Include Btrfs
+  boot.initrd.availableKernelModules = ["btrfs"];
+
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  environment.systemPackages = with pkgs; [
+    kmod
+    btrfs-progs
+  ];
 
   wsl = {
     enable = true;
