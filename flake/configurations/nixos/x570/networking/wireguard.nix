fbefb0c2ff4025968fc0d790759269ce5293055f
Michael Buckley <michaelcbuckley@proton.me>
2025-07-16 15:35:26 -0400

    Converted X570's WG's to the new format

--- flake/configurations/nixos/x570/networking/wireguard.nix
+++ flake/configurations/nixos/x570/networking/wireguard.nix
@@ -2,18 +2,26 @@
   config,
   pkgs,
   self,
+  lib,
   ...
-}: {
-  systemd.services = {
-    "wireguard-mt1" = self.lib.wireguard.genInterface {
-      inherit config pkgs;
-      name = "mt1";
-      ipAddresses = ["192.168.240.242/30"];
-    };
-    "wireguard-mt4" = self.lib.wireguard.genInterface {
-      inherit config pkgs;
-      name = "mt4";
-      ipAddresses = ["192.168.202.10/24"];
-    };
+}: let
+  inherit (self.lib.wireguard) genInterface;
+  inherit (lib) mapAttrs' nameValuePair;
+
+  interfaces = {
+    mt1 = ["192.168.240.242/30"];
+    mt4 = ["192.168.202.10/24"];
   };
+
+  mkInterface = {
+    name,
+    ipAddresses,
+  }:
+    genInterface {
+      inherit config pkgs name ipAddresses;
+    };
+in {
+  systemd.services = mapAttrs' (name: ipAddresses:
+    nameValuePair "wireguard-${name}" (mkInterface {inherit name ipAddresses;}))
+  interfaces;
 }
