d575140ff012c6a48d8e6bdb0cbb58a0e69d1ddd
Michael Buckley <michaelcbuckley@proton.me>
2025-07-18 15:45:56 -0400

    Corrected some errors

--- flake/clusters/uff/modules/nixos/quadlet/forgejo.nix
+++ flake/clusters/uff/modules/nixos/quadlet/forgejo.nix
@@ -7,7 +7,7 @@
     inherit (config.virtualisation.quadlet.networks.br100-fabric) ref;
 
     # Latest as of July 18 2025
-    version = "1.21.5-0";
+    version = "" 12.0 .0 "";
   in {
     containers.forgejo = {
       autoStart = lib.mkDefault false;
@@ -15,13 +15,14 @@
         image = "codeberg.org/forgejo/forgejo:${version}";
         networks = ["${ref}:ip=192.168.52.21"];
         environments = {
-          "USER_UID" = 1000;
-          "USER_GID" = 1000;
+          "USER_UID" = "1000";
+          "USER_GID" = "1000";
         };
+        # WIP: Clocks need testing and verification
         volumes = [
           "/var/lib/quadlet/forgejo:/data/"
-          "/etc/timezone:/etc/timezone:ro"
-          "/etc/localtime:/etc/localtime:ro"
+          # "/etc/timezone:/etc/timezone:ro"
+          # "/etc/localtime:/etc/localtime:ro"
         ];
       };
       serviceConfig.TimeoutStartSec = "60";
--- flake/clusters/uff/modules/nixos/quadlet/vaultwarden.nix
+++ flake/clusters/uff/modules/nixos/quadlet/vaultwarden.nix
@@ -7,12 +7,12 @@
     inherit (config.virtualisation.quadlet.networks.br100-fabric) ref;
 
     # Latest as of July 18 2025
-    hash = "sha256-87996ca0371d043df90f04a0fa8efb72a880927eb71edc3fd55fd8ad354e42c8";
+    version = "sha256-87996ca0371d043df90f04a0fa8efb72a880927eb71edc3fd55fd8ad354e42c8";
   in {
     containers.vaultwarden = {
       autoStart = lib.mkDefault false;
       containerConfig = {
-        image = "vaultwarden/server:${hash}";
+        image = "vaultwarden/server:${version}";
         networks = ["${ref}:ip=192.168.52.20"];
         volumes = ["/var/lib/quadlet/vaultwarden:/data/"];
       };
