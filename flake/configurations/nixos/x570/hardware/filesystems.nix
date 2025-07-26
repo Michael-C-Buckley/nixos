d09836d4c0eede1b2ef47097a807fce17ec88978
Michael Buckley <michaelcbuckley@proton.me>
2025-07-26 14:57:06 -0400

    Split the impermanence preset into another module for the next generation

--- flake/configurations/nixos/x570/hardware/filesystems.nix
+++ flake/configurations/nixos/x570/hardware/filesystems.nix
@@ -3,6 +3,8 @@ _: {
     boot.uuid = "AFF4-A1EE";
 
     impermanence = {
+      usePreset = false;
+      usePreset2 = true;
       enable = true;
       zrootPath = "zroot";
       tmpRootSize = "2G";
@@ -10,8 +12,10 @@ _: {
   };
 
   fileSystems = {
-    "/media/steam" = {
-      device = "/dev/disk/by-uuid/7d9b6689-5ca7-4fb0-85e8-4b4298bdaa9a";
+    "/media/games" = {
+      device = "zroot/local/games";
+      fsType = "zfs";
+      neededForBoot = true;
     };
   };
 }
