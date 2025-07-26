8e60f38a85fa67269132349663896628966bf3eb
Michael Buckley <michaelcbuckley@proton.me>
2025-07-26 14:57:06 -0400

    Split the impermanence preset into another module for the next generation

--- flake/modules/nixos/presets/default.nix
+++ flake/modules/nixos/presets/default.nix
@@ -3,10 +3,10 @@ _: {
     ./kubernetes/single-node.nix
     ./desktop.nix
     ./impermanence.nix
+    ./impermanence2.nix
     ./intelGraphics.nix
     ./laptop.nix
     ./michael.nix
     ./nvidia.nix
-    ./zfs.nix
   ];
 }
