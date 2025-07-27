87028e0a59ccdb64fe9d011c4c9589e693118555
Michael Buckley <michaelcbuckley@proton.me>
2025-07-26 21:41:35 -0400

    Tweaked gaming module for user cache dirs

--- flake/modules/nixos/gaming.nix
+++ flake/modules/nixos/gaming.nix
@@ -4,13 +4,21 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
+  inherit (config.system) impermanence;
   inherit (config.features) gaming;
+
+  commonDir = {directories = [".local/share/Steam"];};
 in {
   options.features.gaming = {
     enable = mkEnableOption "Enable gaming on the host.";
   };
 
   config = mkIf gaming.enable {
+    # Large, downloadable so remove from snapshots
+    environment.persistence."/cache".users = mkIf impermanence.enable {
+      michael = commonDir;
+      shawn = commonDir;
+    };
     # Any Custom Filesystem mounts will be under the host itself
     hardware.steam-hardware.enable = true;
 
