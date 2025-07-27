6427bbe3111b27621b1a0b003094f3d0f7da6976
Michael Buckley <michaelcbuckley@proton.me>
2025-07-27 12:56:11 -0400

    Added persistent homes for LN series

--- flake/clusters/ln/modules/nixos/filesystems.nix
+++ flake/clusters/ln/modules/nixos/filesystems.nix
@@ -1,7 +1,27 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (config.networking) hostName;
+in {
   system = {
     zfs.enable = true;
     impermanence.enable = lib.mkDefault true;
+  };
+
+  # WIP: temporary directories for users
+  fileSystems = {
+    "/home/michael" = {
+      device = "zroot/${hostName}/home/michael";
+      fsType = "zfs";
+      neededForBoot = true;
+    };
+    "/home/shawn" = {
+      device = "zroot/${hostName}/home/shawn";
+      fsType = "zfs";
+      neededForBoot = true;
+    };
   };
 
   swapDevices = [];
