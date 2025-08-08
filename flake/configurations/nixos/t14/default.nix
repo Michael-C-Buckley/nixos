30bfce3b782b702db7072fd100d253d43740ef65
Michael Buckley <michaelcbuckley@proton.me>
2025-08-07 22:06:29 -0400

    Added Mango to my laptop as well

--- flake/configurations/nixos/t14/default.nix
+++ flake/configurations/nixos/t14/default.nix
@@ -1,9 +1,10 @@
 # T14 Laptop Configuration
 {inputs, ...}: let
-  inherit (inputs) nix-secrets;
+  inherit (inputs) nix-secrets mangowc;
 in {
   imports = [
     nix-secrets.nixosModules.t14
+    mangowc.nixosModules.mango
     ./hardware
     ./networking
     ./systemd
@@ -14,6 +15,8 @@ in {
     podman.enable = true;
     libvirtd.enable = true;
   };
+
+  programs.mango.enable = true;
 
   security.tpm2.enable = true;
 
