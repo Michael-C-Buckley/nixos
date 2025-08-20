39eeba6597cdb34b83df1080bbf3251a6d3bd7e8
Michael Buckley <michaelcbuckley@proton.me>
2025-08-20 07:24:48 -0400

    Removed redundant Unbound interface and ACL entries

--- flake/modules/nixos/network/unbound.nix
+++ flake/modules/nixos/network/unbound.nix
@@ -10,8 +10,6 @@ in {
     # Keep non-sensitive settings here
     settings = {
       server = {
-        interface = mkDefault ["127.0.0.1" "::1"];
-        access-control = mkDefault ["127.0.0.0/8 allow" "::1/128 allow"];
         hide-identity = mkDefault "yes";
         hide-version = mkDefault "yes";
         verbosity = 0;
