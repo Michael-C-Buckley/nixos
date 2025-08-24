9870193d3a1a148a908024fb2e04347c619a83fc
Michael Buckley <michaelcbuckley@proton.me>
2025-08-23 21:26:54 -0400

    Streamlined basic program settings

--- flake/modules/nixos/programs/default.nix
+++ flake/modules/nixos/programs/default.nix
@@ -1,18 +1,24 @@
 {lib, ...}: let
   inherit (lib) mkDefault;
 in {
-  imports = [
-    ./direnv.nix
-    ./winbox.nix
-  ];
-
   programs = {
     fish.enable = true;
-    zsh.enable = true;
     neovim.defaultEditor = true;
+    zsh.enable = true;
+
     nh = {
       enable = true;
       flake = mkDefault "/etc/nixos";
+    };
+
+    direnv = {
+      enable = true;
+      silent = true;
+      nix-direnv.enable = true;
+      direnvrcExtra = ''
+        warn_timeout=0
+        hide_env_diff=true
+      '';
     };
   };
 }
