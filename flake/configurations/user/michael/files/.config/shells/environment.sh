b49258dcefc18723e65fc1c0f17372220925ad3c
Michael Buckley <michaelcbuckley@proton.me>
2025-08-24 11:53:03 -0400

    Set XDG user dirs

--- flake/configurations/user/michael/files/.config/shells/environment.sh
+++ flake/configurations/user/michael/files/.config/shells/environment.sh
@@ -24,3 +24,8 @@ export NH_FLAKE=/home/michael/nixos
 
 # GPG Terminal Compatibility for pinentry
 export GPG_TTY=$(tty)
+
+# XDG
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share" 
+export XDG_CACHE_HOME="$HOME/.cache"
