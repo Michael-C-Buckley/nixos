30ca0cae884ae1c4c81570307272c7e6268e4ee1
Michael Buckley <michaelcbuckley@proton.me>
2025-08-29 22:23:41 -0400

    Tweaked the system manifest to match the systems that exist

--- flake/configurations/nixos/default.nix
+++ flake/configurations/nixos/default.nix
@@ -8,6 +8,10 @@ _: {
       system = "x86_64-linux";
       configuration = ./p520;
     };
+    "ssk" = {
+      system = "x86_64-linux";
+      configuration = ./ssk;
+    };
     "t14" = {
       system = "x86_64-linux";
       configuration = ./t14;
@@ -16,23 +20,5 @@ _: {
       system = "x86_64-linux";
       configuration = ./x570;
     };
-
-    # Currently Unused Systems
-    # "ssk" = {
-    #   system = "x86_64-linux";
-    #   configuration = ./ssk;
-    # };
-    # "x370" = {
-    #   system = "x86_64-linux";
-    #   configuration = ./x370;
-    # };
-    # "sff" = {
-    #   system = "x86_64-linux";
-    #   configuration = ./sff;
-    # };
-    # "blade" = {
-    #   system = "x86_64-linux";
-    #   configuration = ./blade;
-    # };
   };
 }
