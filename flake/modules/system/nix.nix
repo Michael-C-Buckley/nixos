f4c0c5358e02f2e2bcef599076942d2cbb1515c3
Michael Buckley <michaelcbuckley@proton.me>
2025-09-07 10:29:05 -0400

    Removed unused microvm.nix cache

--- flake/modules/system/nix.nix
+++ flake/modules/system/nix.nix
@@ -41,11 +41,9 @@ in {
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://microvm.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
       ];
       trusted-users = [
         "@wheel"
