286b1825d8660c4d6ef05bed0b5d1d4719f7cc34
Michael Buckley <michaelcbuckley@proton.me>
2025-09-07 11:57:30 -0400

    First attempts at getting an operational cache

--- flake/modules/system/nix.nix
+++ flake/modules/system/nix.nix
@@ -44,6 +44,8 @@ in {
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # My lab build instance
+        "michael-hydra-1:i6EiwHcLtrM6EAdpeymEWqlWs9p15HVTCjS+Cs/cgH0="
       ];
       trusted-users = [
         "@wheel"
