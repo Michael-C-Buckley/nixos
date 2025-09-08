6f36cb597106fb6b7645c69cb38d1a1b308ae58f
Michael Buckley <michaelcbuckley@proton.me>
2025-09-07 22:16:40 -0400

    Tweaked hydra configs

--- flake/modules/services/hydra.nix
+++ flake/modules/services/hydra.nix
@@ -12,10 +12,25 @@ in {
 
   networking.firewall.allowedTCPPorts = mkIf hydra.enable [3000];
 
+  nix.settings.trusted-users = mkIf hydra.enable ["hydra" "hydra-queue-runner" "hydra-www"];
+
   services.hydra = {
     hydraURL = mkDefault "http://localhost:3000";
     notificationSender = mkDefault "hydra@localhost";
     buildMachinesFiles = mkDefault [];
     useSubstitutes = mkDefault true;
+    extraConfig = ''
+      allow-import-from-derivation = true
+
+      # Use substituters for build dependencies
+      use-substitutes = 1
+
+      # Timeout settings for substituters
+      connect-timeout = 60
+      stalled-download-timeout = 300
+
+      # Allow fetching from binary caches during builds
+      builders-use-substitutes = true
+    '';
   };
 }
