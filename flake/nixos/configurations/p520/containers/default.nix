07c45b666d0e7e77409539a3b75dfe4d4b16c17f
Michael Buckley <michaelcbuckley@proton.me>
2025-09-11 17:38:15 -0400

    Added quadlet with forgejo to P520

---
+++ flake/nixos/configurations/p520/containers/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./forgejo.nix
+  ];
+  virtualisation.quadlet.networks = {
+    br200.networkConfig = {
+      podmanArgs = ["--interface-name=br200"];
+      driver = "bridge";
+      disableDns = true;
+      gateways = ["192.168.53.1/26"];
+      subnets = ["192.168.53.0/26"];
+    };
+  };
+}
