22dbc06f8e2944e0340476eb21555552d48f51b2
Michael Buckley <michaelcbuckley@proton.me>
2025-09-10 23:17:02 -0400

    Added a basic theme setup for GTK using dconf

---
+++ flake/modules/programs/dconf.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config =
+    lib.mkIf config.system.preset
+    == "Desktop" {
+      environment.systemPackages = with pkgs; [
+        materia-theme
+        gruvbox-dark-icons-gtk
+      ];
+      programs.dconf.profiles.user.databases = [
+        {
+          lockAll = true;
+          settings = {
+            "org/gnome/desktop/interface" = {
+              gtk-theme = "Materia-dark";
+              icon-theme = "Gruvbox Dark";
+              font-name = "Sans Regular 11";
+              document-font-name = "Sans Regular 11";
+              monospace-font-name = "Monospace Regular 12";
+              color-scheme = "prefer-dark";
+            };
+          };
+        }
+      ];
+    };
+}
