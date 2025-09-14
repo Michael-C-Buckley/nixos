ea4cb8d6906e7e10b957fd1786a98c56baced2f1
Michael Buckley <michaelcbuckley@proton.me>
2025-09-14 08:49:02 -0400

    Swapped to dynamic logic for the devshell hydra jobs

--- outputs/hydraJobs.nix
+++ outputs/hydraJobs.nix
@@ -1,11 +1,15 @@
 {self, ...}: let
-  inherit (builtins) mapAttrs;
-  inherit (self) nixosConfigurations;
+  inherit (builtins) elem mapAttrs;
+  inherit (self) devShells nixosConfigurations;
+  inherit (self.inputs.nixpkgs.lib) filterAttrs;
   getConfig = host: self.nixosConfigurations.${host}.config.system.build.toplevel;
+
+  # Currently the only systems I use DevShells on
+  validSystems = ["x86_64-linux"];
 in {
   flake.hydraJobs = {
     # For now, only do the architectures I develop on
-    devShells = self.devShells."x86_64-linux";
+    devShells = filterAttrs (n: _: elem n validSystems) devShells;
 
     # Find and build all systems defined
     nixosConfigurations = mapAttrs (n: _: getConfig n) nixosConfigurations;
