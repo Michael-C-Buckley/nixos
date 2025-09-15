c18b5ce1a46debd2722018bb9fc41aa99f1bccd3
Michael Buckley <michaelcbuckley@proton.me>
2025-09-15 14:35:06 -0400

    Fixed the fish functions

--- flake/user/hjem/configs/fish.nix
+++ flake/user/hjem/configs/fish.nix
@@ -32,23 +32,19 @@ in {
     functions = {
       # Use FZF to navigate to a folder matching folders or filenames
       fcd = ''
-        function fcd
-            set -l selected_path (find . | fzf --height 40% --reverse)
+        set -l selected_path (find . | fzf --height 40% --reverse)
 
-            if test -n "$selected_path"
-                if test -d "$selected_path"
-                    cd "$selected_path"
-                else
-                    cd (dirname "$selected_path")
-                end
+        if test -n "$selected_path"
+            if test -d "$selected_path"
+                cd "$selected_path"
+            else
+                cd (dirname "$selected_path")
             end
         end
       '';
       # Passthrough of FRR show command
       show = ''
-        function show
-            sudo vtysh -c "show $argv"
-        end
+        sudo vtysh -c "show $argv"
       '';
     };
   };
