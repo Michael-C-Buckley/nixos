b19c82a17ceeb7ac56351d52829867bc8b6081a3
Michael Buckley <michaelcbuckley@proton.me>
2025-09-30 07:08:05 -0400

    Completed the components to get local LLMs working

--- flake/nixos/configurations/x570/containers/ipex-llm.nix
+++ flake/nixos/configurations/x570/containers/ipex-llm.nix
@@ -1,14 +1,26 @@
 {
-  virtualisation.quadlet.containers.ipex-llm.containerConfig = {
-    image = "intelanalytics/ipex-llm-inference-cpp-xpu:latest";
-    publishPorts = ["11434:11434"];
-    volumes = ["/var/lib/ipex/:/models"];
-    shmSize = "16G";
-    memory = "40G";
-    devices = ["/dev/dri"];
-    environments = {
-      DEVICE = "Arc";
-      no_proxy = "localhost,127.0.0.1";
+  virtualisation.quadlet = {
+    pods.all = {};
+    builds.ipex-llm.buildConfig.networks = ["host"];
+    containers.ipex-llm.containerConfig = {
+      image = "intelanalytics/ipex-llm-inference-cpp-xpu:latest";
+      exec = "ollama/ollama serve";
+      publishPorts = ["11434:11434"];
+      addHosts = ["host.containers.internal:host-gateway"];
+      volumes = [
+        "/var/lib/ipex/models:/models"
+        "/var/lib/ipex/ollama:/llm/ollama"
+      ];
+      shmSize = "16G";
+      memory = "40G";
+      devices = ["/dev/dri"];
+      environments = {
+        DEVICE = "Arc";
+        no_proxy = "localhost,127.0.0.1";
+        OLLAMA_NUM_GPU = "999";
+        ZES_ENABLE_SYSMAN = "1";
+        OLLAMA_HOST = "0.0.0.0:11434";
+      };
     };
   };
 
--- flake/nixos/configurations/x570/containers/open-webui.nix
+++ flake/nixos/configurations/x570/containers/open-webui.nix
@@ -1,10 +1,15 @@
 {
-  virtualisation.quadlet.containers.open-webui.containerConfig = {
-    image = "ghcr.io/open-webui/open-webui:main";
-    publishPorts = ["4400:8080"];
-    volumes = ["/var/lib/open-webui:/open-webui"];
-    environments = {
-      OLLAMA_BASE_URL = "https://127.0.0.1:11434";
+  virtualisation.quadlet = {
+    pods.all = {};
+    builds.open-webui.buildConfig.networks = ["host"];
+    containers.open-webui.containerConfig = {
+      image = "ghcr.io/open-webui/open-webui:main";
+      publishPorts = ["4400:8080"];
+      addHosts = ["host.containers.internal:host-gateway"];
+      volumes = ["/var/lib/open-webui:/open-webui"];
+      environments = {
+        OLLAMA_BASE_URL = "http://host.containers.internal:11434";
+      };
     };
   };
 
