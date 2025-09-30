5b67744980e67234ed384fcf3a74a4646eac12ee
Michael Buckley <michaelcbuckley@proton.me>
2025-09-30 10:07:16 -0400

    Added an additional directory that Ollama complained about not having

--- flake/nixos/configurations/p520/services/ollama.nix
+++ flake/nixos/configurations/p520/services/ollama.nix
@@ -1,6 +1,11 @@
 {config, ...}: {
   environment.persistence."/cache".directories = [
     config.services.ollama.home
+    {
+      directory = "/var/lib/private/ollama";
+      user = "ollama";
+      group = "ollama";
+    }
   ];
   services.ollama = {
     enable = true;
