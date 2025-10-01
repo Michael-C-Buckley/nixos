fc36d1c81b48e8569e21eb0a30208e2e6753de3d
Michael Buckley <michaelcbuckley@proton.me>
2025-09-30 21:42:54 -0400

    Ollama tweaks for T14

--- flake/nixos/configurations/t14/services/ollama.nix
+++ flake/nixos/configurations/t14/services/ollama.nix
@@ -1,8 +1,13 @@
 {
   # Persistent mount handled via filesystems
-  services.ollama = {
-    enable = true;
-    acceleration = "rocm";
-    #rocmOverrideGfx = "9.0.0";
+  services = {
+    ollama = {
+      enable = true;
+      # Skip for now
+      acceleration = false;
+      #rocmOverrideGfx = "9.0.0";
+    };
+    # Add a local UI for convenience
+    nextjs-ollama-llm-ui.enable = true;
   };
 }
