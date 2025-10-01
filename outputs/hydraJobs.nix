034e08398419e249ff067c7f4a552141cddd49b0
Michael Buckley <michaelcbuckley@proton.me>
2025-09-30 22:35:52 -0400

    Removed devshells in favor of a simple traditional nix shell

--- outputs/hydraJobs.nix
+++ outputs/hydraJobs.nix
@@ -1,16 +1,9 @@
 {self, ...}: let
-  inherit (builtins) elem mapAttrs;
-  inherit (self) devShells nixosConfigurations;
-  inherit (self.inputs.nixpkgs.lib) filterAttrs;
+  inherit (builtins) mapAttrs;
+  inherit (self) nixosConfigurations;
   getConfig = host: self.nixosConfigurations.${host}.config.system.build.toplevel;
-
-  # Currently the only systems I use DevShells on
-  validSystems = ["x86_64-linux"];
 in {
   flake.hydraJobs = {
-    # For now, only do the architectures I develop on
-    devShells = filterAttrs (n: _: elem n validSystems) devShells;
-
     # Find and build all systems defined
     nixosConfigurations = mapAttrs (n: _: getConfig n) nixosConfigurations;
   };
