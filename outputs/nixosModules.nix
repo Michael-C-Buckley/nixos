850ca1e1e3d9044b84c4843db78263a6887a2120
Michael Buckley <michaelcbuckley@proton.me>
2025-10-06 08:46:09 -0400

    Flattened the nixos module outputs because nested did not accomplish what I wanted

--- outputs/nixosModules.nix
+++ outputs/nixosModules.nix
@@ -1,22 +1,12 @@
 {
   flake.nixosModules = {
     network = ../flake/nixos/modules/network/_default.nix;
-    security = {
-      gpg = ../flake/nixos/modules/security/gpg.nix;
-    };
-    hardware = {
-      intelGraphics = ../flake/nixos/modules/hardware/intelGraphics.nix;
-    };
-    packages = {
-      default = ../flake/nixos/modules/packages/default.nix;
-    };
-    system = {
-      nix = ../flake/nixos/modules/system/nix.nix;
-    };
-    virtualization = {
-      containerlab = ../flake/nixos/modules/virtualization/containerlab.nix;
-      docker = ../flake/nixos/modules/virtualization/docker.nix;
-      libvirt = ../flake/nixos/modules/virtualization/libvirt.nix;
-    };
+    security-gpg = ../flake/nixos/modules/security/gpg.nix;
+    hardware-intelGraphics = ../flake/nixos/modules/hardware/intelGraphics.nix;
+    packages-default = ../flake/nixos/modules/packages/default.nix;
+    system-nix = ../flake/nixos/modules/system/nix.nix;
+    virtualization-containerlab = ../flake/nixos/modules/virtualization/containerlab.nix;
+    virtualization-docker = ../flake/nixos/modules/virtualization/docker.nix;
+    virtualization-libvirt = ../flake/nixos/modules/virtualization/libvirt.nix;
   };
 }
