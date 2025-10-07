a5d55b97086a385669eb749206b6567b6233a8c3
Michael Buckley <michaelcbuckley@proton.me>
2025-10-07 06:05:59 -0400

    Added missing persistence

--- flake/user/modules/gui/librewolf.nix
+++ flake/user/modules/gui/librewolf.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   environment.persistence."/persist".users.michael.directories = [
     ".cache/librewolf"
+    ".librewolf"
   ];
 
   users.users.michael.packages = [pkgs.librewolf];
