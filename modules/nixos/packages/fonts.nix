d8a975806ba0f75bf6846c349e35aa44e5866e21
Michael Buckley <michaelcbuckley@proton.me>
2025-10-09 17:58:20 -0400

    Converted packages and programs

--- modules/nixos/packages/fonts.nix
+++ modules/nixos/packages/fonts.nix
@@ -1,29 +1,16 @@
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  inherit (builtins) elem;
-  inherit (lib) optionals;
-  inherit (config.system) preset;
+  flake.modules.nixosModules.fonts = {pkgs, ...}: {
+    fonts.packages = with pkgs; [
+      jetbrains-mono
+      dejavu_fonts
+      vista-fonts
+      font-awesome
+      geist-font
+      cascadia-code
+      noto-fonts
 
-  useFonts = elem preset ["desktop" "laptop"];
-
-  extraFonts = with pkgs; [
-    dejavu_fonts
-    vista-fonts
-    font-awesome
-    geist-font
-    cascadia-code
-    noto-fonts
-
-    nerd-fonts.symbols-only
-    nerd-fonts.lilex
-  ];
-in {
-  fonts.packages =
-    # Unconditionally make Jetbrains Mono available for basic things like Ghostty
-    [pkgs.nerd-fonts.jetbrains-mono]
-    ++ optionals useFonts extraFonts;
+      nerd-fonts.symbols-only
+      nerd-fonts.lilex
+    ];
+  };
 }
--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,25 +1,21 @@
-{lib, ...}: let
-  inherit (lib) mkDefault;
-in {
-  programs = {
-    fish.enable = true;
-    vim.enable = true;
-    nano.enable = false;
-    neovim.defaultEditor = true;
+{
+  flake.modules.nixosModules.programs = {
+    programs = {
+      fish.enable = true;
+      vim.enable = true;
+      nano.enable = false;
+      neovim.defaultEditor = true;
+      nh.enable = true;
 
-    nh = {
-      enable = true;
-      flake = mkDefault "/etc/nixos";
-    };
-
-    direnv = {
-      enable = true;
-      silent = true;
-      nix-direnv.enable = true;
-      direnvrcExtra = ''
-        warn_timeout=0
-        hide_env_diff=true
-      '';
+      direnv = {
+        enable = true;
+        silent = true;
+        nix-direnv.enable = true;
+        direnvrcExtra = ''
+          warn_timeout=0
+          hide_env_diff=true
+        '';
+      };
     };
   };
 }
