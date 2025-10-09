dc5da9e7d38fe51f41b50be77111fedb16572f23
Michael Buckley <michaelcbuckley@proton.me>
2025-10-08 20:01:28 -0400

    Updated inputs except nixpkgs (broken due to cmake) and removed OMVF submodule (as it will be deprecated shortly)

--- flake/nixos/modules/virtualization/libvirt.nix
+++ flake/nixos/modules/virtualization/libvirt.nix
@@ -43,10 +43,6 @@ in {
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [pkgs.OVMFFull.fd];
-        };
       };
     };
 
