6195c62c96bc9715f84be6cda188fa5a038c854f
Michael Buckley <michaelcbuckley@proton.me>
2025-10-09 07:13:07 -0400

    Moved P520 to local DNS

--- flake/nixos/configurations/p520/networking/default.nix
+++ flake/nixos/configurations/p520/networking/default.nix
@@ -1,10 +1,13 @@
 {
   services = {
     iperf3.enable = true;
-    unbound.enable = true;
+    unbound.enable = false;
+    resolved.enable = false;
   };
 
   networking = {
+    nameservers = ["192.168.48.1" "192.168.48.31" "192.168.48.32" "192.168.48.33"];
+
     ospf.enable = true;
     hostName = "p520";
     hostId = "181a3ead";
