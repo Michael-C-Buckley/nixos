cfbd6d32b0212ec0ed7709f931e0240bbeda243b
Michael Buckley <michaelcbuckley@proton.me>
2025-10-18 07:28:11 -0400

    Moved hyprland and ghostty configs out of the implied links into my user configs

---
+++ modules/user/hjem/configs/ghostty.nix
@@ -0,0 +1,20 @@
+{
+  flake.module.nixos.hjem-ghostty = {
+    hjem.users.michael.rum.programs.ghostty = {
+      enable = true;
+      settings = {
+        theme = "wombat";
+        background = "#000000";
+        font-size = "11";
+        font-family = "Cascadia Code NF";
+
+        background-opacity = "0.5";
+        cursor-opacity = "0.6";
+        cursor-color = "#44A3A3";
+
+        window-theme = "system";
+        window-decoration = "false";
+      };
+    };
+  };
+}
