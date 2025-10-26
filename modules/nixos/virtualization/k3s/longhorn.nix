1d1e508faa49af453b3bdf30e172c784d9c60bb2
Michael Buckley <michaelcbuckley@proton.me>
2025-10-26 18:01:48 -0400

    Added a name to openiscsi

--- modules/nixos/virtualization/k3s/longhorn.nix
+++ modules/nixos/virtualization/k3s/longhorn.nix
@@ -1,6 +1,12 @@
 {
-  flake.modules.nixos.k3s-longhorn = {
+  flake.modules.nixos.k3s-longhorn = {config, ...}: let
+    inherit (config.system) stateVersion;
+    inherit (config.networking) hostName;
+  in {
     boot.kernelModules = ["iscsi_tcp" "iscsi_ibft"];
-    services.openiscsi.enable = true;
+    services.openiscsi = {
+      enable = true;
+      name = "iqn.nixos-${stateVersion}-${hostName}";
+    };
   };
 }
