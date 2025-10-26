8c674420480695233ced6f669be4887d059ac7aa
Michael Buckley <michaelcbuckley@proton.me>
2025-10-26 16:51:20 -0400

    Finally got the k3s cluster for UFFs functional

--- modules/clusters/uff/modules/k3s.nix
+++ modules/clusters/uff/modules/k3s.nix
@@ -2,22 +2,28 @@
   inherit (config.flake.modules.nixos) k3s;
 in {
   flake.modules.nixos.uff = {config, ...}: let
-    inherit (config) networking;
-
+    inherit (builtins) head split;
     # Get the first (and only) IPv4 set on the loopback interface
-    loopback = builtins.head networking.interfaces.lo.ipv4.addresses;
+    #loopback = (builtins.head networking.interfaces.lo.ipv4.addresses).address;
+    # loopback severely complicated L3 routing, so fallback to best common interface for now
+    enusb = head (split "/" (head config.networkd.enusb1.addresses.ipv4));
   in {
     imports = [k3s];
 
+    networking.hosts = {
+      "192.168.254.1" = ["uff1s"];
+      "192.168.254.2" = ["uff2s"];
+      "192.168.254.3" = ["uff3s"];
+    };
+
     services.k3s = {
       tokenFile = config.sops.secrets.k3s_token.path;
-
+      serverAddr = enusb;
       extraFlags = [
-        "--node-name ${networking.hostName}"
-        "--node-ip ${loopback.address}"
-        "--server https://192.168.61.1:6443"
-        "--server https://192.168.61.2:6443"
-        "--server https://192.168.61.3:6443"
+        "--node-name ${config.networking.hostName}s"
+        "--node-ip ${enusb}"
+        "--advertise-address ${enusb}"
+        "--server https://192.168.254.1:6443" # Serves as the bootstrapper
       ];
     };
   };
