450806c6b3613c315fbce5626835d5ee50243e92
Michael Buckley <michaelcbuckley@proton.me>
2025-10-27 10:56:26 -0400

    Swapped helix for evil-helix

--- modules/user/hjem/configs/helix.nix
+++ modules/user/hjem/configs/helix.nix
@@ -1,13 +1,16 @@
 {
-  flake.modules.nixos.hjem-helix = {
-    hjem.users.michael.rum.programs.helix = {
-      enable = true;
-      settings = {
-        theme = "ayu-dark";
-        editor.cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
+  flake.modules.nixos.hjem-helix = {pkgs, ...}: {
+    hjem.users.michael = {
+      packages = [pkgs.evil-helix];
+      rum.programs.helix = {
+        enable = true;
+        settings = {
+          theme = "ayu-dark";
+          editor.cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
         };
       };
     };
