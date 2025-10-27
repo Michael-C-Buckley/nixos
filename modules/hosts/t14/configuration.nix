2e7deaf4431ccc4e93b03397d3cb056b2de7d9f0
Michael Buckley <michaelcbuckley@proton.me>
2025-10-27 17:14:04 -0400

    Bumped the opacity of ghostty on t14

--- modules/hosts/t14/configuration.nix
+++ modules/hosts/t14/configuration.nix
@@ -10,5 +10,8 @@
     system.stateVersion = "24.11";
     nix.settings.trusted-substituters = ["http://192.168.48.5:5000"];
     virtualisation.podman.enable = true;
+
+    # Decrease Ghostty transparency a bit
+    hjem.users.michael.rum.programs.ghostty.settings.background-opacity = "0.7";
   };
 }
