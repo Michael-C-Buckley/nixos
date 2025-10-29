cea07c6b41c63633dc65dbfd95cf74bd6fa5941e
Michael Buckley <michaelcbuckley@proton.me>
2025-10-29 15:35:41 -0400

    Fixed if-else statements in routing modules and added nftables to the linux preset

--- modules/nixos/network/vrrp.nix
+++ modules/nixos/network/vrrp.nix
@@ -1,13 +1,19 @@
 # Currently completely broken on the FRR side
 # https://github.com/FRRouting/frr/issues/18379
 {
-  flake.modules.nixos.vrrp = {
+  flake.modules.nixos.vrrp = {config, ...}: {
     services.keepalived.enable = true;
-    networking.firewall.extraInputRules = ''
-      ip protocol 112 accept comment "Allow VRRP"
-    '';
-    networking.firewall.extraCommands = ''
-      iptables -A nixos-fw -p 112 -j ACCEPT -m comment --comment "Allow VRRP"
-    '';
+    networking.firewall =
+      if config.networking.nftables.enable
+      then {
+        extraInputRules = ''
+          ip protocol 112 accept comment "Allow VRRP"
+        '';
+      }
+      else {
+        extraCommands = ''
+          iptables -A nixos-fw -p 112 -j ACCEPT -m comment --comment "Allow VRRP"
+        '';
+      };
   };
 }
