e6b7bafbeea02ab3f07ff38b8f2f5324f6e7ab50
Michael Buckley <michaelcbuckley@proton.me>
2025-11-01 12:06:52 -0400

    Moved UFFs to dnscrypt as well

--- modules/clusters/uff/modules/network/default.nix
+++ modules/clusters/uff/modules/network/default.nix
@@ -15,20 +15,6 @@
         enable = true;
         openFirewall = true;
       };
-      # Allow local entities to use DNS
-      unbound.settings.server = {
-        access-control = [
-          "127.0.0.0/8 allow"
-          "::1/128 allow"
-          "192.168.48.0/20 allow"
-        ];
-        interface = [
-          "127.0.0.1"
-          "::1"
-          "192.168.61.0"
-          "192.168.52.1"
-        ];
-      };
     };
   };
 }
