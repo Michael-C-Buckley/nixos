27e089527440b61c137b00c3e61961f03240814d
Michael Buckley <michaelcbuckley@proton.me>
2025-11-01 22:56:38 -0400

    Shifted some devbox options to the preset

--- modules/nixos/presets/microvm.nix
+++ modules/nixos/presets/microvm.nix
@@ -2,9 +2,11 @@
   config,
   inputs,
   ...
-}: {
-  flake.modules.nixos.microvmPreset = {
-    imports = with config.flake.modules.nixos;
+}: let
+  inherit (config.flake) modules nixosConfigurations;
+in {
+  flake.modules.nixos.microvmPreset = {config, ...}: {
+    imports = with modules.nixos;
       [
         linuxPreset
         users
@@ -14,9 +16,40 @@
         inputs.nix-secrets.nixosModules.michael
       ];
 
+    microvm = {
+      hypervisor = "qemu";
+      socket = "control.socket";
+      qemu.extraArgs = [
+        "-cpu"
+        "host"
+        "-smp"
+        (toString config.microvm.vcpu)
+      ];
+
+      shares = [
+        {
+          proto = "virtiofs";
+          tag = "ro-store";
+          source = "/nix/store";
+          mountPoint = "/nix/.ro-store";
+        }
+      ];
+    };
+
+    networking = {
+      useNetworkd = true;
+
+      nameservers = with config.networking; [
+        defaultGateway.address
+        defaultGateway6.address
+      ];
+    };
+
+    services.openssh.enable = true;
+
     system = {
       # Track my main systems, to which my desktop would be good
-      inherit (config.flake.nixosConfigurations.x570.config.system) stateVersion;
+      inherit (nixosConfigurations.x570.config.system) stateVersion;
     };
   };
 }
