2a914ae9fb8434557c78ab360b6f9185a48f2fa7
Michael Buckley <michaelcbuckley@proton.me>
2025-11-02 20:30:08 -0500

    Added wifi module to UFFs

--- modules/clusters/uff/modules/common.nix
+++ modules/clusters/uff/modules/common.nix
@@ -1,5 +1,9 @@
-{
+{config, ...}: {
   flake.modules.nixos.uff = {lib, ...}: {
+    imports = [
+      config.flake.modules.nixos.wifi
+    ];
+
     # Try my local cache first
     nix.settings.substituters = lib.mkBefore [
       "http://192.168.48.5:5000"
