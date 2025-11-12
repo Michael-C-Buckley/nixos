7a65946f7ebb517bf7de6da960f2209b6689692e
Michael Buckley <michaelcbuckley@proton.me>
2025-11-11 20:40:11 -0500

    Fixed VRRP subnet mask

--- modules/clusters/uff/modules/network/vrrp.nix
+++ modules/clusters/uff/modules/network/vrrp.nix
@@ -11,7 +11,7 @@
         state = lib.mkDefault "BACKUP";
         interface = "wlp2s0";
         virtualRouterId = 2;
-        virtualIps = [{addr = "172.16.248.30/24";} {addr = "172.30.248.30/24";}];
+        virtualIps = [{addr = "172.16.248.30/16";} {addr = "172.30.248.30/16";}];
       };
     };
   };
