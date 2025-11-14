270c30334ad21c3a4764887478f4a58943ac4ac8
Michael Buckley <michaelcbuckley@proton.me>
2025-11-13 22:32:16 -0500

    Removed vscode extensions from the declarative portion; will be moving to ms sync instead

--- modules/packages/vscode.nix
+++ modules/packages/vscode.nix
@@ -1,12 +1,13 @@
+# TODO:
+# I use regular vscode and across multiple systems, so use the official sync
+# Dealing with declarative extensions has been a large pain
 {inputs, ...}: {
   perSystem = {pkgs, ...}: let
     # Allow unfree and extend with the extensions overlay
-    pkgs' =
-      (import inputs.nixpkgs {
-        localSystem = pkgs.stdenv.hostPlatform;
-        config.allowUnfree = true;
-      }).extend
-      inputs.nix-vscode-extensions.overlays.default;
+    pkgs' = import inputs.nixpkgs {
+      localSystem = pkgs.stdenv.hostPlatform;
+      config.allowUnfree = true;
+    };
 
     # These are placed in the path of the program
     wrappedInputs = with pkgs'; [
@@ -25,60 +26,55 @@
       go-tools
       golangci-lint
     ];
-
-    vscodeExtensions = with pkgs'.vscode-marketplace-release; [
-      # Trusted Sources
-
-      # Microsoft Official
-      ms-vscode-remote.remote-ssh-edit
-      ms-vscode-remote.remote-ssh
-      ms-python.vscode-pylance
-      ms-vsliveshare.vsliveshare
-      ms-kubernetes-tools.vscode-kubernetes-tools
-      ms-toolsai.jupyter
-      ms-azuretools.vscode-containers
-      ms-vscode-remote.remote-containers
-
-      # Github Tools
-      github.copilot
-      github.copilot-chat
-      github.vscode-github-actions
-
-      # Language
-      ms-python.python
-      ms-python.debugpy
-      redhat.vscode-yaml
-      redhat.vscode-xml
-      redhat.ansible
-      golang.go
-
-      # Themes
-      ms-vscode.theme-predawnkit
-      github.github-vscode-theme
-
-      sourcegraph.amp
-
-      # Unofficial Sources
-
-      # Themes
-      yummygum.city-lights-theme
-      teabyii.ayu
-
-      # Languages
-      bbenoist.nix
-
-      # QOL/usability
-      mkhl.direnv
-      asvetliakov.vscode-neovim
-    ];
+    # vscodeExtensions = with pkgs'.vscode-marketplace-release; [
+    #   # Trusted Sources
+    #
+    #   # Microsoft Official
+    #   ms-vscode-remote.remote-ssh-edit
+    #   ms-vscode-remote.remote-ssh
+    #   ms-python.vscode-pylance
+    #   ms-vsliveshare.vsliveshare
+    #   ms-kubernetes-tools.vscode-kubernetes-tools
+    #   ms-toolsai.jupyter
+    #   ms-azuretools.vscode-containers
+    #   ms-vscode-remote.remote-containers
+    #
+    #   # Github Tools
+    #   github.copilot
+    #   github.copilot-chat
+    #   github.vscode-github-actions
+    #
+    #   # Language
+    #   ms-python.python
+    #   ms-python.debugpy
+    #   redhat.vscode-yaml
+    #   redhat.vscode-xml
+    #   redhat.ansible
+    #   golang.go
+    #
+    #   # Themes
+    #   ms-vscode.theme-predawnkit
+    #   github.github-vscode-theme
+    #
+    #   sourcegraph.amp
+    #
+    #   # Unofficial Sources
+    #
+    #   # Themes
+    #   yummygum.city-lights-theme
+    #   teabyii.ayu
+    #
+    #   # Languages
+    #   bbenoist.nix
+    #
+    #   # QOL/usability
+    #   mkhl.direnv
+    #   asvetliakov.vscode-neovim
+    # ];
   in {
     packages.vscode = pkgs.symlinkJoin {
       name = "code";
-      paths = [
-        (pkgs'.vscode-with-extensions.override {
-          inherit vscodeExtensions;
-        })
-      ];
+      paths = [pkgs'.vscode-fhs];
       buildInputs = wrappedInputs;
       nativeBuildInputs = [pkgs.makeWrapper];
       postBuild = ''
