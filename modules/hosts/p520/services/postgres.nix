57bc4acaaa4061a87c4c5977ff2c212157724010
Michael Buckley <michaelcbuckley@proton.me>
2025-11-19 08:32:41 -0500

    P520/Open-WebUI: Added initial deployment for local Open-WebUI

--- modules/hosts/p520/services/postgres.nix
+++ modules/hosts/p520/services/postgres.nix
@@ -3,7 +3,7 @@
     # Data directory is a ZFS dataset: zroot/p520/postgres
     services.postgresql = {
       enable = true;
-      ensureDatabases = ["hydra" "atticd" "forgejo"];
+      ensureDatabases = ["hydra" "atticd" "forgejo" "openwebui"];
       ensureUsers = [
         {
           name = "hydra";
@@ -18,6 +18,10 @@
           name = "forgejo";
           ensureDBOwnership = true;
         }
+        {
+          name = "openwebui";
+          ensureDBOwnership = true;
+        }
       ];
       # Allow connections from pods (K3s default pod network)
       settings = {
@@ -25,7 +29,12 @@
       };
       authentication = ''
         host forgejo forgejo 10.42.0.0/16 trust
+        host openwebui openwebui 10.42.0.0/16 trust
       '';
     };
+
+    networking.firewall.extraInputRules = ''
+      ip saddr { 127.0.0.1, 10.42.0.0/16 } tcp dport 5432 accept comment "Allow PostgreSQL from localhost and k3s pods"
+    '';
   };
 }
