73f90583d2f0abf1e74f12a17d750796ba4047ad
Michael Buckley <michaelcbuckley@proton.me>
2025-11-20 07:50:04 -0500

    P520/k3s: fixed the declarative k3s manifests properly building

--- modules/hosts/p520/k3s/k3s.nix
+++ modules/hosts/p520/k3s/k3s.nix
@@ -1,6 +1,12 @@
 # Nix-related elements for K3s
 {config, ...}: {
-  flake.modules.nixos.p520 = {
+  flake.modules.nixos.p520 = {pkgs, ...}: let
+    buildManifest = path:
+      pkgs.runCommand "${path}-manifests" {} ''
+        mkdir -p $out
+        ${pkgs.kustomize}/bin/kustomize build ${./${path}} > $out/manifest.yaml
+      '';
+  in {
     imports = [
       config.flake.modules.nixos.k3s
     ];
@@ -19,9 +25,12 @@
       30800 # Open WebUI HTTP
     ];
 
+    # This section merges and flattens the components into a single manifest per app
+    # for example, to view it run:
+    # nix build --no-link --print-out-paths '.#nixosConfigurations.p520.config.services.k3s.manifests.forgejo.source' 2>&1 | tail -1 | read -l output; and cat $output/manifest.yaml
     services.k3s.manifests = {
-      open-webui.source = ./open-webui/kustomization.yaml;
-      forgejo.source = ./forgejo/kustomization.yaml;
+      open-webui.source = buildManifest "open-webui";
+      forgejo.source = buildManifest "forgejo";
     };
   };
 }
