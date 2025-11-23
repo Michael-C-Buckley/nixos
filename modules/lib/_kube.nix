3115313b78aed8daad44128e50ca29a5f51f01b5
Michael Buckley <michaelcbuckley@proton.me>
2025-11-22 19:32:18 -0500

    P520/k3s-attic: fixed the auto-import error for the self-produced image

--- modules/lib/_kube.nix
+++ modules/lib/_kube.nix
@@ -4,4 +4,23 @@
     pkgs.runCommand "${path}-manifests" {} ''
       ${pkgs.kustomize}/bin/kustomize build ${path} > $out
     '';
+
+  # Build an OCI image for k3s with the correct localhost/ prefix
+  # This ensures k3s imports it with the expected name
+  buildK3sImage = {
+    pkgs,
+    name,
+    tag ? "latest",
+    contents ? [],
+    config ? {},
+  }:
+    pkgs.dockerTools.buildImage {
+      name = "localhost/${name}";
+      inherit tag config;
+      copyToRoot = pkgs.buildEnv {
+        name = "${name}-image-root";
+        paths = contents;
+        pathsToLink = ["/bin"];
+      };
+    };
 }
