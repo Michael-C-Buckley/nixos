03e4b3c10e3ef82a0596564d58425d742569bfdb
Michael Buckley <michaelcbuckley@proton.me>
2025-11-23 08:32:16 -0500

    Wrapped/Fish: migrated to lassulus/wrappers for fish

--- modules/user/wrapped/fish/fish.nix
+++ modules/user/wrapped/fish/fish.nix
@@ -1,7 +1,4 @@
-# My fish configuration pre-bundled with starship and configs for both
-# NOTICE: Apparently using this with NixOS has some mechanism replacing the starship path,
-#  thus that is wrapped and explicitly added on its own as well
-{
+{inputs, ...}: {
   perSystem = {
     pkgs,
     lib,
@@ -9,23 +6,27 @@
   }: let
     config = import ./_config.nix {inherit pkgs lib;};
   in {
-    packages.fish = pkgs.symlinkJoin {
-      name = "fish";
-      paths = [pkgs.fish];
-      buildInputs = with pkgs; [
+    packages.fish = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.fish;
+      runtimeInputs = with pkgs; [
         bat
         eza
         fd
         fzf
+        jq
         starship
       ];
-      nativeBuildInputs = [pkgs.makeWrapper];
-      postBuild = ''
-        wrapProgram $out/bin/fish \
-          --add-flags "--init-command 'source ${config}'" \
-          --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.starship]}
-      '';
-      passthru.shellPath = "/bin/fish";
+      flagSeparator = "=";
+      flags = {
+        "--init-command" = "source ${config}";
+      };
+      env = {
+        NH_FLAKE = "/home/michael/nixos";
+      };
+      passthru = {
+        shellPath = "/bin/fish";
+      };
     };
   };
 }
