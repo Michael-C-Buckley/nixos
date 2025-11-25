78953cc956bb2b252052a484ecd5f65a0a52ce78
Michael Buckley <michaelcbuckley@proton.me>
2025-11-25 07:20:54 -0500

    Darwin: added the new locally packaged ghostty to darwin packages

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -4,12 +4,13 @@
   ...
 }: {
   flake.modules.darwin.packages = {pkgs, ...}: let
-    inherit (config.flake.packages.aarch64-darwin) nvf vscode ns;
+    inherit (config.flake.packages.aarch64-darwin) ghostty-dmg ns nvf vscode;
   in {
     environment.systemPackages =
       [
         # Ensure we can rebuild
         inputs.nix-darwin.packages.aarch64-darwin.default
+        ghostty-dmg
         ns
         nvf
         vscode
