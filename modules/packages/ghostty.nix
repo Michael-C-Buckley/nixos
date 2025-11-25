0a679e0a6f8c403904757a68457ec93b8e760a60
Michael Buckley <michaelcbuckley@proton.me>
2025-11-25 07:35:46 -0500

    Flake/Packages: added filters to packages to not package inappropriate systems

--- modules/packages/ghostty.nix
+++ modules/packages/ghostty.nix
@@ -1,33 +1,37 @@
-# Add Ghostty's official MacOS binary since the nixpkgs source-based version is currently
-# broken, this does populate all systems but obviously won't work on Linux
-# I may change the logic, if I am less lazy
+# Add Ghostty's official MacOS binary since the nixpkgs source-based version is currently broken
 {self, ...}: {
-  perSystem = {pkgs, ...}: let
+  perSystem = {
+    pkgs,
+    system,
+    lib,
+    ...
+  }: let
     source = (pkgs.callPackage "${self}/_sources/generated.nix" {}).ghostty-dmg;
     inherit (source) pname version src;
-  in {
-    packages.ghostty-dmg = pkgs.stdenv.mkDerivation {
-      inherit pname version src;
+  in
+    lib.optionalAttrs (!lib.hasSuffix "linux" system) {
+      packages.ghostty-dmg = pkgs.stdenv.mkDerivation {
+        inherit pname version src;
 
-      unpackPhase = ''
-        undmg "$src" || 7zz x -snld "$src"
-      '';
+        unpackPhase = ''
+          undmg "$src" || 7zz x -snld "$src"
+        '';
 
-      installPhase = ''
-        mkdir -p $out/Applications
-        cp -r Ghostty.app $out/Applications/
+        installPhase = ''
+          mkdir -p $out/Applications
+          cp -r Ghostty.app $out/Applications/
 
-        mkdir -p $out/bin
-        makeWrapper $out/Applications/Ghostty.app/Contents/MacOS/ghostty $out/bin/ghostty-dmg
-      '';
+          mkdir -p $out/bin
+          makeWrapper $out/Applications/Ghostty.app/Contents/MacOS/ghostty $out/bin/ghostty-dmg
+        '';
 
-      nativeBuildInputs = with pkgs; [
-        _7zz
-        makeWrapper
-        undmg
-      ];
+        nativeBuildInputs = with pkgs; [
+          _7zz
+          makeWrapper
+          undmg
+        ];
 
-      sourceRoot = ".";
+        sourceRoot = ".";
+      };
     };
-  };
 }
