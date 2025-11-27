9d921852021024668964cb8297362550798dc9ac
Michael Buckley <michaelcbuckley@proton.me>
2025-11-27 10:30:35 -0500

    UFF/Networking: removed custom loopback reference, instead uses flake hosts reference

--- modules/clusters/uff/uff1/network.nix
+++ modules/clusters/uff/uff1/network.nix
@@ -1,24 +1,7 @@
-{config, ...}: let
-  inherit (config.flake.hosts.uff1.interfaces) lo eno1 enusb1;
-in {
+{
   flake.modules.nixos.uff1 = {
     custom.enusb1.mac = "6c:1f:f7:06:27:8e";
     networking = {
-      interfaces = {
-        enusb1.ipv4.addresses = [
-          {
-            address = enusb1.ipv4;
-            prefixLength = 28;
-          }
-        ];
-        eno1.ipv4.addresses = [
-          {
-            address = eno1.ipv4;
-            prefixLength = 24;
-          }
-        ];
-      };
-
       networkmanager.ensureProfiles.profiles = {
         home = {
           ipv4.address = "172.16.248.31/16";
@@ -30,7 +13,6 @@ in {
         };
       };
 
-      loopback.ipv4 = lo.ipv4;
       hostId = "ab0406ca";
       hostName = "uff1";
 
--- modules/clusters/uff/uff3/network.nix
+++ modules/clusters/uff/uff3/network.nix
@@ -1,24 +1,7 @@
-{config, ...}: let
-  inherit (config.flake.hosts.uff3.interfaces) lo eno1 enusb1;
-in {
+{
   flake.modules.nixos.uff3 = {
     custom.enusb1.mac = "6c:1f:f7:06:13:8f";
     networking = {
-      interfaces = {
-        enusb1.ipv4.addresses = [
-          {
-            address = enusb1.ipv4;
-            prefixLength = 28;
-          }
-        ];
-        eno1.ipv4.addresses = [
-          {
-            address = eno1.ipv4;
-            prefixLength = 24;
-          }
-        ];
-      };
-
       networkmanager.ensureProfiles.profiles = {
         home = {
           ipv4.address = "172.16.248.33/16";
@@ -30,7 +13,6 @@ in {
         };
       };
 
-      loopback.ipv4 = lo.ipv4;
       hostId = "f303a8e8";
       hostName = "uff3";
 
