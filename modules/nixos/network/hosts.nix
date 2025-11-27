6f141214c54388c326d94f5bc194c4f4b17bd596
Michael Buckley <michaelcbuckley@proton.me>
2025-11-27 10:50:23 -0500

    Network/Hosts: made the `/etc/hosts` file be populated from flake hosts

--- modules/nixos/network/hosts.nix
+++ modules/nixos/network/hosts.nix
@@ -1,4 +1,6 @@
-{
+{config, ...}: let
+  inherit (config.flake) hosts;
+in {
   flake.modules.nixos.network = {
     networking.hosts = {
       "192.168.48.1" = ["mt3" "mt3.michael.lan"];
@@ -6,24 +8,25 @@
       "192.168.48.3" = ["r8200" "r8200.michael.lan"];
 
       "192.168.48.20" = ["m1" "m1.michael.lan"];
-      "192.168.63.10" = ["x570" "x570.michael.lan"];
+      "${hosts.x570.interfaces.lo.ipv4}" = ["x570" "x570.michael.lan"];
 
-      "192.168.63.5" = ["p520" "p520.michael.lan"];
-      "192.168.49.5" = [
+      "${hosts.p520.interfaces.lo.ipv4}" = [
+        "p520"
+        "p520.michael.lan"
         "git.p520.michael.lan"
         "owi.p520.michael.lan"
       ];
 
       # 2.5GbE
-      "192.168.61.145" = ["uff1s"];
-      "192.168.61.146" = ["uff2s"];
-      "192.168.61.147" = ["uff3s"];
+      "${hosts.uff1.interfaces.enusb1.ipv4}" = ["uff1s"];
+      "${hosts.uff2.interfaces.enusb1.ipv4}" = ["uff2s"];
+      "${hosts.uff3.interfaces.enusb1.ipv4}" = ["uff3s"];
 
       # Loopbacks
       "192.168.61.0" = ["uff" "uff.michael.lan"];
-      "192.168.61.1" = ["uff1" "uff1.michael.lan"];
-      "192.168.61.2" = ["uff2" "uff2.michael.lan"];
-      "192.168.61.3" = ["uff3" "uff3.michael.lan"];
+      "${hosts.uff1.interfaces.lo.ipv4}" = ["uff1" "uff1.michael.lan"];
+      "${hosts.uff2.interfaces.lo.ipv4}" = ["uff2" "uff2.michael.lan"];
+      "${hosts.uff3.interfaces.lo.ipv4}" = ["uff3" "uff3.michael.lan"];
     };
   };
 }
