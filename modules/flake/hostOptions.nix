c87fbc6db8127b237451c537b4c2b1adea73cbb1
Michael Buckley <michaelcbuckley@proton.me>
2025-11-27 09:44:42 -0500

    Flake/Hosts: added IP references for the various hosts within the flake

--- modules/flake/hostOptions.nix
+++ modules/flake/hostOptions.nix
@@ -1,7 +1,7 @@
 # Options used in NixOS hosts
 {lib, ...}: let
   inherit (lib) mkOption;
-  inherit (lib.types) enum;
+  inherit (lib.types) enum str nullOr attrsOf submodule listOf;
 in {
   options.host = {
     bootloader = mkOption {
@@ -9,6 +9,40 @@ in {
       type = enum ["systemd-boot" "grub" "limine" "none"];
       default = "systemd-boot";
       description = "Which bootloader flake module to use with the host.";
+    };
+
+    hosts = mkOption {
+      type = attrsOf (submodule {
+        options = {
+          aliases = mkOption {
+            type = listOf str;
+            default = [];
+            description = "Additional hostnames/aliases for this host.";
+          };
+
+          interfaces = mkOption {
+            type = attrsOf (submodule {
+              options = {
+                ipv4 = mkOption {
+                  type = nullOr str;
+                  default = null;
+                  description = "IPv4 address for this interface.";
+                };
+
+                ipv6 = mkOption {
+                  type = nullOr str;
+                  default = null;
+                  description = "IPv6 address for this interface.";
+                };
+              };
+            });
+            default = {};
+            description = "Interface-specific IP addresses.";
+          };
+        };
+      });
+      default = {};
+      description = "Host IP mappings for hosts file generation and routing configuration.";
     };
   };
 }
