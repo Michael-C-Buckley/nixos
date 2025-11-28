208eb0a2b58113717abb510f15f1f40da8672e52
Michael Buckley <michaelcbuckley@proton.me>
2025-11-28 09:12:28 -0500

    DNS: deprecate unbound and resolved

--- modules/nixos/network/dns/dnscrypt-proxy2.nix
+++ modules/nixos/network/dns/dnscrypt-proxy2.nix
@@ -14,43 +14,38 @@ in {
       DynamicUser = lib.mkForce false;
     };
 
-    services = {
-      resolved.enable = false;
-      unbound.enable = false;
+    services.dnscrypt-proxy = {
+      enable = true;
+      settings = {
+        # Listen on localhost for DNS queries
+        listen_addresses = ["127.0.0.1:53" "[::1]:53"];
 
-      dnscrypt-proxy = {
-        enable = true;
-        settings = {
-          # Listen on localhost for DNS queries
-          listen_addresses = ["127.0.0.1:53" "[::1]:53"];
+        # Use servers that support DNSCrypt, DOH, and Anonymized DNS
+        server_names = ["cloudflare" "quad9-dnscrypt-ip4-filter-pri"];
 
-          # Use servers that support DNSCrypt, DOH, and Anonymized DNS
-          server_names = ["cloudflare" "quad9-dnscrypt-ip4-filter-pri"];
+        # Enable DNS-over-HTTPS
+        doh_servers = true;
 
-          # Enable DNS-over-HTTPS
-          doh_servers = true;
+        # Require DNSSEC validation
+        require_dnssec = true;
+        require_nolog = true;
+        require_nofilter = false; # Allow filtering servers
 
-          # Require DNSSEC validation
-          require_dnssec = true;
-          require_nolog = true;
-          require_nofilter = false; # Allow filtering servers
+        # Caching
+        cache = true;
+        cache_size = 4096;
+        cache_min_ttl = 2400;
+        cache_max_ttl = 86400;
 
-          # Caching
-          cache = true;
-          cache_size = 4096;
-          cache_min_ttl = 2400;
-          cache_max_ttl = 86400;
-
-          # Public blocklists
-          sources.public-resolvers = {
-            urls = [
-              "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-              "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-            ];
-            cache_file = "${dnsDir}/public-resolvers.md";
-            minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-            refresh_delay = 72;
-          };
+        # Public blocklists
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "${dnsDir}/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          refresh_delay = 72;
         };
       };
     };
