df309b9f8bb3347bcfc012469f095b991b843b08
Michael Buckley <michaelcbuckley@proton.me>
2025-11-28 20:07:55 -0500

    X570/Secrets: transitioned secrets into this flake

---
+++ modules/hosts/x570/secrets.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules.nixos.x570 = {config, ...}: let
+    wgSecret = name: {
+      sopsFile = "/etc/secrets/wireguard/${name}.sops";
+      format = "binary";
+    };
+  in {
+    sops.secrets =
+      {
+        # TODO: Not sure if I still want to keep using this
+        # Public -- x570-1:b3fLRrQyBeUbmpS+AGi68O1L2F1kSLEVX2ePAyDPNWk=
+        cachePrivateKey.sopsFile = "/etc/secrets/x570.yaml";
+        pam_u2f_auth.sopsFile = "/etc/secrets/x570.yaml";
+      }
+      # Wireguards - stored as separate binary files for convenience
+      // builtins.listToAttrs (map (n: {
+        name = "wireguard-${n}";
+        value = wgSecret n;
+      }) ["k1" "mt1" "mt4"]);
+
+    environment.etc = {
+      "wireguard/k1.conf".source = config.sops.secrets.wireguard-k1.path;
+    };
+
+    security.pam.u2f.settings.authfile = config.sops.secrets.pam_u2f_auth.path;
+  };
+}
