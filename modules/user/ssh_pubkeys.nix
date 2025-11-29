78ff958fbfe855606fa8e4eb106a9c96174ef7af
Michael Buckley <michaelcbuckley@proton.me>
2025-11-28 20:15:31 -0500

    Users/SSH: added Shawn to key files

--- modules/user/ssh_pubkeys.nix
+++ modules/user/ssh_pubkeys.nix
@@ -2,18 +2,26 @@
 # to share between NixOS and Darwin
 {
   flake.hjemConfig.ssh-pubkeys = {
-    users.users.michael.openssh.authorizedKeys.keys = [
-      "/etc/secrets/michael/ssh/gpg-573.pub"
-      "/etc/secrets/michael/ssh/gpg-902.pub"
-      "/etc/secrets/michael/ssh/gpg-074.pub"
-      "/etc/secrets/michael/ssh/gpg-870.pub"
-      "/etc/secrets/michael/ssh/sk-902.pub"
-      "/etc/secrets/michael/ssh/sk-573.pub"
-      "/etc/secrets/michael/ssh/sk-074.pub"
-      "/etc/secrets/michael/ssh/sk-870.pub"
-      "/etc/secrets/michael/ssh/piv-902.pub"
-      "/etc/secrets/michael/ssh/piv-573.pub"
-      "/etc/secrets/michael/ssh/piv-074.pub"
-    ];
+    users.users = {
+      michael.openssh.authorizedKeys.keyFiles = [
+        "/etc/secrets/michael/ssh/gpg-573.pub"
+        "/etc/secrets/michael/ssh/gpg-902.pub"
+        "/etc/secrets/michael/ssh/gpg-074.pub"
+        "/etc/secrets/michael/ssh/gpg-870.pub"
+        "/etc/secrets/michael/ssh/sk-902.pub"
+        "/etc/secrets/michael/ssh/sk-573.pub"
+        "/etc/secrets/michael/ssh/sk-074.pub"
+        "/etc/secrets/michael/ssh/sk-870.pub"
+        "/etc/secrets/michael/ssh/piv-902.pub"
+        "/etc/secrets/michael/ssh/piv-573.pub"
+        "/etc/secrets/michael/ssh/piv-074.pub"
+      ];
+      # TODO: Move to own module
+      shawn.openssh.authorizedKeys.keyFiles = [
+        "/etc/secrets/shawn/ssh/t14.pub"
+        "/etc/secrets/shawn/ssh/gpg1.pub"
+        "/etc/secrets/shawn/ssh/gpg2.pub"
+      ];
+    };
   };
 }
