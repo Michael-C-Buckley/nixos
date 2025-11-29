a47a0b4adfc555000a74b6e9dd933de9798fee1c
Michael Buckley <michaelcbuckley@proton.me>
2025-11-29 09:54:53 -0500

    O1/Secrets: converted to new standard

---
+++ modules/hosts/o1/networking/acme.nix
@@ -0,0 +1,17 @@
+{
+  flake.modules.nixos.o1 = {
+    security.acme = {
+      acceptTerms = true;
+      defaults = {
+        email = "michaelcbuckley@proton.me";
+        dnsProvider = "cloudflare";
+        environmentFile = "/run/secrets/groovyreserveToken";
+        webroot = null;
+      };
+      certs."groovyreserve.com" = {
+        dnsPropagationCheck = true;
+        domain = "groovyreserve.com";
+      };
+    };
+  };
+}
