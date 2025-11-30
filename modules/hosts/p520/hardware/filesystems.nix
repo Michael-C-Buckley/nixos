9bdcbef1798c203dda10c157d31c4baf198f0e5f
Michael Buckley <michaelcbuckley@proton.me>
2025-11-30 15:52:16 -0500

    P520/ZFS: added sanoid, syncoid, and CML ZFS settings

--- modules/hosts/p520/hardware/filesystems.nix
+++ modules/hosts/p520/hardware/filesystems.nix
@@ -1,16 +1,33 @@
-{
-  flake.modules.nixos.p520 = let
-    zfsFs = device: {
-      inherit device;
-      fsType = "zfs";
-      neededForBoot = true;
-    };
-  in {
+let
+  pushToHDD = name: {
+    source = "zroot/p520/${name}";
+    target = "zhdd/backup/p520/${name}";
+    recvOptions = "o compression=zstd";
+  };
+  zfsFs = device: {
+    inherit device;
+    fsType = "zfs";
+    neededForBoot = true;
+  };
+in {
+  flake.modules.nixos.p520 = {
     boot.zfs.extraPools = ["zhdd"];
 
-    environment.persistence."/persist".directories = [
-      "/var/lib/quadlet"
-    ];
+    services = {
+      sanoid.datasets = {
+        "zroot/p520/persist".useTemplate = ["short"];
+        "zroot/p520/postgres".useTemplate = ["short"];
+        "zhdd/backup/p520/persist".useTemplate = ["normal"];
+        "zhdd/backup/p520/postgres".useTemplate = ["normal"];
+      };
+      syncoid = {
+        enable = true;
+        commands = {
+          "persist-backup" = pushToHDD "persist";
+          "postgres-backup" = pushToHDD "postgres";
+        };
+      };
+    };
 
     fileSystems = {
       "/boot" = {
@@ -23,7 +40,7 @@
         fsType = "tmpfs";
         options = [
           "defaults"
-          "size=40G"
+          "size=40G" # Big because it's a builder
           "mode=755"
         ];
       };
