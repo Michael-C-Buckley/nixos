b6fa61b7ec1c0d6530f6590aceed12ba7d0e5f7c
Michael Buckley <michaelcbuckley@proton.me>
2025-12-02 19:56:34 -0500

    Nebula/main: implemented the 'main' nebula network on O1, X570, and T14

--- modules/nixos/network/nebula/main.nix
+++ modules/nixos/network/nebula/main.nix
@@ -1,13 +1,38 @@
 {
-  flake.modules.nixos.nebula-main = {lib, ...}: {
-    services.nebula = {
-      enable = true;
+  flake.modules.nixos.nebula-main = {
+    config,
+    lib,
+    ...
+  }: {
+    sops.secrets = {
+      "nebula/main/main-ca" = {
+        sopsFile = "/etc/secrets/common/common.yaml";
+        owner = "nebula-main";
+        group = "nebula-main";
+        mode = "0640";
+      };
+      # These two are taken care of by the end-host
+      "nebula/main/key" = {
+        owner = "nebula-main";
+        group = "nebula-main";
+        mode = "0640";
+      };
+      "nebula/main/crt" = {
+        owner = "nebula-main";
+        group = "nebula-main";
+        mode = "0640";
+      };
+    };
+
+    services.nebula = let
+      inherit (config.services.nebula.networks) main;
+    in {
       # We'll start with one network and call it main for now
       networks.main = {
-        # Sops-nix will deploy the files
-        ca = "/run/secrets/nebula-main-ca.crt";
-        cert = "/run/secrets/nebula-main-cert.crt";
-        key = "/run/secrets/nebula-main-key.key";
+        enable = true;
+        ca = config.sops.secrets."nebula/main/main-ca".path;
+        cert = config.sops.secrets."nebula/main/crt".path;
+        key = config.sops.secrets."nebula/main/key".path;
 
         tun.device = "nebula-main";
 
@@ -16,6 +41,10 @@
 
         settings = {
           static_map.network = lib.mkDefault "ip4";
+        };
+
+        staticHostMap = lib.mkIf (!main.isLighthouse) {
+          "100.127.0.1" = ["o1.groovyreserve.com:4242"];
         };
 
         # Allow all traffic for now
