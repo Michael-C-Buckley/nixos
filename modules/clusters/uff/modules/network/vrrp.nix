801edabac80a3f894a3e278e0c155a9426c2ed0b
Michael Buckley <michaelcbuckley@proton.me>
2025-12-03 17:48:06 -0500

    UFF/VRRP: restored a VRRP for the LAN, if needed

--- modules/clusters/uff/modules/network/vrrp.nix
+++ modules/clusters/uff/modules/network/vrrp.nix
@@ -7,6 +7,12 @@
         virtualRouterId = 2;
         virtualIps = [{addr = "172.16.248.30/16";}];
       };
+      lan = {
+        state = lib.mkDefault "BACKUP";
+        interface = "wlp2s0";
+        virtualRouterId = 3;
+        virtualIps = [{addr = "192.168.48.30/24";}];
+      };
     };
   };
 }
