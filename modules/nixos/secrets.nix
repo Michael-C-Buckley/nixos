056ba05776bce177d2cad80d419c3f14303de9c4
Michael Buckley <michaelcbuckley@proton.me>
2025-12-04 13:12:32 -0500

    Activation/secrets: added a script to update local secrets not tracked in this git repo or nix

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -1,6 +1,27 @@
-# Single point of contact for mixing in secrets
 {
-  flake.modules.nixos.secrets = {config, ...}: {
-    nix.settings.secret-key-files = [config.sops.secrets.builderKey.path];
+  flake.modules.nixos.secrets = {
+    # Updates my local private secrets
+    # Impure and imperative, I cannot say I recommend this approach
+    # but it is one I'm testing out
+    system.activationScripts.pullSecretsRepo = {
+      deps = [];
+      text = ''
+        echo "[secrets-update] Checking for secrets repo in /etc/secrets"
+        if [ ! -d /etc/secrets/.git ]; then
+          echo "No git repo found in /etc/secrets; skipping pull"
+          exit 0
+        fi
+        cd /etc/secrets/
+        if [ -d .git ]; then
+          git pull --ff-only
+        fi
+        if git pull --ff-only; then
+          echo "[secrets-update] Pull successful"
+        else
+          echo "[secrets-update] Pull failed"
+          exit 0
+        fi
+      '';
+    };
   };
 }
