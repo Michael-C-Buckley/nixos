491e91b0999d687a289fc9b23c1247848724143c
Michael Buckley <michaelcbuckley@proton.me>
2025-12-04 13:31:16 +0000

    O1/config: fixed the general config, swapped k3s to the module

--- modules/hosts/o1/configuration.nix
+++ modules/hosts/o1/configuration.nix
@@ -1,28 +1,16 @@
 {config, ...}: let
   inherit (config.flake.modules) nixos;
 in {
-  flake.modules.nixos.o1 = {
-    config,
-    lib,
-    ...
-  }: {
+  flake.modules.nixos.o1 = {lib, ...}: {
     imports = with nixos; [
       cloudPreset
+      k3s
     ];
     environment = {
       # This is not linking for some reason, attempting to force copy instead of link
       etc."nix/nix.conf".mode = lib.mkForce "0755";
     };
 
-    services = {
-      harmonia = {
-        enable = true;
-        signKeyPaths = [config.sops.secrets.cachePrivateKey.path];
-      };
-      k3s.enable = true;
-    };
-
-    # For remote building
     #  It's a 4 vCPU server, don't overload it
     nix.settings = {
       cores = 2;
