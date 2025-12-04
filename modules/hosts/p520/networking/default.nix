cc419eb7cb8301688c323dfb9854326cfc68cf18
Michael Buckley <michaelcbuckley@proton.me>
2025-12-03 19:30:16 -0500

    P520/Networking: finalized tweaks for the new routing schema and lack of flannel interference

--- modules/hosts/p520/networking/default.nix
+++ modules/hosts/p520/networking/default.nix
@@ -7,41 +7,24 @@ in {
     };
 
     networking = {
+      # TODO: switch to using internal DNS server once set up
       nameservers = [
-        "192.168.49.1"
-        "192.168.49.31"
-        "192.168.49.32"
-        "192.168.49.33"
+        "1.1.1.1"
+        "8.8.8.8"
       ];
 
       hostName = "p520";
       hostId = "181a3ead";
 
-      # I would like to learn default routes with routing but container
-      # networking is a massive thorn as it creates endless unwanted
-      # default routes that I absolutely did not ask for;
-      # So for now, use my core switch within the LAN
-      defaultGateway = {
-        address = "192.168.49.1";
-        interface = "br0";
-      };
-
       firewall.trustedInterfaces = ["br0"];
 
       bridges = {
         br0.interfaces = []; # General use bridge, typically for VMs
-        br1.interfaces = []; # k3s CNI bridge; see `cni.nix` for more
       };
       interfaces = {
         br0.ipv4.addresses = [
           {
             address = interfaces.br0.ipv4;
-            prefixLength = 27;
-          }
-        ];
-        br1.ipv4.addresses = [
-          {
-            address = interfaces.br1.ipv4;
             prefixLength = 27;
           }
         ];
