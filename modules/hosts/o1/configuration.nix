29a1a1c9684cc073c099274e38358666decc694f
Michael Buckley <michaelcbuckley@proton.me>
2025-12-04 00:10:16 -0500

    O1/Netbird: added netbird

--- modules/hosts/o1/configuration.nix
+++ modules/hosts/o1/configuration.nix
@@ -1,16 +1,29 @@
 {config, ...}: let
   inherit (config.flake.modules) nixos;
 in {
-  flake.modules.nixos.o1 = {lib, ...}: {
+  flake.modules.nixos.o1 = {
+    config,
+    lib,
+    ...
+  }: {
     imports = with nixos; [
       cloudPreset
-      k3s
+      netbird
     ];
     environment = {
       # This is not linking for some reason, attempting to force copy instead of link
       etc."nix/nix.conf".mode = lib.mkForce "0755";
     };
 
+    services = {
+      harmonia = {
+        enable = true;
+        signKeyPaths = [config.sops.secrets.cachePrivateKey.path];
+      };
+      k3s.enable = true;
+    };
+
+    # For remote building
     #  It's a 4 vCPU server, don't overload it
     nix.settings = {
       cores = 2;
