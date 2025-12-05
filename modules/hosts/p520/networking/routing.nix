762bfe6b7ef740c55e78b26034f3fbefb9d9d0a8
Michael Buckley <michaelcbuckley@proton.me>
2025-12-05 07:10:39 -0500

    P520/OSPF: moved to network statements for better matching of ephemeral interfaces

--- modules/hosts/p520/networking/routing.nix
+++ modules/hosts/p520/networking/routing.nix
@@ -1,27 +1,35 @@
-{
+{config, ...}: let
+  inherit (config.flake.hosts.p520.interfaces) lo eno1 ens1f0 ens1f1 br0;
+in {
+  # This host uses network statements since I also use ContainerLab and advertise
+  # the IPs to reach the nodes, which creates an emphereal bridge with a changing
+  # name, so network statements match it much better
   flake.modules.nixos.p520 = {
     services.frr.config = ''
       ip forwarding
 
       int lo
         ip ospf passive
-        ip ospf area 0
 
       int br0
         ip ospf passive
-        ip ospf area 0
 
       int eno1
-        ip ospf area 0
         ip ospf cost 1000
 
       int ens1f0
-        ip ospf area 0
         ip ospf cost 100
 
       int ens1f1
-        ip ospf area 0
         ip ospf cost 100
+
+      router ospf
+        network ${lo.ipv4}/32 area 0
+        network ${eno1.ipv4}/32 area 0
+        network ${ens1f0.ipv4}/32 area 0
+        network ${ens1f1.ipv4}/32 area 0
+        network ${br0.ipv4}/32 area 0
+        network 172.20.20.0/24 area 0
     '';
   };
 }
