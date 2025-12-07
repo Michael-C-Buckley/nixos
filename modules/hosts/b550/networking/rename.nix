acab9f96adad7dd38242a0d5d3720aaf6b3f6519
Michael Buckley <michaelcbuckley@proton.me>
2025-12-07 07:09:33 -0500

    B550/NICs: systemd rename

---
+++ modules/hosts/b550/networking/rename.nix
@@ -0,0 +1,35 @@
+# Statically rename all NICs to prevent issues
+let
+  mkLink = name: mac: ''
+    [Match]
+    MACAddress=${mac}
+
+    [Link]
+    Name=${name}
+  '';
+in {
+  flake.modules.nixos.b550 = {config, ...}: {
+    environment.etc = {
+      "udev/rules.d/10-rename-nics.rules" = config.sops.templates.udev-rename.path;
+      "systemd/network/10-eno1.link".source = config.sops.templates."link-eno1".path;
+      "systemd/network/10-enp2.link".source = config.sops.templates."link-enp2".path;
+      "systemd/network/10-enp3.link".source = config.sops.templates."link-enp3".path;
+      "systemd/network/10-enp4.link".source = config.sops.templates."link-enp4".path;
+    };
+
+    sops = {
+      secrets = {
+        "nic/eno1" = {};
+        "nic/enp2" = {};
+        "nic/enx3" = {};
+        "nic/enx4" = {};
+      };
+      templates = {
+        "link-eno1".content = mkLink "eno1" config.sops.placeholder."nic/eno1";
+        "link-enp2".content = mkLink "enp2" config.sops.placeholder."nic/enp2";
+        "link-enx3".content = mkLink "enx3" config.sops.placeholder."nic/enx3";
+        "link-enx4".content = mkLink "enx4" config.sops.placeholder."nic/enx4";
+      };
+    };
+  };
+}
