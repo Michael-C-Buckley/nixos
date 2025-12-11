8e4c8803af7d79dca4016a068c2774ff88563eb5
Michael Buckley <michaelcbuckley@proton.me>
2025-12-11 15:39:02 +0000

    NixOS/Niri: added initial attempt a basic config, largely inspired from their default

--- modules/nixos/graphical/desktop-environments/noctalia.nix
+++ modules/nixos/graphical/desktop-environments/noctalia.nix
@@ -1,41 +1,68 @@
 # Not technically a DE/WM but a Quickshell theme I'm just living here
 {inputs, ...}: {
-  flake.modules.nixos.noctalia = {pkgs, ...}: {
-    imports = [
-      inputs.noctalia.nixosModules.default
-    ];
+  flake.modules.nixos.noctalia = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    options.custom.noctalia = {
+      wrapperScript = lib.mkOption {
+        type = lib.types.package;
+        description = "Path to a wrapper script for Noctalia.";
+        default = pkgs.writeShellScript "noctalia-wrapper" ''
+          #!/usr/bin/env bash
+          exec $(cat ~/.local/share/noctalia_path) "$@"
+        '';
+      };
+      pathScript = lib.mkOption {
+        type = lib.types.package;
+        description = "Script to capture the active Noctalia session and path for IPC usage";
+        default = pkgs.writeShellScript "noctalia-path" ''
+          #!/usr/bin/env bash
+          mkdir -p ~/.local/share
+          echo $(whereis noctalia-shell | awk '{print $2}') > ~/.local/share/noctalia_path
+        '';
+      };
+    };
+    config = {
+      imports = [
+        inputs.noctalia.nixosModules.default
+      ];
 
-    custom.impermanence.persist.user.directories = [
-      ".config/noctalia"
-    ];
-    # Dependencies and fonts
-    environment = {
-      systemPackages = with pkgs; [
-        brightnessctl
-        cava
-        cliphist
-        coreutils
-        ddcutil
-        file
-        findutils
-        gpu-screen-recorder
-        libnotify
-        matugen
-        swww
-        wl-clipboard
-        wlsunset
+      custom.impermanence.persist.user.directories = [
+        ".config/noctalia"
+      ];
+      # Dependencies and fonts
+      environment.systemPackages = with pkgs;
+        [
+          brightnessctl
+          cava
+          cliphist
+          coreutils
+          ddcutil
+          file
+          findutils
+          gpu-screen-recorder
+          libnotify
+          matugen
+          swww
+          wl-clipboard
+          wlsunset
+        ]
+        # Add the wrapper scripts so they can be reused
+        ++ (with config.custom.noctalia; [wrapperScript pathScript]);
+
+      services.noctalia-shell = {
+        enable = true;
+      };
+
+      # These fonts are used by default
+      fonts.packages = with pkgs; [
+        roboto
+        inter
+        material-symbols
       ];
     };
-
-    services.noctalia-shell = {
-      enable = true;
-    };
-
-    # These fonts are used by default
-    fonts.packages = with pkgs; [
-      roboto
-      inter
-      material-symbols
-    ];
   };
 }
