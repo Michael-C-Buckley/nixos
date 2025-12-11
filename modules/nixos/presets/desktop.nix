80d21a94c87976314b472e47a9ad56e829988a38
Michael Buckley <michaelcbuckley@proton.me>
2025-12-10 22:58:14 -0500

    Virtualization/Incus: readded incus and put in the desktop preset

--- modules/nixos/presets/desktop.nix
+++ modules/nixos/presets/desktop.nix
@@ -17,6 +17,7 @@ in {
 
       # Virtualization
       containerlab
+      incus
       libvirt
 
       # Userspace
---
+++ modules/nixos/virtualization/incus.nix
@@ -0,0 +1,17 @@
+{
+  flake.modules.nixos.incus = {pkgs, ...}: {
+    # Incus is bested used with these modules available
+    boot.kernelModules = ["apparmor" "virtiofs" "9p" "9pnet_virtio"];
+    # Incus will prefer Red Hat's Virtiofs over 9P
+    environment.systemPackages = [pkgs.virtiofsd];
+
+    custom.impermanence.cache.directories = ["/var/lib/incus"];
+
+    virtualisation.incus = {
+      enable = true;
+      package = pkgs.incus; # Stable version is old
+      ui.enable = true;
+      agent.enable = true;
+    };
+  };
+}
