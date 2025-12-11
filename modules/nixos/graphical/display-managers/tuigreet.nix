d9aa28f21cf68b73bde25b1611da901795ee77e3
Michael Buckley <michaelcbuckley@proton.me>
2025-12-11 14:30:09 +0000

    NixOS/tuigreet: added an option for changing the default session it uses for upcoming niri

--- modules/nixos/graphical/display-managers/tuigreet.nix
+++ modules/nixos/graphical/display-managers/tuigreet.nix
@@ -1,22 +1,36 @@
 {
-  flake.modules.nixos.tuigreet = {pkgs, ...}: {
-    environment.systemPackages = [pkgs.tuigreet];
+  flake.modules.nixos.tuigreet = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    options.custom.tuigreet = {
+      defaultCommand = lib.mkOption {
+        type = lib.types.str;
+        default = "hyprland";
+        description = "The default session command for tuigreet and auto-login";
+      };
+    };
 
-    hardware.graphics.enable = true;
-    services.xserver.enable = true;
+    config = {
+      environment.systemPackages = [pkgs.tuigreet];
+      hardware.graphics.enable = true;
+      services.xserver.enable = true;
 
-    services.greetd = {
-      enable = true;
-      settings = {
-        # Session on first login which would use auto-login
-        initial_session = {
-          user = "michael";
-          command = "hyprland";
-        };
-        # All other sessions
-        default_session = {
-          command = "tuigreet --cmd hyprland";
-          user = "greeter";
+      services.greetd = {
+        enable = true;
+        settings = {
+          # Session on first login which would use auto-login
+          initial_session = {
+            user = "michael";
+            command = config.custom.tuigreet.defaultCommand;
+          };
+          # All other sessions
+          default_session = {
+            command = "tuigreet --cmd " + config.custom.tuigreet.defaultCommand;
+            user = "greeter";
+          };
         };
       };
     };
