2067d92055dd0efb745f5f406ead873032eb88a7
Michael Buckley <michaelcbuckley@proton.me>
2025-12-10 21:50:25 -0500

    P520/Postgres: removed now unused tables

--- modules/hosts/p520/services/postgres.nix
+++ modules/hosts/p520/services/postgres.nix
@@ -3,38 +3,21 @@
     # Data directory is a ZFS dataset: zroot/p520/postgres
     services.postgresql = {
       enable = true;
-      ensureDatabases = ["hydra" "atticd" "forgejo" "openwebui"];
+      ensureDatabases = ["hydra"];
       ensureUsers = [
         {
           name = "hydra";
           ensureDBOwnership = true;
           ensureClauses = {createdb = true;};
         }
-        {
-          name = "atticd";
-          ensureDBOwnership = true;
-        }
-        {
-          name = "forgejo";
-          ensureDBOwnership = true;
-        }
-        {
-          name = "openwebui";
-          ensureDBOwnership = true;
-        }
       ];
-      # Allow connections from pods (K3s default pod network)
       settings = {
         listen_addresses = lib.mkForce "0.0.0.0";
       };
-      authentication = ''
-        host forgejo forgejo 10.42.0.0/16 trust
-        host openwebui openwebui 10.42.0.0/16 trust
-      '';
     };
 
     networking.firewall.extraInputRules = ''
-      ip saddr { 127.0.0.1, 10.42.0.0/16 } tcp dport 5432 accept comment "Allow PostgreSQL from localhost and k3s pods"
+      ip saddr { 127.0.0.1, 10.42.0.0/16 192.168.0.0/16 } tcp dport 5432 accept comment "Allow PostgreSQL from LAN"
     '';
   };
 }
