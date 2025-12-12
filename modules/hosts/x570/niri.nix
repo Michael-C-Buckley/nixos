5d197957807c8fd1033602663a5c9f1206551e3d
Michael Buckley <michaelcbuckley@proton.me>
2025-12-12 17:41:22 -0500

    Wrapped/Niri: added X570

--- modules/hosts/x570/niri.nix
+++ modules/hosts/x570/niri.nix
@@ -1,34 +1,38 @@
-let
+{config, ...}: let
   mon1 = "DP-1";
   mon2 = "HDMI-A-3";
 in {
-  flake.modules.nixos.x570 = {
-    hjem.users.michael = {
-      files.".config/niri/host.kdl".text = ''
-        workspace "N1" { open-on-output "${mon1}"; }
-        workspace "N2" { open-on-output "${mon1}"; }
-        workspace "N3" { open-on-output "${mon1}"; }
-        workspace "N4" { open-on-output "${mon1}"; }
-        workspace "N5" { open-on-output "${mon1}"; }
+  flake.modules.nixos.x570 = {pkgs, ...}: {
+    programs.niri = {
+      enable = true;
+      package = config.flake.wrappers.mkNiri {
+        inherit pkgs;
+        extraConfig = ''
+          workspace "N1" { open-on-output "${mon1}"; }
+          workspace "N2" { open-on-output "${mon1}"; }
+          workspace "N3" { open-on-output "${mon1}"; }
+          workspace "N4" { open-on-output "${mon1}"; }
+          workspace "N5" { open-on-output "${mon1}"; }
 
-        workspace "S1"  { open-on-output "${mon2}"; }
-        workspace "S2"  { open-on-output "${mon2}"; }
-        workspace "S3"  { open-on-output "${mon2}"; }
-        workspace "S4"  { open-on-output "${mon2}"; }
-        workspace "S5"  { open-on-output "${mon2}"; }
+          workspace "S1"  { open-on-output "${mon2}"; }
+          workspace "S2"  { open-on-output "${mon2}"; }
+          workspace "S3"  { open-on-output "${mon2}"; }
+          workspace "S4"  { open-on-output "${mon2}"; }
+          workspace "S5"  { open-on-output "${mon2}"; }
 
-        output "${mon1}" {
-          mode "3440x1440@165.000"
-          position x=0 y=0
-          focus-at-startup
-        }
+          output "${mon1}" {
+            mode "3440x1440@165.000"
+            position x=0 y=0
+            focus-at-startup
+          }
 
-        output "${mon2}" {
-          mode "2560x1440@74.599"
-          position x=3440 y=-500
-          transform "270"
-        }
-      '';
+          output "${mon2}" {
+            mode "2560x1440@74.599"
+            position x=3440 y=-500
+            transform "270"
+          }
+        '';
+      };
     };
   };
 }
