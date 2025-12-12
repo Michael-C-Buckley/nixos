f9a102ca27ac4f5d923f432f720ec72fd68f0963
Michael Buckley <michaelcbuckley@proton.me>
2025-12-11 21:06:03 -0500

    Niri: fixed up T14; globally disabled hot-corners

--- modules/hosts/t14/niri.nix
+++ modules/hosts/t14/niri.nix
@@ -9,6 +9,11 @@ in {
         workspace "N3" { open-on-output "${mon1}"; }
         workspace "N4" { open-on-output "${mon1}"; }
         workspace "N5" { open-on-output "${mon1}"; }
+
+        output "${mon1}" {
+          mode "1920x1080@60.008"
+          scale 1.0
+        }
       '';
     };
   };
