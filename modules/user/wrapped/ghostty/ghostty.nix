d49125dad07abf15c8da8c0ad79a13813309eb15
Michael Buckley <michaelcbuckley@proton.me>
2025-12-13 08:58:24 -0500

    Wrapped/Ghostty: moved ghostty to a wrapper

---
+++ modules/user/wrapped/ghostty/ghostty.nix
@@ -0,0 +1,35 @@
+{config, ...}: {
+  perSystem = {pkgs, ...}: {
+    packages = {
+      ghostty = config.flake.wrappers.mkGhostty {
+        inherit pkgs;
+        extraConfig = ''
+          window-decoration = false
+        '';
+      };
+      ghosttyMac = config.flake.wrappers.mkGhostty {
+        inherit pkgs;
+      };
+    };
+  };
+
+  flake.wrappers.mkGhostty = {
+    pkgs,
+    pkg ? pkgs.ghostty,
+    extraConfig ? "",
+    extraRuntimeInputs ? [],
+  }: let
+    buildInputs = [pkgs.cascadia-code] ++ extraRuntimeInputs;
+  in
+    pkgs.symlinkJoin {
+      name = "ghostty";
+      paths = [pkg];
+      inherit buildInputs;
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/ghostty \
+          --add-flags "-c ${import ./_config.nix {inherit pkgs extraConfig;}}" \
+          --prefix PATH : ${pkgs.lib.makeBinPath buildInputs}
+      '';
+    };
+}
