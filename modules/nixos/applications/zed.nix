659d274be0433e4b01772df4d641e51392b59e85
Michael Buckley <michaelcbuckley@proton.me>
2025-12-15 18:01:11 -0500

    Flake/modules: removed last unneeded self reference

--- modules/nixos/applications/zed.nix
+++ modules/nixos/applications/zed.nix
@@ -1,7 +1,7 @@
 # Include my Zed plus the persisted directories
-{self, ...}: {
+{config, ...}: {
   flake.modules.nixos.app-zed = {pkgs, ...}: {
-    hjem.users.michael.packages = [self.packages.${pkgs.stdenv.hostPlatform.system}.zeditor];
+    hjem.users.michael.packages = [config.flake.packages.${pkgs.stdenv.hostPlatform.system}.zeditor];
 
     custom.impermanence = {
       persist.user.directories = [
