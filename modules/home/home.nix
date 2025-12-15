c03182d0f7fbcc2aff8b7f92ea2b2de2c7167dcf
Michael Buckley <michaelcbuckley@proton.me>
2025-12-15 20:40:26 +0000

    Alpine/Home: split out the configs a bit more appropriately

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -3,40 +3,17 @@
 # copy it, I suggest looking elsewhere for home-manager inspiration
 # as I will be attempting crazy madness and doing things which probably
 # will break, and break a lot
-{config, ...}: let
-  inherit (config) flake;
-in {
-  flake.modules.homeManager.default = {
-    pkgs,
-    lib,
-    ...
-  }: let
-    inherit (pkgs.stdenv.hostPlatform) system;
-
-    # My shell
-    shell = flake.wrappers.mkFish {
-      inherit pkgs;
-      env = {NH_FLAKE = "/home/michael/nixos";};
-    };
-  in {
-    programs = {
-      # Generates completions but also uses my wrapped one, best of both worlds
-      fish = {
-        enable = true;
-        package = shell;
-      };
-      home-manager.enable = true;
-    };
-
+{
+  flake.modules.homeManager.default = {pkgs, ...}: {
     home = {
       username = "michael";
       homeDirectory = "/home/michael";
-
-      stateVersion = lib.mkDefault "25.11";
 
       packages = with pkgs; [
         nh # limited on non-NixOS systems but still useful
       ];
     };
+
+    programs.home-manager.enable = true;
   };
 }
