c2784a3797b9ac11d15e4e56694d5bd0f3dc9d32
Michael Buckley <michaelcbuckley@proton.me>
2025-12-15 16:39:51 +0000

    Darwin/M1: added wrapped git with appropriate expected signing key

--- modules/darwin/m1/hjem.nix
+++ modules/darwin/m1/hjem.nix
@@ -1,15 +1,14 @@
 {
   flake.modules.darwin.m1 = {
-    files = {
-      ".zshrc" = {
-        text = ''
+    hjem.users.michael = {
+      git.signingKey = "6F749AA097DC10EA46FE0ECD22CDD3676227046F!";
+      files = {
+        ".zshrc".text = ''
           if [[ -o interactive ]]; then
             export GPG_TTY=$(tty)
             export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
           fi
         '';
-        mode = "copy";
-        permissions = "0644";
       };
     };
   };
