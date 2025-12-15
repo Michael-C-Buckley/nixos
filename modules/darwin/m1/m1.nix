df2908bee53000206acf68a6a7c6f309511f3071
Michael Buckley <michaelcbuckley@proton.me>
2025-12-15 16:39:51 +0000

    Darwin/M1: added wrapped git with appropriate expected signing key

--- modules/darwin/m1/m1.nix
+++ modules/darwin/m1/m1.nix
@@ -3,11 +3,12 @@
   inputs,
   ...
 }: let
-  inherit (config.flake.modules.darwin) default packages;
+  inherit (config.flake.modules.darwin) default packages m1;
   inherit (config.flake) hjemConfig;
 in {
   flake.darwinConfigurations.m1 = inputs.nix-darwin.lib.darwinSystem {
     modules = [
+      m1
       default
       packages
       hjemConfig.darwin
