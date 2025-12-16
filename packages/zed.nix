bef7c29557acb4bbc445500e877c5934ec8551ce
Michael Buckley <michaelcbuckley@proton.me>
2025-12-16 15:03:17 +0000

    Jail/Zed: wrapped linux zed, derivation builds but needs further verification

--- packages/zed.nix
+++ packages/zed.nix
@@ -1,6 +1,12 @@
 # Just a simple way to put some tools in the Zed path
-{
-  perSystem = {pkgs, ...}: let
+# This also bwraps the linux variant
+{inputs, ...}: {
+  perSystem = {
+    pkgs,
+    lib,
+    system,
+    ...
+  }: let
     zedInputs = with pkgs; [
       # Nix
       alejandra
@@ -20,8 +26,8 @@
       pyright
       basedpyright
     ];
-  in {
-    packages.zeditor = pkgs.symlinkJoin {
+
+    localPkg = pkgs.symlinkJoin {
       name = "zeditor";
       paths = [pkgs.zed-editor];
       buildInputs = zedInputs;
@@ -31,5 +37,28 @@
         --prefix PATH : ${pkgs.lib.makeBinPath zedInputs}
       '';
     };
+
+    jail = inputs.jail.lib.init pkgs;
+    inherit (jail.combinators) gui gpu readonly rw-bind noescape;
+    homeBind = path: (rw-bind (noescape path) (noescape path));
+
+    # These are my directories I use, you likely want to adjust this
+    bwrapFeatures = [
+      gui
+      gpu
+      (readonly "/nix/store")
+      (homeBind "~/nixos") # where I keep my system flake
+      (homeBind "~/projects") # The rest of where I usually keep projects
+      (homeBind "~/.config/zed")
+      (homeBind "~/.cache/zed")
+      (homeBind "~/.local/share/zed")
+    ];
+  in {
+    # Split handling so that the linux packages can be bwrapped
+    packages.zeditor =
+      if (lib.hasSuffix "linux" system)
+      then (jail "zeditor" localPkg bwrapFeatures)
+      # Untouched for anything not linux
+      else localPkg;
   };
 }
