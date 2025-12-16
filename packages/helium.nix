70ed7747acdaeab49722679664c3208d287998e3
Michael Buckley <michaelcbuckley@proton.me>
2025-12-16 07:29:45 -0500

    Jail/Helium: converted the helium derivation to be jailed

--- packages/helium.nix
+++ packages/helium.nix
@@ -1,24 +1,43 @@
-{
+# Helium Browser, pulled from the appImage and jailed
+{inputs, ...}: {
   perSystem = {
     pkgs,
     system,
     lib,
     ...
   }: let
+    # Preparation definitions on various tooling
     source = (pkgs.callPackage ../_sources/generated.nix {}).helium;
     contents = pkgs.appimageTools.extract source;
     inherit (source) pname version src;
-  in
-    lib.optionalAttrs (!lib.hasSuffix "darwin" system) {
-      packages.helium = pkgs.appimageTools.wrapType2 {
-        inherit pname version src;
 
-        extraInstallCommands = ''
-          install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
-          substituteInPlace $out/share/applications/${pname}.desktop \
-            --replace 'Exec=AppRun' 'Exec=${pname}'
-          cp -r ${contents}/usr/share/icons $out/share
-        '';
-      };
+    jail = inputs.jail.lib.init pkgs;
+    inherit (jail.combinators) network gui readonly rw-bind noescape;
+    homeBind = path: (rw-bind (noescape path) (noescape path));
+
+    features = [
+      network
+      gui
+      (readonly "/nix/store")
+      (homeBind "~/.local/share/applications/")
+      (homeBind "~/Downloads")
+      (homeBind "~/.cache/net.imput.helium/")
+      (homeBind "~/.config/net.imput.helium/")
+    ];
+
+    extraInstallCommands = ''
+      install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
+
+    localPkg = pkgs.appimageTools.wrapType2 {
+      inherit pname version src extraInstallCommands;
+    };
+  in
+    # Helium is only available on linux and bwrap is a linux only utility
+    lib.optionalAttrs (lib.hasSuffix "linux" system) {
+      packages.helium = jail "helium" localPkg features;
     };
 }
