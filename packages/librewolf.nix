aa1ed208d652a7fd46676b8a2b4922017f397560
Michael Buckley <michaelcbuckley@proton.me>
2025-12-16 15:23:31 +0000

    Jail/Librewolf: wrapped linux librewolf (and bin), derivation builds but needs further verification

---
+++ packages/librewolf.nix
@@ -0,0 +1,31 @@
+{inputs, ...}: {
+  perSystem = {
+    pkgs,
+    system,
+    lib,
+    ...
+  }: let
+    jail = inputs.jail.lib.init pkgs;
+    inherit (jail.combinators) network gui readonly rw-bind noescape;
+    homeBind = path: (rw-bind (noescape path) (noescape path));
+
+    features = [
+      network
+      gui
+      (readonly "/nix/store")
+      (homeBind "~/.local/share/applications/")
+      (homeBind "~/Downloads")
+      (homeBind "~/.librewolf")
+      (homeBind "~/.cache/librewolf/")
+    ];
+
+    mkLibrewolf = pkg: jail "librewolf" pkg features;
+  in
+    # Bwrap only for linux
+    lib.optionalAttrs (lib.hasSuffix "linux" system) {
+      packages = {
+        librewolf = mkLibrewolf pkgs.librewolf;
+        librewolf-bin = mkLibrewolf pkgs.librewolf-bin;
+      };
+    };
+}
