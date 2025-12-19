af3aaa2f112fb76936019f137cfde545284bb6fc
Michael Buckley <michaelcbuckley@proton.me>
2025-12-19 14:20:46 +0000

    Alpine/Home: added termcaps for less fallback on ash

--- modules/home/alpine/files.nix
+++ modules/home/alpine/files.nix
@@ -10,6 +10,20 @@
         alias ls='ls --color=auto'
         alias grep='grep --color=auto'
 
+        # Set Less to use color when paging
+        # This is overridden by Bat when using my shell, but this will
+        # at least be available everywhere else when not
+
+        export LESS='-R'
+        export LESS_TERMCAP_mb=$'\e[1;31m'
+        export LESS_TERMCAP_md=$'\e[1;36m'
+        export LESS_TERMCAP_me=$'\e[0m'
+        export LESS_TERMCAP_so=$'\e[01;44;33m'
+        export LESS_TERMCAP_se=$'\e[0m'
+        export LESS_TERMCAP_us=$'\e[1;32m'
+        export LESS_TERMCAP_ue=$'\e[0m'
+        export MANPAGER="less"
+
         if [ -z "$DISABLE_NIX_PROFILE" ]; then
             # Activate Nix profile
             . "${config.home.profileDirectory}"
