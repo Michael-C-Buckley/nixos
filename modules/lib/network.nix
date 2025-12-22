87e59a7fefa1d9eaef455e2e8e5289ef4fe836bf
Michael Buckley <michaelcbuckley@proton.me>
2025-12-21 20:14:53 -0500

    Flake/Network: enhanced the lab network module to create systemd units

--- modules/lib/network.nix
+++ modules/lib/network.nix
@@ -1,12 +1,19 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   flake.lib.network = {
+    # Simple helper to change the VLAN names
+    fixVlanName = name: builtins.replaceStrings ["-"] ["."] name;
+
     # Takes a host's interface list from the flake host file
     # and parses out the VLANs and returns a list in the string format
     # such as `eth1.100` where it's name and ID
     #getVlanList = interfaces: map (a: builtins.replaceStrings ["-"] ["."] a) (builtins.filter (builtins.attrNames interfaces));
     getVlanList = interfaces:
       map
-      (a: builtins.replaceStrings ["-"] ["."] a)
+      config.flake.lib.network.fixVlanName
       (lib.filter (lib.hasInfix "-") (builtins.attrNames interfaces));
 
     # Template for the VLAN schema I use
