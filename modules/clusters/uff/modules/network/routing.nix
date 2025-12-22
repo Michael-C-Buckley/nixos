fca30a79d1ce19adb62627dea9377a9afad8aeaa
Michael Buckley <michaelcbuckley@proton.me>
2025-12-22 08:00:27 -0500

    Networking/Lab: moved BGP to the lab module, reduced from UFF only

--- modules/clusters/uff/modules/network/routing.nix
+++ modules/clusters/uff/modules/network/routing.nix
@@ -7,14 +7,8 @@ in {
     ...
   }: let
     inherit (lib.strings) concatMapStringsSep;
-    inherit (hosts.${config.networking.hostName}.interfaces) lo;
 
     uffHosts = ["uff1" "uff2" "uff3"];
-
-    # Exclude the current host from the neighbors
-    neighbors = concatMapStringsSep "\n" (
-      hostname: "neighbor ${hosts.${hostname}.interfaces.lo.ipv4} peer-group fabric"
-    ) (lib.filter (h: h != config.networking.hostName) uffHosts);
 
     # Exclude current host from BFD peers
     bfdPeers = let
@@ -39,18 +33,6 @@ in {
       concatMapStringsSep "\n" (n: "peer ${n}") (uffInterfaces ++ otherPeers);
   in {
     services.frr.config = ''
-      ip prefix-list MT3 seq 5 permit 192.168.48.0/20
-      ip prefix-list MT3 seq 10 permit 192.168.64.0/20
-      ip prefix-list 65102-OUT seq 5 permit 192.168.48.0/20
-      ip prefix-list 65102-OUT seq 10 deny 0.0.0.0/0
-      ip prefix-list 65102-IN seq 5 permit 192.168.64.0/20
-      ip prefix-list 65102-IN seq 10 deny 0.0.0.0/0
-      !
-      ip forwarding
-      ipv6 forwarding
-      !
-      router ospf
-       router-id ${lo.ipv4}
       int lo
        ip ospf area 0
        ip ospf passive
@@ -58,36 +40,10 @@ in {
        ip ospf bfd
        ip ospf area 0
        ip ospf cost 400
-      int br100
-       ip ospf passive
-       ip ospf area 0
       int enu2
        ip ospf bfd
        ip ospf area 0
        ip ospf cost 1000
-      !
-      router bgp 65101
-       bgp router-id ${lo.ipv4}
-       no bgp default ipv4-unicast
-
-       neighbor fabric peer-group
-       neighbor fabric update-source ${lo.ipv4}
-       neighbor fabric remote-as 65101
-       ${neighbors}
-       neighbor 192.168.49.1 remote-as 65101
-       neighbor 192.168.49.1 bfd
-
-       address-family l2vpn evpn
-        neighbor fabric activate
-        advertise-all-vni
-       exit-address-family
-
-       address-family ipv4 unicast
-        neighbor 192.168.49.1 prefix-list MT3 in
-        neighbor 192.168.49.1 activate
-
-       exit-address-family
-      exit
       !
       bfd
        ${bfdPeers}
