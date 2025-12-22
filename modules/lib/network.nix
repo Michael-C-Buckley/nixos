2ff94d2500ec28060740041d4b468b74c8c97334
Michael Buckley <michaelcbuckley@proton.me>
2025-12-21 19:00:33 -0500

    Flake/Network: added lib and common lab module for the network expansion

---
+++ modules/lib/network.nix
@@ -0,0 +1,30 @@
+{lib, ...}: {
+  flake.lib.network = {
+    # Takes a host's interface list from the flake host file
+    # and parses out the VLANs and returns a list in the string format
+    # such as `eth1.100` where it's name and ID
+    #getVlanList = interfaces: map (a: builtins.replaceStrings ["-"] ["."] a) (builtins.filter (builtins.attrNames interfaces));
+    getVlanList = interfaces:
+      map
+      (a: builtins.replaceStrings ["-"] ["."] a)
+      (lib.filter (lib.hasInfix "-") (builtins.attrNames interfaces));
+
+    # Template for the VLAN schema I use
+    # converts from subinterface in the format `eno1.100`
+    # where the decimal separate interface and VLAN ID
+    mkVlanNetdev = vlanName: let
+      parts = builtins.split "\\." vlanName;
+      #iface = builtins.elemAt parts 0;
+      vlanId = builtins.elemAt parts 2;
+    in {
+      name = "10-${vlanName}";
+      value = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = vlanName;
+        };
+        vlanConfig.Id = builtins.fromJSON vlanId;
+      };
+    };
+  };
+}
