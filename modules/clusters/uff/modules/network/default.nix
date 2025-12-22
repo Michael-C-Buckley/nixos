edfb95b1cdea97a3ae9ae2f050994288d0ed20ea
Michael Buckley <michaelcbuckley@proton.me>
2025-12-22 11:21:06 -0500

    Networking/Lab: purged the old IP schema from the config

--- modules/clusters/uff/modules/network/default.nix
+++ modules/clusters/uff/modules/network/default.nix
@@ -1,14 +1,26 @@
-{config, ...}: {
-  flake.modules.nixos.uff = {
-    imports = with config.flake.modules.nixos; [
+{config, ...}: let
+  inherit (config) flake;
+in {
+  flake.modules.nixos.uff = {config, ...}: {
+    imports = with flake.modules.nixos; [
       lab-network
       vrrp
     ];
 
     networking = {
       interfaces = {
-        eno1.mtu = 1500;
+        eno1.mtu = 9000;
         enu2.mtu = 9000;
+        lo.ipv4.addresses = [
+          {
+            address = flake.hosts.${config.networking.hostName}.interfaces.lo.ipv4;
+            prefixLength = 32;
+          }
+          {
+            address = "192.168.61.0";
+            prefixLength = 32;
+          }
+        ];
       };
       useDHCP = false;
       networkmanager = {
