02e21bd66431b17ee2d2179b4460ac8e1b5626aa
Michael Buckley <michaelcbuckley@proton.me>
2025-12-23 13:44:35 -0500

    Flake/Routing: updated routing on the lab hosts for the new design

--- modules/clusters/uff/modules/network/routing.nix
+++ modules/clusters/uff/modules/network/routing.nix
@@ -1,9 +1,6 @@
 {
   flake.modules.nixos.uff = {
     services.frr.config = ''
-      router ospf
-       distribute-list BLOCK-DEFAULT eno1.4
-       distribute-list BLOCK-DEFAULT eno2.6
       int lo
        ip ospf area 0
        ip ospf passive
@@ -11,18 +8,6 @@
        ip ospf area 0
        ip ospf cost 1000
       int enu2
-       ip ospf area 0
-       ip ospf cost 400
-      int eno1.3
-       ip ospf area 0
-       ip ospf cost 1010
-      int eno1.4
-       ip ospf area 0
-       ip ospf cost 1000
-      int enu2.5
-       ip ospf area 0
-       ip ospf cost 410
-      int enu2.6
        ip ospf area 0
        ip ospf cost 400
       !
