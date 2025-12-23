12ab9cd61cbd4c5972087e97c047fef9f52fb588
Michael Buckley <michaelcbuckley@proton.me>
2025-12-23 13:51:54 -0500

    Networking/Hosts: simplified the logic in the file

--- modules/nixos/network/hosts.nix
+++ modules/nixos/network/hosts.nix
@@ -1,28 +1,29 @@
 {config, ...}: let
   inherit (config.flake) hosts;
-  inherit (config.lib.network) getAddress;
+  inherit (config.flake.lib.network) getAddress;
+
+  a = host: interface: getAddress hosts.${host}.interfaces.${interface}.ipv4;
 in {
   flake.modules.nixos.network = {
     networking.hosts = {
       "192.168.48.1" = ["mt3" "mt3.michael.lan"];
       "192.168.48.2" = ["c9300" "c9300.michael.lan"];
       "192.168.48.3" = ["r8200" "r8200.michael.lan"];
-
       "192.168.48.20" = ["m1" "m1.michael.lan"];
-      "${getAddress hosts.x570.interfaces.lo.ipv4}" = ["x570" "x570.michael.lan"];
-      "${getAddress hosts.p520.interfaces.lo.ipv4}" = ["p520" "p520.michael.lan"];
 
       # 2.5GbE
-      "${getAddress hosts.uff1.interfaces.enu2.ipv4}" = ["uff1s"];
-      "${getAddress hosts.uff2.interfaces.enu2.ipv4}" = ["uff2s"];
-      "${getAddress hosts.uff3.interfaces.enu2.ipv4}" = ["uff3s"];
+      "${a "uff1" "enu2"}" = ["uff1s"];
+      "${a "uff2" "enu2"}" = ["uff2s"];
+      "${a "uff3" "enu2"}" = ["uff3s"];
 
       # Loopbacks
       "192.168.61.0" = ["uff" "uff.michael.lan"];
-      "${getAddress hosts.uff1.interfaces.lo.ipv4}" = ["uff1" "uff1.michael.lan"];
-      "${getAddress hosts.uff2.interfaces.lo.ipv4}" = ["uff2" "uff2.michael.lan"];
-      "${getAddress hosts.uff3.interfaces.lo.ipv4}" = ["uff3" "uff3.michael.lan"];
-      "${getAddress hosts.b550.interfaces.lo.ipv4}" = ["b550" "b550.michael.lan"];
+      "${a "uff1" "lo"}" = ["uff1" "uff1.michael.lan"];
+      "${a "uff2" "lo"}" = ["uff2" "uff2.michael.lan"];
+      "${a "uff3" "lo"}" = ["uff3" "uff3.michael.lan"];
+      "${a "b550" "lo"}" = ["b550" "b550.michael.lan"];
+      "${a "x570" "lo"}" = ["x570" "x570.michael.lan"];
+      "${a "p520" "lo"}" = ["p520" "p520.michael.lan"];
     };
   };
 }
