7605d1be3749f3501a5fd8262c27bec48dbb81aa
Michael Buckley <michaelcbuckley@proton.me>
2025-12-23 13:44:35 -0500

    Flake/Routing: updated routing on the lab hosts for the new design

--- modules/hosts/b550/networking/routing.nix
+++ modules/hosts/b550/networking/routing.nix
@@ -1,11 +1,6 @@
 {
   flake.modules.nixos.b550 = {
     services.frr.config = ''
-      router ospf
-       distribute-list BLOCK-DEFAULT eno1.4
-       distribute-list BLOCK-DEFAULT eno2.6
-       distribute-list BLOCK-DEFAULT enx3.8
-       distribute-list BLOCK-DEFAULT enx4.8
       int lo
        ip ospf area 0
        ip ospf passive
@@ -22,30 +17,6 @@
        ip ospf area 0
        ip ospf cost 100
       int enx4
-       ip ospf area 0
-       ip ospf cost 100
-      int eno1.3
-       ip ospf area 0
-       ip ospf cost 1010
-      int eno1.4
-       ip ospf area 0
-       ip ospf cost 1000
-      int enp2.5
-       ip ospf area 0
-       ip ospf cost 410
-      int enp2.6
-       ip ospf area 0
-       ip ospf cost 400
-      int enx3.7
-       ip ospf area 0
-       ip ospf cost 110
-      int enx3.8
-       ip ospf area 0
-       ip ospf cost 100
-      int enx4.7
-       ip ospf area 0
-       ip ospf cost 110
-      int enx4.8
        ip ospf area 0
        ip ospf cost 100
     '';
--- modules/hosts/p520/networking/routing.nix
+++ modules/hosts/p520/networking/routing.nix
@@ -1,10 +1,6 @@
 {
   flake.modules.nixos.p520 = {
     services.frr.config = ''
-      router ospf
-       distribute-list BLOCK-DEFAULT eno1.4
-       distribute-list BLOCK-DEFAULT enx2.8
-       distribute-list BLOCK-DEFAULT enx3.8
       int lo
        ip ospf passive
        ip ospf area 0
@@ -18,24 +14,6 @@
        ip ospf cost 100
        ip ospf area 0
       int enx3
-       ip ospf cost 100
-       ip ospf area 0
-      int eno1.3
-       ip ospf cost 1010
-       ip ospf area 0
-      int eno1.4
-       ip ospf cost 1000
-       ip ospf area 0
-      int enx2.7
-       ip ospf cost 110
-       ip ospf area 0
-      int enx2.8
-       ip ospf cost 100
-       ip ospf area 0
-      int enx3.7
-       ip ospf cost 110
-       ip ospf area 0
-      int enx3.8
        ip ospf cost 100
        ip ospf area 0
     '';
