7c024cad61187d290152ef1020fbfd249f56c404
Michael Buckley <michaelcbuckley@proton.me>
2025-12-26 09:04:28 -0500

    NVF/Python: added astral's ty

--- packages/nvf/modules/languages/python.nix
+++ packages/nvf/modules/languages/python.nix
@@ -1,11 +1,20 @@
 {
-  flake.modules.nvf.python = {
-    vim.languages = {
-      python = {
+  flake.modules.nvf.python = {pkgs, ...}: {
+    vim = {
+      extraPackages = [pkgs.ty];
+      extraPlugins.ty = {
+        package = pkgs.ty;
+        setup = ''
+          vim.lsp.enable('ty')
+        '';
+      };
+      languages.python = {
         enable = true;
         dap.enable = true;
         format.enable = true;
-        lsp.enable = true;
+        lsp = {
+          enable = true;
+        };
         treesitter.enable = true;
       };
     };
