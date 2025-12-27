72e981defa1ac0b453ede4baa96f0640be3dbbe7
Michael Buckley <michaelcbuckley@proton.me>
2025-12-27 09:59:13 -0500

    UFF/k3s: module clean up

--- modules/clusters/uff/modules/k3s.nix
+++ modules/clusters/uff/modules/k3s.nix
@@ -1,10 +1,15 @@
 {config, ...}: let
-  inherit (config.flake) modules hosts lib;
+  inherit (config.flake) modules hosts;
+  inherit (config.flake.lib.network) getAddress;
 in {
-  flake.modules.nixos.uff = {config, ...}: let
+  flake.modules.nixos.uff = {
+    config,
+    lib,
+    ...
+  }: let
     inherit (config.networking) hostName;
     # This IP is the locally evaluated host's
-    enu2 = lib.network.getAddress hosts.${hostName}.interfaces.enu2.ipv4;
+    enu2 = getAddress hosts.${hostName}.interfaces.enu2.ipv4;
   in {
     imports = with modules.nixos; [
       k3s
@@ -13,13 +18,17 @@ in {
 
     services.k3s = {
       tokenFile = config.sops.secrets.k3s_token.path;
+      clusterInit = hostName == "uff1";
       serverAddr = enu2;
-      extraFlags = [
-        "--node-name ${hostName}s"
-        "--node-ip ${enu2}"
-        "--advertise-address ${enu2}"
-        "--server https://${lib.network.getAddress hosts.uff1.interfaces.enu2.ipv4}:6443" # Serves as the bootstrapper
-      ];
+      extraFlags =
+        [
+          "--node-name ${hostName}s"
+          "--node-ip ${enu2}"
+          "--advertise-address ${enu2}"
+        ]
+        ++ lib.optionals (hostName != "uff1") [
+          "--server https://${getAddress hosts.uff1.interfaces.enu2.ipv4}:6443" # Serves as the bootstrapper
+        ];
     };
   };
 }
--- modules/clusters/uff/uff1/k3s.nix
+++
@@ -1,8 +0,0 @@
-{
-  flake.modules.nixos.uff1 = {
-    services.k3s = {
-      # UFF1 will spawn a cluster if it doesn't exist
-      clusterInit = true;
-    };
-  };
-}
