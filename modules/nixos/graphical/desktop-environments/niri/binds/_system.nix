dfe1a99fa67293a4164540779ffbfac11235220b
Michael Buckley <michaelcbuckley@proton.me>
2025-12-30 17:37:41 -0500

    Desktop/Niri: cleaned up unused legacy files and structure

--- modules/nixos/graphical/desktop-environments/niri/binds/_system.nix
+++
@@ -1,8 +0,0 @@
-''
-  Mod+Escape allow-inhibiting=false { toggle-keyboard-shortcuts-inhibit; }
-  Ctrl+Alt+Delete { quit skip-confirmation=true; }
-  Mod+Shift+P { power-off-monitors; }
-
-  Ctrl+Mod+semicolon { spawn-sh "systemctl poweroff"; }
-  Ctrl+Alt+Mod+semicolon { spawn-sh "systemctl reboot"; }
-''
