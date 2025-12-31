53c9239cfd8abf19e9c08a5825f60cada23ce285
Michael Buckley <michaelcbuckley@proton.me>
2025-12-31 09:02:52 -0500

    X570/Hardware: tweaked kernel modules for wifi

--- modules/hosts/x570/hardware/default.nix
+++ modules/hosts/x570/hardware/default.nix
@@ -13,12 +13,10 @@
     boot = {
       binfmt.emulatedSystems = ["aarch64-linux"];
       kernelPackages = pkgs.linuxPackages_zen;
-      kernelModules = ["kvm" "kvm-amd" "virtiofs" "9p" "9pnet_virtio"];
+      kernelModules = ["kvm" "kvm-amd" "virtiofs" "9p" "9pnet_virtio" "mt7921e"];
       kernelParams = [
         "amd_pstate=active" # AMD Power efficiency on Linux 6.3+
-        #"zfs.zfs_arc_max=17179869184" # 16GB max
-        #"zfs.zfs_arc_min=4294967296" # 4GB min
-        #"pcie_aspm=off"
+        "pcie_aspm=off"
       ];
       extraModulePackages = [];
       initrd = {
