1dc70abe9cb1fca208133ecd3937fcfe410722f8
Michael Buckley <michaelcbuckley@proton.me>
2026-01-02 23:25:23 -0500

    Michael/SSH: updated pubkeys

--- modules/nixos/users/michael/michael.nix
+++ modules/nixos/users/michael/michael.nix
@@ -18,8 +18,6 @@ in {
         // envars;
     };
   in {
-    environment.etc."ssh/authorized_keys.d/michael".source = config.sops.secrets.michael_ssh_pubkeys.path;
-
     users = {
       powerUsers.members = ["michael"];
       users.michael = {
@@ -27,6 +25,16 @@ in {
         isNormalUser = true;
         extraGroups = config.users.powerUsers.groups;
         shell = "${shell}${shell.shellPath}";
+        openssh.authorizedKeys.keys = [
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICVdKrhTH1OxUE/164StP+Iu5sOGcGEmpTyNvarAUn69AAAABHNzaDo="
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILqRzNVovg805v52UxSRSZxZu0RwUOPlTA7eSHhkDpbrAAAABHNzaDo="
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBDfIFxTCpYmNKivpDhn2YRPFHcL3QN4ztuimnA2o/+DAAAABHNzaDo="
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDmeP5ouNAD/hWUMq6DsZzLQCtOIh8rvQghX/huztRc8AAAAEXNzaDptaWNoYWVsQHlrNTcz"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPf4FlUmj1rIXYC80u1YtvYijLEC9HNsNoNNSli26yfiAAAAEXNzaDptaWNoYWVsQHlrODcw"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKAR+0i/+FR8pFkwiU7jubzaPrDJAhtX3qMpYrGVnVE/AAAAEXNzaDptaWNoYWVsQHlrOTAy"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHs226+TygXNbePufYVItfHQqTgAO8JChAigzEfQK4ftAAAAEXNzaDptaWNoYWVsQHlrMDcz"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAeSC5gh3pMr5v3p+/Dh3SMddk/n5Dr+srLS3OV3hEqOAAAABHNzaDo="
+        ];
       };
     };
   };
