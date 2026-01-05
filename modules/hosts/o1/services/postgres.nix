3bb63b311107a012d3f40e3154c44135b8c8c894
Michael Buckley <michaelcbuckley@proton.me>
2026-01-05 13:44:31 -0500

    O1/postgres: adjusted unit to start after k3s and added auth for pods

--- modules/hosts/o1/services/postgres.nix
+++ modules/hosts/o1/services/postgres.nix
@@ -6,8 +6,20 @@
   }: {
     networking.firewall.allowedTCPPorts = [5432];
 
+    systemd.services.postgresql.after = ["k3s.service"];
+
     services.postgresql = {
       enable = true;
+      authentication = ''
+        # TYPE  DATABASE        USER            ADDRESS                 METHOD
+        local   all             all                                     peer
+        host    all             all             127.0.0.1/32            scram-sha-256
+        host    all             all             ::1/128                 scram-sha-256
+
+        # Allow connections from k3s pod network
+        host    authentik       authentik       10.42.0.0/16            scram-sha-256
+        host    vaultwarden     vaultwarden     10.42.0.0/16            scram-sha-256
+      '';
       package = pkgs.postgresql_18;
       settings = {
         listen_addresses = lib.mkForce "127.0.0.1,10.42.0.1";
