09fda3c9f9d691f5bdd94f122c85ad79854027e5
Michael Buckley <michaelcbuckley@proton.me>
2026-01-05 09:58:40 -0500

    O1/vaultwarden: altered to include DB info necessary to connect

---
+++ modules/hosts/o1/k3s/vaultwarden/vaultwarden.nix
@@ -0,0 +1,41 @@
+{
+  flake.modules.nixos.o1 = {config, ...}: let
+    inherit (config.sops) placeholder;
+  in {
+    sops = {
+      secrets = {
+        "vaultwarden/postgres_password" = {};
+      };
+      templates = {
+        vaultwarden-secrets.content =
+          # yaml
+          ''
+            apiVersion: v1
+            kind: Secret
+            metadata:
+              name: vaultwarden-secrets
+              namespace: vaultwarden
+            stringData:
+              postgres_password: ${placeholder."vaultwarden/postgres_password"}
+              database_url: postgresql://vaultwarden:${placeholder."vaultwarden/postgres_password"}@localhost:5432/vaultwarden
+          '';
+      };
+    };
+    services = {
+      postgresql = {
+        ensureDatabases = ["vaultwarden"];
+        ensureUsers = [
+          {
+            name = "vaultwarden";
+            ensureDBOwnership = true;
+            ensureClauses = {createdb = true;};
+          }
+        ];
+      };
+      k3s.manifests = {
+        vaultwarden-secrets.source = config.sops.templates.vaultwarden-secrets.path;
+        vaultwarden-manifest.source = ./vaultwarden.yaml;
+      };
+    };
+  };
+}
