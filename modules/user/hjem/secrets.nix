f3f6841e0c335a2b3439acf823f26fb0121d5204
Michael Buckley <michaelcbuckley@proton.me>
2026-01-19 23:06:23 -0500

    Hjem/Secrets: removed unused module

--- modules/user/hjem/secrets.nix
+++
@@ -1,7 +0,0 @@
-# Where I will be keeping personal secrets
-{
-  # Gloriously empty, moved secrets to the system
-  # for now, will likely populate again later
-  flake.hjemConfigs.secrets = {
-  };
-}
