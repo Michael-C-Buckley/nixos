ead19acedfb59c94feb5206ec2af3274978051d1
Michael Buckley <michaelcbuckley@proton.me>
2026-01-21 18:40:58 -0500

    Hjem/Helium: add widevine support

--- modules/hjem/configs/applications/helium.nix
+++ modules/hjem/configs/applications/helium.nix
@@ -10,6 +10,12 @@ in {
     hjem.users.michael = {
       packages = [flake.packages.${pkgs.stdenv.hostPlatform.system}.helium];
 
+      # Add widevine support, inspired from this comment:
+      # https://github.com/imputnet/helium/issues/116#issuecomment-3668370766
+      xdg.config.files."net.imput.helium/WidevineCdm/latest-component-updated-widevine-cdm".text = ''
+        {"Path":"${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm"}
+      '';
+
       impermanence = {
         persist.directories = lib.optionals config.custom.impermanence.home.enable [".config/net.imput.helium"];
         cache.directories = [".cache/net.imput.helium"];
