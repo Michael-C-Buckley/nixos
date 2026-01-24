2982aaebacd9e345ed69393b41201dfb70f13631
Michael Buckley <michaelcbuckley@proton.me>
2026-01-24 13:37:35 -0500

    X570/Wireguard: turn them back on

--- modules/hosts/x570/networking/wireguard.nix
+++ modules/hosts/x570/networking/wireguard.nix
@@ -10,16 +10,13 @@ in {
     inherit (flake.lib.wireguard) genInterface;
     inherit (lib) mapAttrs' nameValuePair;
 
-    # Disable these for now
-    enable = false;
-
     interfaces = {
       mt1 = ["192.168.240.242/30"];
       mt4 = ["192.168.202.10/24"];
     };
   in {
     systemd.services = mapAttrs' (name: ipAddresses:
-      nameValuePair "wireguard-${name}" (genInterface {inherit enable config pkgs name ipAddresses;}))
+      nameValuePair "wireguard-${name}" (genInterface {inherit config pkgs name ipAddresses;}))
     interfaces;
   };
 }
