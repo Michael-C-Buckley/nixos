926b9bbb7472d0cbe6db769e75c5f628ab979185
Michael Buckley <michaelcbuckley@proton.me>
2026-01-26 07:57:20 -0500

    Root/SSH: added my keys to root

--- modules/hjem/root.nix
+++ modules/hjem/root.nix
@@ -8,6 +8,7 @@ in {
     pkgs,
     ...
   }: let
+    inherit (config.users.users) michael shawn;
     shell = flake.wrappers.mkFish {
       inherit pkgs;
       env = config.custom.shell.environmentVariables;
@@ -16,6 +17,7 @@ in {
     users.users.root = {
       hashedPasswordFile = "/etc/secrets/users/root/hashedPassword";
       shell = "${shell}${shell.shellPath}";
+      openssh.authorizedKeys.keys = michael.openssh.authorizedKeys.keys ++ shawn.openssh.authorizedKeys.keys;
     };
 
     hjem.users.root = {
