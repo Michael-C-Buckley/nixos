890027f382df920590dc3784a37231db8946a071
Michael Buckley <michaelcbuckley@proton.me>
2026-02-03 09:19:57 -0500

    Debian/SSH: fixed the agents sockets from colliding

--- modules/home/common/wsl.nix
+++ modules/home/common/wsl.nix
@@ -18,6 +18,7 @@ in {
     ];
 
     systemd.user.services = lib.mkIf config.custom.systemd.use {
+      # Standard SSH agent for normal activity when passing yubikey through USBIPD
       ssh-agent = {
         unit = {
           Description = "SSH Agent Socket";
@@ -26,11 +27,13 @@ in {
         };
         Service = {
           ExecStart = "${pkgs.openssh}/bin/ssh-agent -a /home/michael/.ssh/ssh-agent.sock";
+          ExecStop = "${pkgs.coreutils}/bin/rm -r /home/michael/.ssh/ssh-agent.sock";
           Type = "oneshot";
           RemainAfterExit = true;
         };
         Install.WantedBy = ["default.target"];
       };
+      # WSL2 agent for when not passing a yubikey
       wsl2-ssh-agent = {
         unit = {
           Description = "WSL2 SSH Agent Bridge";
@@ -39,6 +42,7 @@ in {
         };
         Service = {
           ExecStart = "${lib.getExe pkgs.wsl2-ssh-agent} --verbose --foreground --socket=/home/michael/.ssh/wsl2-ssh-agent.sock";
+          ExecStop = "${pkgs.coreutils}/bin/rm -r /home/michael/.ssh/wsl2-ssh-agent.sock";
           Restart = "on-failure";
         };
         Install.WantedBy = ["default.target"];
