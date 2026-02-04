d198eb1513d5f14d2bde47421d7b7481c6b32518
Michael Buckley <michaelcbuckley@proton.me>
2026-02-04 09:40:12 -0500

    Debian/WSL: added minor `/etc` files

--- modules/system/debian/system.nix
+++ modules/system/debian/system.nix
@@ -3,5 +3,22 @@
     system-manager.allowAnyDistro = true;
 
     nixpkgs.hostPlatform = "x86_64-linux";
+
+    environment.etc = {
+      "resolv.conf".text = ''
+        nameserver 1.1.1.1
+        nameserver 9.9.9.9
+      '';
+
+      "wsl.conf".text = ''
+        [boot]
+        systemd=true
+
+        [network]
+        generateHosts=false
+        generateResolvConf=false
+        hostname=debian
+      '';
+    };
   };
 }
