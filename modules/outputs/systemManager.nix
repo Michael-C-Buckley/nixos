f080fb5d1ff72c15a9bf9d5aca1d3d96ef3c6391
Michael Buckley <michaelcbuckley@proton.me>
2026-02-08 20:25:06 -0500

    Systems/Docs: described intent on system-manager outputs for my uses

--- modules/outputs/systemManager.nix
+++ modules/outputs/systemManager.nix
@@ -1,3 +1,11 @@
+# WARNING: Highly Unstable and Experimental
+# System-Manager is a new tool from Numtide for managing the base system
+# of non-NixOS systems somewhat conceptually to what Home-Manager does
+# for users
+#
+# These are *extremely* experimental and I'm just testing
+# some capabilities and rather crazy ideas, they should
+# not be taken seriously
 {
   config,
   inputs,
