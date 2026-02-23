cfc20e60482a9ee6559c9361abdf8d771523c398
Michael Buckley <michaelcbuckley@proton.me>
2026-02-23 17:00:03 -0500

    Graphics/Greetd: removed ly to move exclusively to greetd with agreety

---
+++ modules/nixos/graphical/greetd.nix
@@ -0,0 +1,38 @@
+# Baseline Greetd used in the linux preset
+# This is specific for my graphical hosts extending it
+{
+  flake.modules.nixos.greetd = {pkgs, ...}: {
+    config,
+    lib,
+    ...
+  }: {
+    options.custom.greetd = {
+      defaultCommand = lib.mkOption {
+        type = lib.types.str;
+        default = "start-hyprland";
+        description = "The default session command for greetd and auto-login";
+      };
+    };
+
+    config = {
+      hardware.graphics.enable = true;
+      services.xserver.enable = true;
+
+      # Session on first login which would use auto-login
+      services.greetd = {
+        enable = true;
+        useTextGreeter = true;
+        settings = {
+          default_session = {
+            command = "${pkgs.greetd}/bin/agreety";
+            user = "greeter";
+          };
+          initial_session = {
+            user = "michael";
+            command = config.custom.greetd.defaultCommand;
+          };
+        };
+      };
+    };
+  };
+}
