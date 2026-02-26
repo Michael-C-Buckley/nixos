6b10df2f1eaeab2bf05542ca443a130828cff352
Michael Buckley <michaelcbuckley@proton.me>
2026-02-26 18:22:07 -0500

    Michael/Envs: added a few more environments

--- packages/buildEnv/default.nix
+++ packages/buildEnv/default.nix
@@ -4,27 +4,47 @@
     pkgs,
     system,
     ...
-  }: {
-    packages.fullEnv = pkgs.buildEnv {
-      name = "Michael's Environment";
-      paths = with config.flake.packages.${system};
-        [
+  }: let
+    local = config.flake.packages.${system};
+  in {
+    packages = {
+      # Intended for lightweight hosts that I want something on
+      slimEnv = pkgs.buildEnv {
+        name = "Michael's slim env";
+        paths = with local; [
+          nushell
+        ];
+      };
+      termEnv = pkgs.buildEnv {
+        # Extra packages for CLI hosts like development servers
+        name = "Michael's terminal env";
+        paths = with local; [
           nushell
           helix
-          vscode
-          zeditor
-          kitty
-          helium
-        ]
-        ++ (with pkgs; [
-          # Communication
-          legcord
-          signal-desktop
-          materialgram
+        ];
+      };
+      fullEnv = pkgs.buildEnv {
+        # Fully loaded graphical environments
+        name = "Michael's full env";
+        paths = with local;
+          [
+            nushell
+            helix
+            vscode
+            zeditor
+            kitty
+            helium
+          ]
+          ++ (with pkgs; [
+            # Communication
+            legcord
+            signal-desktop
+            materialgram
 
-          # Productivity
-          novelwriter
-        ]);
+            # Productivity
+            novelwriter
+          ]);
+      };
     };
   };
 }
