aefba9f15a16c9f77db4f33dc9270050d15817d5
Michael Buckley <michaelcbuckley@proton.me>
2026-03-10 23:19:20 -0400

    NixOS/Libvirt: swapped libvirt to use a common secret for the required secret now

--- modules/nixos/virtualization/libvirt.nix
+++ modules/nixos/virtualization/libvirt.nix
@@ -8,13 +8,6 @@
     inherit (lib) optionals;
     gfx = config.hardware.graphics.enable;
   in {
-    custom.impermanence = lib.mkIf config.custom.impermanence.var.enable {
-      cache = {
-        directories = ["/var/lib/libvirt"];
-        allUsers.directories = optionals gfx [".cache/virt-manager"];
-      };
-    };
-
     environment.systemPackages = with pkgs;
       optionals gfx [
         virt-manager
@@ -23,6 +16,14 @@
       ];
 
     users.powerUsers.groups = ["libvirtd"];
+
+    # Libvirt 10.2+ requires a systemd-creds passed credential
+    # it can be just randomly created but I've gone and actually provisioned and declared it
+    systemd.services.libvirtd.serviceConfig.LoadCredential = [
+      "secrets-encryption-key:${config.sops.secrets.libvirt-key.path}"
+    ];
+    # I use 1 key among the various hosts, it's low priority but persisted in case I ever need to access anything
+    sops.secrets.libvirt-key.sopsFile = "/etc/secrets/common/common.yaml";
 
     virtualisation.libvirtd = {
       enable = true;
