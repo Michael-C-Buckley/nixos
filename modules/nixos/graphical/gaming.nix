ea0320f4381c832405a0d2fe42b5bd82b75d9ff7
Michael Buckley <michaelcbuckley@proton.me>
2026-03-26 08:43:11 -0400

    NixOS/Steam: added override on steam to disable HW acceleration for a more consistent and functional experience

--- modules/nixos/graphical/gaming.nix
+++ modules/nixos/graphical/gaming.nix
@@ -1,11 +1,15 @@
 {
-  flake.modules.nixos.gaming = {
+  flake.modules.nixos.gaming = {pkgs, ...}: {
     custom.impermanence.cache.allUsers.directories = [".local/share/Steam"];
 
     hardware.steam-hardware.enable = true;
 
     programs.steam = {
       enable = true;
+      # Disabling hardware acceleration allows it to work more consistently such as on Niri
+      package = pkgs.steam.override {
+        extraArgs = "-cef-disable-gpu -cef-disable-gpu-compositing";
+      };
       remotePlay.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
     };
