d57473ec1754f251356bff2a327c374731ca5cd3
Michael Buckley <michaelcbuckley@proton.me>
2026-03-30 09:37:05 -0400

    Flake/Systems: removed system manager as it will be spun off into a separate project

--- modules/system/packages.nix
+++
@@ -1,7 +0,0 @@
-{config, ...}: {
-  flake.modules.systemManager.packages = {pkgs, ...}: {
-    environment.systemPackages = [
-      (pkgs.callPackage "${config.flake.npins.system-manager}/package.nix" {})
-    ];
-  };
-}
