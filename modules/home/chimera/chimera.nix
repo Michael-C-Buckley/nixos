f13d635fece13a63461ceb5121cc11f17088a9ad
Michael Buckley <michaelcbuckley@proton.me>
2026-03-31 12:50:36 -0400

    Home/Chimera: add blueman within nix to avoid GUI issues when using nix-wrapped niri

--- modules/home/chimera/chimera.nix
+++ modules/home/chimera/chimera.nix
@@ -1,11 +1,14 @@
 {config, ...}: {
-  flake.modules.homeManager.chimera = {
+  flake.modules.homeManager.chimera = {pkgs, ...}: {
     imports = with config.flake.modules.homeManager; [
       default
       helium
       chimera-ssh-agent
       chimera-zed-deploy
     ];
-    home.stateVersion = "25.11";
+    home = {
+      packages = with pkgs; [blueman];
+      stateVersion = "25.11";
+    };
   };
 }
