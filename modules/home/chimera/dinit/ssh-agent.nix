ad7054cc008626d574d602022762cd9c42fcd24f
Michael Buckley <michaelcbuckley@proton.me>
2026-03-31 20:04:14 -0400

    Home/Chimera: fixed the SSH agent logic

--- modules/home/chimera/dinit/ssh-agent.nix
+++ modules/home/chimera/dinit/ssh-agent.nix
@@ -9,6 +9,8 @@
 
     wrapper = pkgs.writeShellScriptBin "dinit-ssh-wrapper" ''
       dinitctl --user setenv SSH_AUTH_SOCK=${SOCK}
+      /usr/bin/rm -f ${SOCK}
+      /usr/bin/rm -f ${pidFile}
 
       /usr/bin/ssh-agent -D -a ${SOCK} &
       /usr/bin/echo $! > "${pidFile}"
@@ -16,15 +18,17 @@
 
       # Wait for socket to appear, then add keys
       while [ ! -S ${SOCK} ]; do sleep 0.1; done
-      /usr/bin/ssh-add /home/michael/.ssh/id_*
+      for f in /home/michael/.ssh/id_*; do
+        [[ -f "$f" && ! "$f" =~ \.pub$ ]] && /usr/bin/ssh-add "$f"
+      done
 
       exit 0
     '';
 
     kill-wrapper = pkgs.writeShellScriptBin "dinit-ssh-kill" ''
       /usr/bin/kill $(cat ${pidFile})
-      /usr/bin/rm ${pidFile}
-      /usr/bin/rm ${wrapper}
+      /usr/bin/rm -f ${pidFile}
+      /usr/bin/rm -f ${SOCK}
     '';
   in {
     home.file = {
