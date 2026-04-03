8a797457b8bca477904eb295bb6f8467bf926208
Michael Buckley <michaelcbuckley@proton.me>
2026-04-03 09:05:49 -0400

    Packages/Nordzy: limited the packages to linux as that is the only actual source for them

--- packages/nordzy-cursor.nix
+++ packages/nordzy-cursor.nix
@@ -1,24 +1,29 @@
-{
-  perSystem = {pkgs, ...}: {
-    # The full package is ~900MB and I use literally only 1 color
-    # So, debloat by hitching all the rest from nixpkgs
-    # Original source was from `pkgs.nordzy-cursor-theme`
-    packages.nordzy-cursor = pkgs.stdenv.mkDerivation {
-      pname = "nordzy-cursor-white";
-      version = "1.0";
+{lib, ...}: {
+  perSystem = {
+    pkgs,
+    system,
+    ...
+  }:
+    lib.optionalAttrs (lib.hasSuffix "linux" system) {
+      # The full package is ~900MB and I use literally only 1 color
+      # So, debloat by hitching all the rest from nixpkgs
+      # Original source was from `pkgs.nordzy-cursor-theme`
+      packages.nordzy-cursor = pkgs.stdenv.mkDerivation {
+        pname = "nordzy-cursor-white";
+        version = "1.0";
 
-      src = pkgs.nordzy-cursor-theme;
+        src = pkgs.nordzy-cursor-theme;
 
-      installPhase = ''
-        mkdir -p $out/share/icons
-        cp -r share/icons/Nordzy-cursors-white $out/share/icons/Nordzy-cursors-white
-        cp -r share/icons/Nordzy-hyprcursors-white $out/share/icons/Nordzy-hyprcursors-white
-      '';
+        installPhase = ''
+          mkdir -p $out/share/icons
+          cp -r share/icons/Nordzy-cursors-white $out/share/icons/Nordzy-cursors-white
+          cp -r share/icons/Nordzy-hyprcursors-white $out/share/icons/Nordzy-hyprcursors-white
+        '';
 
-      meta = {
-        description = "Nordzy Cursor - White";
-        platforms = pkgs.lib.platforms.linux;
+        meta = {
+          description = "Nordzy Cursor - White";
+          platforms = pkgs.lib.platforms.linux;
+        };
       };
     };
-  };
 }
