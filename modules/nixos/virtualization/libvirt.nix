aca50e2465f15fa2fb2c6b8994d48b5a718cf742
Michael Buckley <michaelcbuckley@proton.me>
2026-04-04 15:38:29 -0400

    NixOS/Journald: libvirt logging namespace added

--- modules/nixos/virtualization/libvirt.nix
+++ modules/nixos/virtualization/libvirt.nix
@@ -1,38 +1,61 @@
-{
+{config, ...}: let
+  inherit (config) flake;
+  libvirtUnits = config.flake.custom.lib.mkJournalNamespace "libvirt" [
+    "libvirtd"
+    "libvirt-guests"
+    "virtlogd"
+    "virtlockd"
+  ];
+in {
   flake.modules.nixos.libvirt = {
     config,
     lib,
     pkgs,
     ...
   }: let
-    inherit (lib) optionals;
+    inherit (config.custom.journald) libvirt;
     gfx = config.hardware.graphics.enable;
   in {
-    environment.systemPackages = with pkgs;
-      optionals gfx [
-        virt-manager
-        virt-viewer
-        tigervnc
-      ];
+    options.custom.journald.libvirt = flake.custom.lib.mkJournalOptions;
 
-    users.powerUsers.groups = ["libvirtd"];
+    config = {
+      environment = {
+        etc."systemd/journald@libvirt.conf".text = flake.custom.lib.mkJournalEtcFile libvirt;
+        systemPackages = with pkgs;
+          lib.optionals gfx [
+            virt-manager
+            virt-viewer
+            tigervnc
+          ];
+      };
 
-    # Libvirt 10.2+ requires a systemd-creds passed credential
-    # it can be just randomly created but I've gone and actually provisioned and declared it
-    systemd.services.libvirtd.serviceConfig.LoadCredential = [
-      "secrets-encryption-key:${config.sops.secrets.libvirt-key.path}"
-    ];
-    # I use 1 key among the various hosts, it's low priority but persisted in case I ever need to access anything
-    sops.secrets.libvirt-key.sopsFile = "/etc/secrets/common/common.yaml";
+      users.powerUsers.groups = ["libvirtd"];
 
-    virtualisation.libvirtd = {
-      enable = true;
-      allowedBridges = lib.mkDefault (builtins.attrNames config.networking.bridges);
-      parallelShutdown = 5;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
+      systemd.services =
+        # Merge secret with the logging update
+        lib.recursiveUpdate
+        {
+          # Libvirt 10.2+ requires a systemd-creds passed credential
+          # it can be just randomly created but I've gone and actually provisioned and declared it
+          libvirtd.serviceConfig.LoadCredential = [
+            "secrets-encryption-key:${config.sops.secrets.libvirt-key.path}"
+          ];
+        }
+        libvirtUnits;
+
+      # I use 1 key among the various hosts, it's low priority but persisted in case I ever need to access anything
+      # The default NixOS has is to generate fresh random ones, I'll use static instead
+      sops.secrets.libvirt-key.sopsFile = "/etc/secrets/common/common.yaml";
+
+      virtualisation.libvirtd = {
+        enable = true;
+        allowedBridges = lib.mkDefault (builtins.attrNames config.networking.bridges);
+        parallelShutdown = 5;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          swtpm.enable = true;
+        };
       };
     };
   };
