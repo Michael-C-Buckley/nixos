35303979a886c8c42535fa1617a828136f836fd7
Michael Buckley <michaelcbuckley@proton.me>
2026-04-06 09:45:06 -0400

    Darwin/Hjem: added ghostty as the nixpkgs wrapped edition does not launch correctly

--- modules/darwin/darwin.nix
+++ modules/darwin/darwin.nix
@@ -4,10 +4,12 @@
   ...
 }: let
   inherit (config.flake.modules) darwin;
+  inherit (config.flake.custom) hjemConfigs;
 in {
   flake.modules.darwin.default = {
     imports = builtins.attrValues {
       inherit (darwin) nix;
+      inherit (hjemConfigs) ghostty;
     };
     nixpkgs.config.allowUnfree = true;
 
