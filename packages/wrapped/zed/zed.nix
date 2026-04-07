5e27ff06daa05aa1e8789f9f48dc9615673b6eb6
Michael Buckley <michaelcbuckley@proton.me>
2026-04-07 06:53:03 -0400

    Wrapped/Zed: slashed unneeded aspects of the wrapper and exposed packages so that they can be added to my user on macos

--- packages/wrapped/zed/zed.nix
+++ packages/wrapped/zed/zed.nix
@@ -1,34 +1,13 @@
-# Just a simple way to put some tools in the Zed path
-# This also bwraps the linux variant
 {
   config,
   lib,
   ...
 }: let
-  inherit (config.flake.custom.wrappers) mkZedConfig;
+  inherit (config.flake.custom.wrappers) mkZedConfig mkZedPkgList;
 in {
-  flake.custom.wrappers.mkZedConfig = {
-    pkgs,
-    extraConfig ? {},
-  }:
-    pkgs.runCommand "zed-settings.json" {
-      nativeBuildInputs = [pkgs.biome];
-      json = builtins.toJSON (lib.recursiveUpdate (import ./_settings.nix) extraConfig);
-      passAsFile = ["json"];
-    } ''
-      biome format --stdin-file-path settings.json < "$jsonPath" > $out
-    '';
-
-  perSystem = {
-    pkgs,
-    lib,
-    system,
-    ...
-  }: let
-    runtimeEnv = pkgs.buildEnv {
-      name = "zed-runtime-env";
-      pathsToLink = ["/bin"];
-      paths = with pkgs; [
+  flake.custom.wrappers = {
+    mkZedPkgList = pkgs:
+      with pkgs; [
         # Nix
         alejandra
         nil
@@ -52,63 +31,41 @@ in {
         # Yaml
         yaml-language-server
       ];
-    };
-    isMac = lib.hasSuffix "darwin" pkgs.stdenv.hostPlatform.system;
-
-    fonts =
-      if isMac
-      then ""
-      else "--set FONTCONFIG_FILE ${pkgs.makeFontsConf {fontDirectories = with pkgs; [ibm-plex lilex];}}";
-
-    macScript = pkgs.writeShellScriptBin "zed-mac-script" ''
-      export __PATH_HELPER_LOADED=1
-      export PATH="${runtimeEnv}/bin:$PATH"
-      exec -a "$0" "${lib.getExe pkgs.zed-editor}" "$@"
-    '';
-
-    localPkg = pkgs.symlinkJoin {
-      name = "zeditor";
-      paths = [pkgs.zed-editor];
-      nativeBuildInputs = [pkgs.makeWrapper];
-      meta.mainProgram = "zeditor";
-      postBuild = ''
-        wrapProgram $out/bin/zeditor \
-        --prefix PATH : ${runtimeEnv}/bin ${fonts}
-
-        cp ${lib.getExe macScript} $out/bin/zed-mac
-
-
-        ${
-          if isMac
-          then "sed -i '1s|^#!.*|#!/bin/bash -e|' $out/bin/zeditor"
-          else ""
-        }
+    mkZedConfig = {
+      pkgs,
+      extraConfig ? {},
+    }:
+      pkgs.runCommand "zed-settings.json" {
+        nativeBuildInputs = [pkgs.biome];
+        json = builtins.toJSON (lib.recursiveUpdate (import ./_settings.nix) extraConfig);
+        passAsFile = ["json"];
+      } ''
+        biome format --stdin-file-path settings.json < "$jsonPath" > $out
       '';
+  };
+
+  perSystem = {pkgs, ...}: let
+    runtimeEnv = pkgs.buildEnv {
+      name = "zed-runtime-env";
+      pathsToLink = ["/bin"];
+      paths = mkZedPkgList {inherit pkgs;};
     };
-
-    jail = (import "${config.flake.npins.jail}/lib").init pkgs;
-    inherit (jail.combinators) gui gpu readonly rw-bind noescape;
-    homeBind = path: (rw-bind (noescape path) (noescape path));
-
-    # These are my directories I use, you likely want to adjust this
-    bwrapFeatures = [
-      gui
-      gpu
-      (readonly "/nix/store")
-      (homeBind "~/nixos") # where I keep my system flake
-      (homeBind "~/projects") # The rest of where I usually keep projects
-      (homeBind "~/.config/zed")
-      (homeBind "~/.cache/zed")
-      (homeBind "~/.local/share/zed")
-    ];
   in {
-    packages =
-      {
-        zeditor = localPkg;
-        zedConfig = mkZedConfig {inherit pkgs;};
-      }
-      // lib.optionalAttrs (lib.hasSuffix "linux" system) {
-        zeditor-jailed = jail "zeditor" localPkg bwrapFeatures;
+    packages = {
+      zeditor = pkgs.symlinkJoin {
+        name = "zeditor";
+        paths = [pkgs.zed-editor];
+        nativeBuildInputs = [pkgs.makeWrapper];
+        meta.mainProgram = "zeditor";
+        postBuild = ''
+          wrapProgram $out/bin/zeditor \
+          --prefix PATH : ${runtimeEnv}/bin \
+          --set FONTCONFIG_FILE ${pkgs.makeFontsConf {fontDirectories = with pkgs; [ibm-plex lilex];}}
+        '';
       };
+
+      zedConfig = mkZedConfig {inherit pkgs;};
+      zedPkgs = mkZedPkgList {inherit pkgs;};
+    };
   };
 }
