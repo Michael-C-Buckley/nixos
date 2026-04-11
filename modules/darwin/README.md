db49fa8ace3a438fa35a8a0ee4030b245632a0ec
Michael Buckley <michaelcbuckley@proton.me>
2026-04-10 20:53:27 -0400

    Darwin/M4: tweaked default additional packages

--- modules/darwin/README.md
+++ modules/darwin/README.md
@@ -3,3 +3,11 @@
 I am a new Nix-Darwin user because I have an M1 mini and want to get at least somewhat declarative.
 
 Be warned - I am new specifically to nix-darwin and I am still learning the hardway. I don't recommend copying any habits or suggestions here.
+
+## M1
+
+A simple M1 mini I have been using for a short bit and my introduction to daily driving MacOS.
+
+## M4
+
+My work macbook, which gets generally most of my development setup.
--- modules/darwin/m4/default.nix
+++ modules/darwin/m4/default.nix
@@ -1,6 +1,22 @@
-{
-  flake.modules.darwin.m4 = {
+{config, ...}: {
+  flake.modules.darwin.m4 = {pkgs, ...}: {
     system.stateVersion = 6;
     nixpkgs.hostPlatform = "aarch64-darwin";
+
+    hjem.users.michael = {
+      packages = builtins.attrValues {
+        inherit
+          (config.flake.packages.${pkgs.stdenv.hostPlatform.system})
+          ns
+          helix
+          ;
+
+        nushell = config.flake.custom.wrappers.mkNushell {
+          inherit pkgs;
+          # Work has a long verbose hostname, I just call it M4 for simplicity
+          extraAliases = {nhs = "nh darwin switch /Users/michael/nixos#m4";};
+        };
+      };
+    };
   };
 }
