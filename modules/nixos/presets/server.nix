139437443f1a1ce6b6b1cd3b2286b6579d1d52c2
Michael Buckley <michaelcbuckley@proton.me>
2026-05-01 07:28:05 -0400

    Presets/Server: moved the servers to LTS and ensured gnome keyring is not running

--- modules/nixos/presets/server.nix
+++ modules/nixos/presets/server.nix
@@ -1,11 +1,7 @@
 {config, ...}: let
   inherit (config.flake) modules;
 in {
-  flake.modules.nixos.serverPreset = {
-    pkgs,
-    lib,
-    ...
-  }: {
+  flake.modules.nixos.serverPreset = {pkgs, ...}: {
     imports = builtins.attrValues {
       inherit
         (modules.nixos)
@@ -26,13 +22,16 @@ in {
         ;
     };
 
-    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+    #boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_6_19; # Deprecated, will fall back to 6.18 LTS
 
     environment.systemPackages = [
       pkgs.python3
       config.flake.packages.${pkgs.stdenv.hostPlatform.system}.vim
     ];
 
-    services.resolved.enable = false;
+    services = {
+      resolved.enable = false;
+      gnome.gnome-keyring.enable = false;
+    };
   };
 }
