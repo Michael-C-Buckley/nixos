fb3f6df5f06820856fb4126f9ddce6c69747b4ee
Michael Buckley <michaelcbuckley@proton.me>
2026-05-01 06:38:43 -0400

    NixOS/Nix: removed the generic module and moved it back under nixos

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -1,11 +1,48 @@
-{config, ...}: {
-  flake.modules.nixos.nix = {pkgs, ...}: {
-    imports = [config.flake.modules.generic.nix];
+{inputs, ...}: {
+  flake.modules.nixos.nix = {
+    pkgs,
+    lib,
+    ...
+  }: {
     nix = {
       package = pkgs.nixVersions.latest;
+      # Disable channels and add the flake inputs to the registry
+      channel.enable = false;
+      registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+
+      extraOptions = ''
+        # Optimizing cache requests with faster failing
+        connect-timeout          = 5
+        stalled-download-timeout = 30
+        download-attempts        = 3
+        http-connections         = 50
+        max-substitution-jobs    = 32
+        fallback                 = true
+      '';
+
       settings = {
-        trusted-users = ["root" "@wheel"];
-        allowed-users = ["root" "@wheel"];
+        warn-dirty = false;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+          "pipe-operators"
+        ];
+
+        # Clear the default registry and add the inputs to the nix path
+        nix-path = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+        flake-registry = "";
+
+        substituters = [
+          "https://cache.nixos-cuda.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        ];
+        trusted-users = ["root" "@wheel" "builder"];
+        allowed-users = ["root" "@wheel" "builder"];
       };
     };
   };
