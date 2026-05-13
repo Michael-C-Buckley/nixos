f1d8c5c0745c89e935d1a6fb45c6736007a04f03
Michael Buckley <michaelcbuckley@proton.me>
2026-05-13 19:34:31 -0400

    NixOS/SSH: just go with teh standard agent

--- modules/nixos/users/michael/ssh-agent.nix
+++ modules/nixos/users/michael/ssh-agent.nix
@@ -6,44 +6,8 @@
   }: let
     link_script = pkgs.writers.writePython3 "ssh_link_script" {} ./ssh_link.py;
   in {
-    # Simplify my custom agent with a simple user agent
-    hjem.users.michael = {
-      environment.sessionVariables = {
-        SSH_AUTH_SOCK = "/home/michael/.ssh/agent/standard.sock";
-      };
-      systemd.services = {
-        ssh-agent-internal = let
-          internal = "/home/michael/.ssh/agent/standard.sock";
-        in {
-          description = "SSH Agent for internal hosts";
-          wantedBy = ["multi-user.target"];
-          after = ["local-fs.target"];
+    programs.ssh.startAgent = true;
 
-          path = with pkgs; [openssh busybox yubikey-manager];
-
-          preStart = ''
-            mkdir -p /home/michael/.ssh/agent
-            rm -f ${internal}
-          '';
-
-          postStart = ''
-            ${link_script}
-            ssh-add /home/michael/.ssh/active/internal
-            ssh-add /home/michael/.ssh/active/external
-            ssh-add /home/michael/.ssh/active/github
-            ssh-add /home/michael/.ssh/active/signing
-          '';
-
-          serviceConfig = {
-            Type = "forking";
-            Environment = "SSH_AUTH_SOCK=/home/michael/.ssh/agent/standard.sock";
-            ExecStart = ''
-              ${pkgs.openssh}/bin/ssh-agent -a ${internal}
-            '';
-          };
-        };
-      };
-    };
     services = {
       gnome.gcr-ssh-agent.enable = false;
       udev.extraRules = ''
