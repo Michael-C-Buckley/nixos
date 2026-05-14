8af2af4d001a39fbe19c819fa931767d5e21f7c2
Michael Buckley <michaelcbuckley@proton.me>
2026-05-14 15:57:03 -0400

    BuildEnv/Mac: added my custom ns script

--- packages/buildEnv/mac.nix
+++ packages/buildEnv/mac.nix
@@ -1,17 +1,26 @@
-{
+{config, ...}: {
   perSystem = {pkgs, ...}: {
     packages.macEnv = pkgs.buildEnv {
       # Packages I use on macs that don't ship my whole env
       name = "mac-env";
-      paths = with pkgs; [
-        nix-tree
-        nix-direnv
+      paths = builtins.attrValues {
+        inherit
+          (pkgs)
+          # Openssh from nixpkgs has better libfido2 support
+          openssh
+          # Nix Tools
+          nix-tree
+          nix-direnv
+          alejandra
+          nil
+          nixd
+          ;
 
-        # Nix Tools
-        alejandra
-        nil
-        nixd
-      ];
+        inherit
+          (config.flake.packages.${pkgs.stdenv.hostPlatform.system})
+          ns
+          ;
+      };
     };
   };
 }
