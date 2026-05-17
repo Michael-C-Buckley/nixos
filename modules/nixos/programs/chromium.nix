43564cdf30fcefd77d55d3d3cc8c3a42cfc50d08
Michael Buckley <michaelcbuckley@proton.me>
2026-05-17 19:10:43 -0400

    NixOS/Shawn: changed shell to zsh

--- modules/nixos/programs/chromium.nix
+++ modules/nixos/programs/chromium.nix
@@ -2,12 +2,6 @@
   flake.modules.nixos.chromiumPolicies = {
     programs.chromium = {
       enable = true; # merely enables policies, does not install anything
-      extensions = [
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Darkreader
-        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
-        "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-        "chphlpgkkbolifaimnlloiipkdnihall" # OneTab
-      ];
       extraOpts = {
         PasswordManagerEnabled = false;
         SpellcheckEnabled = true;
--- modules/nixos/users/shawn/shawn.nix
+++ modules/nixos/users/shawn/shawn.nix
@@ -1,23 +1,18 @@
-{config, ...}: let
-  inherit (config) flake;
-in {
+{
   flake.modules.nixos.shawn = {
     config,
     pkgs,
     ...
-  }: let
-    shell = flake.custom.wrappers.mkFish {
-      inherit pkgs;
-      env = config.custom.shell.environmentVariables;
-    };
-  in {
+  }: {
+    programs.zsh.enable = true;
+
     users = {
       powerUsers.members = ["shawn"];
       users.shawn = {
         initialHashedPassword = "$6$qqfYcXgaiZknCMEO$vQWbS.ojgq1Z278tlMxXuHXwwXIbvnuYifD7InXpvzdg.jLYcMoawE1GGtJzEVGJGn80PLfT1cMVMlcsaX3h5.";
         isNormalUser = true;
         extraGroups = config.users.powerUsers.groups;
-        shell = "${shell}${shell.shellPath}";
+        shell = pkgs.zsh;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHGkt46r4YJfNSji+DpfwlU/kCRVBwLAbUFbvyN2Ax1"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIW/Nlbf1An3q7yoWY5D6wYSm8Y5tWFtTMJm0pImjK1g"
