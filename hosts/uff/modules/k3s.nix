f796e4420d3d0070fc7f10601e71e9044518e1a1
Michael Buckley <michaelcbuckley@proton.me>
2026-05-18 17:04:31 -0400

    Hosts/UFFs: converted to the non-dendritic host format

--- hosts/uff/modules/k3s.nix
+++ hosts/uff/modules/k3s.nix
@@ -1,38 +1,35 @@
-{config, ...}: let
-  inherit (config.flake) modules;
-  inherit (config.flake.custom) hosts;
-  inherit (config.flake.custom.lib.network) getAddress;
+{
+  flake,
+  config,
+  lib,
+  ...
+}: let
+  inherit (config.networking) hostName;
+  inherit (flake.custom) hosts;
+  inherit (flake.custom.lib.network) getAddress;
+  # This IP is the locally evaluated host's
+  enu2 = getAddress hosts.${hostName}.interfaces.enu2.ipv4;
 in {
-  flake.modules.nixos.uff = {
-    config,
-    lib,
-    ...
-  }: let
-    inherit (config.networking) hostName;
-    # This IP is the locally evaluated host's
-    enu2 = getAddress hosts.${hostName}.interfaces.enu2.ipv4;
-  in {
-    imports = with modules.nixos; [
-      k3s
-      kube-longhorn
-    ];
+  imports = with flake.modules.nixos; [
+    k3s
+    kube-longhorn
+  ];
 
-    systemd.services.k3s.serviceConfig.LoadCredentialEncrypted = [
-      "k3s_token:/var/lib/systemd/credentials/k3s_token"
-    ];
+  systemd.services.k3s.serviceConfig.LoadCredentialEncrypted = [
+    "k3s_token:/var/lib/systemd/credentials/k3s_token"
+  ];
 
-    services.k3s = {
-      tokenFile = "/run/credentials/k3s.service/k3s_token";
-      clusterInit = hostName == "uff1";
-      extraFlags =
-        [
-          "--node-name ${hostName}s"
-          "--node-ip ${enu2}"
-          "--advertise-address ${enu2}"
-        ]
-        ++ lib.optionals (hostName != "uff1") [
-          "--server https://${getAddress hosts.uff1.interfaces.enu2.ipv4}:6443" # Serves as the bootstrapper
-        ];
-    };
+  services.k3s = {
+    tokenFile = "/run/credentials/k3s.service/k3s_token";
+    clusterInit = hostName == "uff1";
+    extraFlags =
+      [
+        "--node-name ${hostName}s"
+        "--node-ip ${enu2}"
+        "--advertise-address ${enu2}"
+      ]
+      ++ lib.optionals (hostName != "uff1") [
+        "--server https://${getAddress hosts.uff1.interfaces.enu2.ipv4}:6443" # Serves as the bootstrapper
+      ];
   };
 }
--- hosts/uff/modules/network/vrrp.nix
+++ hosts/uff/modules/network/vrrp.nix
@@ -1,18 +1,16 @@
-{
-  flake.modules.nixos.uff = {lib, ...}: {
-    services.keepalived.vrrpInstances = {
-      wifi = {
-        state = lib.mkDefault "BACKUP";
-        interface = "wlan1";
-        virtualRouterId = 2;
-        virtualIps = [{addr = "172.16.166.30/16";}];
-      };
-      lan = {
-        state = lib.mkDefault "BACKUP";
-        interface = "br1";
-        virtualRouterId = 3;
-        virtualIps = [{addr = "192.168.59.30/27";}];
-      };
+{lib, ...}: {
+  services.keepalived.vrrpInstances = {
+    wifi = {
+      state = lib.mkDefault "BACKUP";
+      interface = "wlan1";
+      virtualRouterId = 2;
+      virtualIps = [{addr = "172.16.166.30/16";}];
+    };
+    lan = {
+      state = lib.mkDefault "BACKUP";
+      interface = "br1";
+      virtualRouterId = 3;
+      virtualIps = [{addr = "192.168.59.30/27";}];
     };
   };
 }
