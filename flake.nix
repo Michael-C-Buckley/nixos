4e7b4d5c7b5847720febd75fb90ccfabf4e7aca4
Michael Buckley <michaelcbuckley@proton.me>
2026-05-19 19:05:12 -0400

    Flake: removed flake-parts from inputs and outputs

--- flake.nix
+++ flake.nix
@@ -1,27 +1,14 @@
 {
   description = "Michael's System Flake";
 
-  # I live the majority of things in files matching the name of their flake output type
-  outputs = {flake-parts, ...} @ inputs: let
+  outputs = inputs: let
     inherit (import ./lib/flake {inherit inputs;}) mkModule;
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-      imports = [
-        flake-parts.flakeModules.modules
-        flake-parts.flakeModules.touchup
-      ];
-
-      flake = {
-        nixosConfigurations = import ./outputs/nixosConfigurations.nix {inherit inputs;};
-        nixosModules = mkModule ./modules/nixos;
-        packages = import ./outputs/packages.nix {inherit inputs;};
-        devShells = import ./outputs/devShells.nix {inherit inputs;};
-      };
-
-      # I apparently need to tell them I don't use a formatter to not bug out
-      touchup.attr.formatter.enable = false;
-    };
+  in {
+    nixosConfigurations = import ./outputs/nixosConfigurations.nix {inherit inputs;};
+    nixosModules = mkModule ./modules/nixos;
+    packages = import ./outputs/packages.nix {inherit inputs;};
+    devShells = import ./outputs/devShells.nix {inherit inputs;};
+  };
 
   # I use some alternative fetchers to get some sources
   # Npins for git repos
@@ -31,11 +18,6 @@
     # It does properly lock and my main reason is to avoid github as a source
     # since lately there has been a good amount of availability issues
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
 
     hjem = {
       url = "github:feel-co/hjem";
@@ -54,7 +36,6 @@
       url = "github:numtide/disko-zfs";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
         disko.follows = "disko";
       };
     };
