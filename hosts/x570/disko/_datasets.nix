6da2c699261d56427742b5678b86d7852b274f7a
Michael Buckley <michaelcbuckley@proton.me>
2026-05-25 21:10:36 -0400

    X570/ZFS: added chimera datasets

--- hosts/x570/disko/_datasets.nix
+++ hosts/x570/disko/_datasets.nix
@@ -1,11 +1,16 @@
+# Declared datasets, which includes other distros I run as well since Disko is good at
+# formatting the drives to start with and disko-zfs helps too
 let
   # Reduce duplication
-  z = attrs:
-    {
+  z = attrs: let
+    base = {
       type = "zfs_fs";
-      options.mountpoint = "legacy";
-    }
-    // attrs;
+      options = {mountpoint = "legacy";};
+    };
+  in
+    base // attrs // {options = base.options // (attrs.options or {});};
+
+  chimera = "/var/lib/machines/chimera";
 in {
   nixos = z {
     options.mountpoint = "none";
@@ -42,5 +47,48 @@ in {
   # among distros but without various state folders, via binds
   michael = z {
     mountpoint = "/media/michael";
+  };
+
+  # Chimera Linux
+  # It's another distro I have installed, but mounted here so it can be run
+  # as a systemd-nspawn for convenience
+  chimera = z {
+    options.mountpoint = "none";
+  };
+
+  "chimera/system" = z {
+    options.mountpoint = "none";
+  };
+  "chimera/system/usr" = z {
+    mountpoint = "${chimera}/usr";
+    options.recordsize = "16K";
+  };
+  "chimera/system/etc" = z {
+    mountpoint = "${chimera}/etc";
+    options.recordsize = "4K";
+  };
+
+  "chimera/home" = z {
+    mountpoint = "${chimera}/home";
+  };
+  "chimera/var" = z {
+    mountpoint = "${chimera}/var";
+  };
+  "chimera/var/lib" = z {
+    mountpoint = "${chimera}/var/lib";
+  };
+  "chimera/var/log" = z {
+    mountpoint = "${chimera}/var/log";
+    options = {
+      recordsize = "1M";
+      compression = "zstd";
+    };
+  };
+  "chimera/nix" = z {
+    mountpoint = "${chimera}/nix";
+    options.recordsize = "32K";
+  };
+  "chimera/root" = z {
+    mountpoint = "${chimera}";
   };
 }
--- hosts/x570/disko/default.nix
+++ hosts/x570/disko/default.nix
@@ -1,24 +1,26 @@
 # I have split the disko config into several files as it's extremely
 # verbose and nested if kept together, which is just hard to read
 {
-  # WARNING: Must absolutely make sure of the device names at install time
-  disko.devices = {
-    disk = import ./_disks.nix;
-    lvm_vg = import ./_lvm.nix;
-    zpool.zroot = {
-      type = "zpool";
-      options = {
-        ashift = "12";
-        autotrim = "on";
+  disko = {
+    zfs.enable = true;
+    devices = {
+      disk = import ./_disks.nix;
+      lvm_vg = import ./_lvm.nix;
+      zpool.zroot = {
+        type = "zpool";
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        rootFsOptions = {
+          acltype = "posixacl";
+          atime = "off";
+          compression = "lz4";
+          normalization = "none";
+          xattr = "sa";
+        };
+        datasets = import ./_datasets.nix;
       };
-      rootFsOptions = {
-        acltype = "posixacl";
-        atime = "off";
-        compression = "lz4";
-        normalization = "none";
-        xattr = "sa";
-      };
-      datasets = import ./_datasets.nix;
     };
   };
 }
