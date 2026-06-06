cd6023c119aaf0ae6610b131af4508fda0c9b6c7
Michael Buckley <michaelcbuckley@proton.me>
2026-06-06 17:09:37 -0400

    BuildEnvs: added one for gentoo as well

--- packages/linux/chimeraEnv.nix
+++ packages/linux/chimeraEnv.nix
@@ -5,22 +5,21 @@
   ...
 }: let
   local = inputs.self.packages.${system};
+  nix-tools = builtins.attrValues {inherit (pkgs) nix-tree nix-direnv alejandra nil nixd;
+  inherit        (inputs.self.packages.${system})
+        ns
+        ;};
 in {
+  gentooEnv = pkgs.buildEnv {
+    name = "gentoo-env";
+    paths = with pkgs; [
+      yazi
+      pcmanfm
+    ] ++ nix-tools;
+
+    };
   chimeraEnv = pkgs.buildEnv {
     name = "chimera-runtime-env";
-    paths = builtins.attrValues {
-      inherit
-        (local)
-        # zeditor
-        #helium
-        ns
-        ;
-
-      inherit
-        (pkgs)
-        nix-tree
-        nix-direnv
-        ;
-    };
+    paths = nix-tools;
   };
 }
